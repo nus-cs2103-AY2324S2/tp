f40bb8ff962df9b452645a53c3b615169e024a7c
Chua Zi Long <e0957913@u.nus.edu>
2024-02-22 20:21:24 +0800

    Add code relevant to Tutorial 2

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.core;
+
+import seedu.address.logic.parser.Prefix;
+
+public class Messages {
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid Command";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "???";
+}
