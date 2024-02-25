eb7c924496e743e877b752b944d33651d1978638
yeoshuheng <yeoshuheng@gmail.com>
2024-02-25 21:51:26 +0800

    style: Fix checkstyle for test

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -75,7 +75,7 @@ public class JsonAdaptedPersonTest {
     @Test
     public void toModelType_nullPhone_throwsIllegalValueException() {
         JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_FUNDING, VALID_INDUSTRY,
-          null, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
+            null, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
