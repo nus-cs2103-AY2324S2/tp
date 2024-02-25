6c916d78717a24e63ace889976d7f9ad6a775c9f
Ashley <>
2024-02-26 01:29:36 +0800

    Edit the code to adhere to checkstyle

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -41,7 +41,8 @@
 //        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
 //        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
 //
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
+//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS,
+//            Messages.format(editedPerson));
 //
 //        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 //        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
@@ -62,7 +63,8 @@
 //                .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
 //        EditCommand editCommand = new EditCommand(indexLastPerson, descriptor);
 //
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
+//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS,
+//            Messages.format(editedPerson));
 //
 //        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 //        expectedModel.setPerson(lastPerson, editedPerson);
@@ -91,7 +93,8 @@
 //        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
 //                new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
 //
-//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
+//        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS,
+//            Messages.format(editedPerson));
 //
 //        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 //        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
