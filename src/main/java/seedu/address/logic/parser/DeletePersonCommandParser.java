27eebabf9d5d39ffc9df45574dd99c0158c60fd2
ForAeons <w.xy020203@gmail.com>
2024-02-26 20:56:50 +0800

    Rename crud command parser to crud person command parser

--- src/main/java/seedu/address/logic/parser/DeletePersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePersonCommandParser.java
@@ -7,7 +7,7 @@ import seedu.address.logic.commands.DeletePersonCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeletePersonCommand object
  */
 public class DeletePersonCommandParser implements Parser<DeletePersonCommand> {
 
