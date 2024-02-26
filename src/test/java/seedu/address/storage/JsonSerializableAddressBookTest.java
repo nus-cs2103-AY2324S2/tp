cd6d7fa261ad6227f6f67a612d77290f442ea665
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-26 18:57:05 +0800

    Fix JsonSerializableAddressBookTest

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.exceptions.DataLoadingException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.AddressBook;
@@ -30,10 +31,9 @@ public class JsonSerializableAddressBookTest {
     }
 
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
-        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    public void toModelType_invalidPersonFile_throwsDataLoadingException() {
+        assertThrows(DataLoadingException.class, () -> JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+                JsonSerializableAddressBook.class));
     }
 
     @Test
