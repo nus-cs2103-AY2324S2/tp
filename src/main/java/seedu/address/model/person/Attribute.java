b35d03a07be1dbaa30a3adf3716775de6d99efba
dom <songjunjiedominic@gmail.com>
2024-02-27 12:52:30 +0800

    fixed import and checkstyle issues for skeleton code

--- src/main/java/seedu/address/model/person/Attribute.java
+++ src/main/java/seedu/address/model/person/Attribute.java
@@ -1,12 +1,5 @@
 package seedu.address.model.person;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.time.LocalDate;
-
 abstract class Attribute {
     protected String name;
 
