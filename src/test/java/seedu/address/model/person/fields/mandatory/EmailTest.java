203ad1f68a26f417863ae4632e2a84ecbd92f76f
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-28 14:06:43 +0800

    Add TagTest

--- src/test/java/seedu/address/model/person/fields/mandatory/EmailTest.java
+++ src/test/java/seedu/address/model/person/fields/mandatory/EmailTest.java
@@ -1,12 +1,10 @@
-package seedu.address.model.person;
+package seedu.address.model.person.fields.mandatory;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-
-import seedu.address.model.person.fields.mandatory.Email;
 
 public class EmailTest {
 
