6e4d97e9d7e85b0ebe22ea9e449fd87ce51fa67a
Eunice Koh <eunicekoh46@gmail.com>
2024-03-04 13:34:54 +0800

    Debugging Edit Command and Edit Command Parser Test

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -23,6 +23,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.EditPersonDescriptorBuilder;
 
 /**
  * Contains helper methods for testing commands.
@@ -80,6 +81,26 @@ public class CommandTestUtil {
     // salary need to be above 0 dollar
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
+
+    //edit command test
+
+
+    public static final EditCommand.EditPersonDescriptor DESC_AMY;
+    public static final EditCommand.EditPersonDescriptor DESC_BOB;
+
+    static {
+        DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
+                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
+                .withTags(TAG_DESC_NO_REPLY).withJobDescription(JOB_DESCRIPTION_DESC_AMY)
+                .withInterviewDate(INTERVIEW_DATE_DESC_AMY).withInternDuration(INTERN_DURATION_DESC_AMY)
+                .withSalary(SALARY_DESC_AMY).build();
+        DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
+                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
+                .withTags(TAG_DESC_NO_REPLY).withJobDescription(JOB_DESCRIPTION_DESC_BOB)
+                .withInterviewDate(INTERVIEW_DATE_DESC_BOB).withInternDuration(INTERN_DURATION_DESC_BOB)
+                .withSalary(SALARY_DESC_BOB).build();
+    }
+
 
     /**
      * Executes the given {@code command}, confirms that <br>
