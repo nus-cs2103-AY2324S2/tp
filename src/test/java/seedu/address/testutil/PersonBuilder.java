ce8ddefcf75b69c586f005135ba4470c982db787
Chua Zi Long <e0957913@u.nus.edu>
2024-03-04 14:07:50 +0800

    Update code to comply with checkstyle

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -8,9 +8,9 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
-import seedu.address.model.person.Remark;
 
 /**
  * A utility class to help with building Person objects.
