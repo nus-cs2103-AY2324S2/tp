b423ed5d621f4fde5819ccfce54a440fe36f3f82
Chua Zi Long <e0957913@u.nus.edu>
2024-03-04 13:58:35 +0800

    Update code to comply with checkstyle

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.commons.core.Messages;
