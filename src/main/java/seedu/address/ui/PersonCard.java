ca6b0f1fea279cf2320412f2ff7c4affbb4b0eb1
Howlong11 <e1093040@u.nus.edu>
2024-03-05 16:40:46 +0800

    Revert "Add Remark command"
    This reverts commit 9c07543433eb0761bf98896e727f3453161aecaf.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -27,8 +27,6 @@ public class PersonCard extends UiPart<Region> {
     public final Person person;
 
     @FXML
-    private Label remark;
-    @FXML
     private HBox cardPane;
     @FXML
     private Label name;
@@ -54,7 +52,6 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        remark.setText(person.getRemark().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
