e753152296e03113d0ef994b161bbaae3a232541
Jolonauh <jeremiahau016@gmail.com>
2024-03-06 23:48:07 +0800

    Update Person class with description and nextOfKin fields

--- src/main/java/seedu/address/model/person/Description.java
+++ src/main/java/seedu/address/model/person/Description.java
@@ -3,16 +3,12 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-/**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
- */
-public class Address {
+public class Description {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Descriptions can take any values, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
+     * The first character of the description must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[^\\s].*";
@@ -20,20 +16,20 @@ public class Address {
     public final String value;
 
     /**
-     * Constructs an {@code Address}.
+     * Constructs a {@code Description}.
      *
-     * @param address A valid address.
+     * @param description A valid description.
      */
-    public Address(String address) {
-        requireNonNull(address);
-        checkArgument(isValidAddress(address), MESSAGE_CONSTRAINTS);
-        value = address;
+    public Description(String description) {
+        requireNonNull(description);
+        checkArgument(isValidDescription(description), MESSAGE_CONSTRAINTS);
+        value = description;
     }
 
     /**
      * Returns true if a given string is a valid description.
      */
-    public static boolean isValidAddress(String test) {
+    public static boolean isValidDescription(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
@@ -49,17 +45,16 @@ public class Address {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof Address)) {
+        if (!(other instanceof Description)) {
             return false;
         }
 
-        Address otherAddress = (Address) other;
-        return value.equals(otherAddress.value);
+        Description otherDescription = (Description) other;
+        return value.equals(otherDescription.value);
     }
 
     @Override
     public int hashCode() {
         return value.hashCode();
     }
-
 }
--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,11 +3,7 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -20,11 +16,15 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_DESCRIPTION = "Description placeholder";
+    public static final String DEFAULT_NOK = "Next of kin placeholder";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private Description description;
+    private NextOfKin nextOfKin;
     private Set<Tag> tags;
 
     /**
@@ -35,6 +35,8 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        description = new Description(DEFAULT_DESCRIPTION);
+        nextOfKin = new NextOfKin(DEFAULT_NOK);
         tags = new HashSet<>();
     }
 
@@ -46,6 +48,8 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        description = personToCopy.getDescription();
+        nextOfKin = personToCopy.getNextOfKin();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -89,8 +93,18 @@ public class PersonBuilder {
         return this;
     }
 
+    public PersonBuilder withDescription(String description) {
+        this.description = new Description(description);
+        return this;
+    }
+
+    public PersonBuilder withNextOfKin(String nextOfKin) {
+        this.nextOfKin = new NextOfKin(nextOfKin);
+        return this;
+    }
+
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, description, nextOfKin, tags);
     }
 
 }
