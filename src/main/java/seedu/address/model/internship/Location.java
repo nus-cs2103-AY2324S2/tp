e5c5c35dc6434c7e5fb0e5eb1da1aeee21b7982f
jieqiboh <jieqiboh5836@gmail.com>
2024-03-06 16:03:54 +0800

    Add Internship class and its basic fields

---
+++ src/main/java/seedu/address/model/internship/Location.java
@@ -0,0 +1,48 @@
+package seedu.address.model.internship;
+
+import static java.util.Objects.requireNonNull;
+
+public class Location {
+    /**
+     * Enum of locations
+     */
+    public enum LocationEnum {
+        LOCAL,
+        OVERSEAS,
+        REMOTE
+    }
+
+    public final LocationEnum location;
+
+    /**
+     * Constructs a {@code ApplicationStatus}.
+     *
+     * @param location A valid location.
+     */
+    public Location(LocationEnum location) {
+        requireNonNull(location);
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        switch (location) {
+            case LOCAL:
+                return "Local";
+            case OVERSEAS:
+                return "Overseas";
+            case REMOTE:
+                return "Remote";
+            default:
+                throw new IllegalArgumentException("Unexpected location: " + location);
+        }
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return location.hashCode();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/internship/Role.java
@@ -0,0 +1,43 @@
+package seedu.address.model.internship;
+
+import static java.util.Objects.requireNonNull;
+
+public class Role {
+
+    public final String role;
+
+    /**
+     * Constructs a {@code Role}.
+     *
+     * @param role A valid role.
+     */
+    public Role(String role) {
+        requireNonNull(role);
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Role)) {
+            return false;
+        }
+
+        Role otherName = (Role) other;
+        return role.equals(otherName.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return role.hashCode();
+    }
+}
