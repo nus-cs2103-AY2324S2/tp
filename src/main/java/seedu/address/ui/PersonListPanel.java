b87d1d1689bf7879354c676271e700f6f4414c64
caitlyntang <caitlyntong6@gmail.com>
2024-03-08 10:47:22 +0800

    Add new classes for appointment and case log view

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -33,14 +33,42 @@ public class PersonListPanel extends UiPart<Region> {
         caseLogListView.setItems(personList);
 
         personListView.setCellFactory(listView -> new PersonListViewCell());
-        appointmentListView.setCellFactory(listView -> new PersonListViewCell());
-        caseLogListView.setCellFactory(listView -> new PersonListViewCell());
+        appointmentListView.setCellFactory(listView -> new AppointmentListViewCell());
+        caseLogListView.setCellFactory(listView -> new CaseLogListViewCell());
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
     class PersonListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+    class AppointmentListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+    class CaseLogListViewCell extends ListCell<Person> {
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
