14abc8c0e62952317ed345aedd69d74f340bcdd6
NeoHW <neohaowei@gmail.com>
2024-03-08 17:22:36 +0800

    Fix failing TC in AddressBookTest
    Error was in toString method of AddressBook.java, where hardcoding of
    "person" lead to the failure

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -100,7 +100,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("persons", persons)
+                .add("patients", persons)
                 .toString();
     }
 
