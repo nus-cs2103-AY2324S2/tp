01aaa0603d740fa34ab3a8a0400e61c2f872a00c
chiageng <chiageng12@gmail.com>
2024-03-09 16:05:35 +0800

    Update AddressBookParser testing

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,16 +1,28 @@
 package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMISSION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMPLOYMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRODUCT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SALARY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SKILL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddMaintainerCommand;
+import seedu.address.logic.commands.AddStaffCommand;
+import seedu.address.logic.commands.AddSupplierCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.model.person.Maintainer;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Staff;
+import seedu.address.model.person.Supplier;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -25,6 +37,17 @@ public class PersonUtil {
         return AddCommand.COMMAND_WORD + " " + getPersonDetails(person);
     }
 
+    public static String getAddStaffCommand(Staff person) {
+        return AddStaffCommand.COMMAND_WORD + " " + getStaffDetails(person);
+    }
+
+    public static String getAddSupplierCommand(Supplier person) {
+        return AddSupplierCommand.COMMAND_WORD + " " + getSupplierDetails(person);
+    }
+    public static String getAddMaintainerCommand(Maintainer person) {
+        return AddMaintainerCommand.COMMAND_WORD + " " + getMaintainerDetails(person);
+    }
+
     /**
      * Returns the part of command string for the given {@code person}'s details.
      */
@@ -34,6 +57,63 @@ public class PersonUtil {
         sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
+        /*
+        person.getTags().stream().forEach(
+            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+         */
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code staff}'s details.
+     */
+    public static String getStaffDetails(Staff person) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + person.getName().fullName + " ");
+        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
+        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
+        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
+        sb.append(PREFIX_SALARY + person.getSalary().value + " ");
+        sb.append(PREFIX_EMPLOYMENT + person.getEmployment().employment + " ");
+        /*
+        person.getTags().stream().forEach(
+            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+         */
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code supplier}'s details.
+     */
+    public static String getSupplierDetails(Supplier person) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + person.getName().fullName + " ");
+        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
+        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
+        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
+        sb.append(PREFIX_PRODUCT + person.getProduct().product + " ");
+        sb.append(PREFIX_PRICE + person.getPrice().price + " ");
+        /*
+        person.getTags().stream().forEach(
+            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+         */
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code maintainer}'s details.
+     */
+    public static String getMaintainerDetails(Maintainer person) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + person.getName().fullName + " ");
+        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
+        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
+        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
+        sb.append(PREFIX_SKILL + person.getSkill().skill + " ");
+        sb.append(PREFIX_COMMISSION + person.getCommission().commission + " ");
         /*
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
