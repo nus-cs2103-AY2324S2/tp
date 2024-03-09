0159a73810563769027594fff5e11ed0d3b09253
tsulim <limxiujia18@gmail.com>
2024-03-10 04:32:56 +0800

    Refactor package seedu.address to staffconnect in test

---
+++ src/test/java/staffconnect/logic/commands/ListCommandTest.java
@@ -0,0 +1,39 @@
+package staffconnect.logic.commands;
+
+import static staffconnect.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static staffconnect.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static staffconnect.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static staffconnect.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import staffconnect.model.Model;
+import staffconnect.model.ModelManager;
+import staffconnect.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
