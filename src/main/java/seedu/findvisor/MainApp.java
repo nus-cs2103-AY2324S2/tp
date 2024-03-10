aea57699acc9408a972d89cf660a4b9054d74644
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-03-10 21:17:39 +0800

    Refactor package name
    Renamed all instaces of `seedu.address` to `seedu.findvisor`.

---
+++ src/main/java/seedu/findvisor/MainApp.java
@@ -0,0 +1,186 @@
+package seedu.findvisor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import seedu.findvisor.commons.core.Config;
+import seedu.findvisor.commons.core.LogsCenter;
+import seedu.findvisor.commons.core.Version;
+import seedu.findvisor.commons.exceptions.DataLoadingException;
+import seedu.findvisor.commons.util.ConfigUtil;
+import seedu.findvisor.commons.util.StringUtil;
+import seedu.findvisor.logic.Logic;
+import seedu.findvisor.logic.LogicManager;
+import seedu.findvisor.model.AddressBook;
+import seedu.findvisor.model.Model;
+import seedu.findvisor.model.ModelManager;
+import seedu.findvisor.model.ReadOnlyAddressBook;
+import seedu.findvisor.model.ReadOnlyUserPrefs;
+import seedu.findvisor.model.UserPrefs;
+import seedu.findvisor.model.util.SampleDataUtil;
+import seedu.findvisor.storage.AddressBookStorage;
+import seedu.findvisor.storage.JsonAddressBookStorage;
+import seedu.findvisor.storage.JsonUserPrefsStorage;
+import seedu.findvisor.storage.Storage;
+import seedu.findvisor.storage.StorageManager;
+import seedu.findvisor.storage.UserPrefsStorage;
+import seedu.findvisor.ui.Ui;
+import seedu.findvisor.ui.UiManager;
+
+/**
+ * Runs the application.
+ */
+public class MainApp extends Application {
+
+    public static final Version VERSION = new Version(0, 2, 2, true);
+
+    private static final Logger logger = LogsCenter.getLogger(MainApp.class);
+
+    protected Ui ui;
+    protected Logic logic;
+    protected Storage storage;
+    protected Model model;
+    protected Config config;
+
+    @Override
+    public void init() throws Exception {
+        logger.info("=============================[ Initializing AddressBook ]===========================");
+        super.init();
+
+        AppParameters appParameters = AppParameters.parse(getParameters());
+        config = initConfig(appParameters.getConfigPath());
+        initLogging(config);
+
+        UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
+        UserPrefs userPrefs = initPrefs(userPrefsStorage);
+        AddressBookStorage addressBookStorage = new JsonAddressBookStorage(userPrefs.getAddressBookFilePath());
+        storage = new StorageManager(addressBookStorage, userPrefsStorage);
+
+        model = initModelManager(storage, userPrefs);
+
+        logic = new LogicManager(model, storage);
+
+        ui = new UiManager(logic);
+    }
+
+    /**
+     * Returns a {@code ModelManager} with the data from {@code storage}'s address book and {@code userPrefs}. <br>
+     * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
+     * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
+     */
+    private Model initModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
+        logger.info("Using data file : " + storage.getAddressBookFilePath());
+
+        Optional<ReadOnlyAddressBook> addressBookOptional;
+        ReadOnlyAddressBook initialData;
+        try {
+            addressBookOptional = storage.readAddressBook();
+            if (!addressBookOptional.isPresent()) {
+                logger.info("Creating a new data file " + storage.getAddressBookFilePath()
+                        + " populated with a sample AddressBook.");
+            }
+            initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
+        } catch (DataLoadingException e) {
+            logger.warning("Data file at " + storage.getAddressBookFilePath() + " could not be loaded."
+                    + " Will be starting with an empty AddressBook.");
+            initialData = new AddressBook();
+        }
+
+        return new ModelManager(initialData, userPrefs);
+    }
+
+    private void initLogging(Config config) {
+        LogsCenter.init(config);
+    }
+
+    /**
+     * Returns a {@code Config} using the file at {@code configFilePath}. <br>
+     * The default file path {@code Config#DEFAULT_CONFIG_FILE} will be used instead
+     * if {@code configFilePath} is null.
+     */
+    protected Config initConfig(Path configFilePath) {
+        Config initializedConfig;
+        Path configFilePathUsed;
+
+        configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+
+        if (configFilePath != null) {
+            logger.info("Custom Config file specified " + configFilePath);
+            configFilePathUsed = configFilePath;
+        }
+
+        logger.info("Using config file : " + configFilePathUsed);
+
+        try {
+            Optional<Config> configOptional = ConfigUtil.readConfig(configFilePathUsed);
+            if (!configOptional.isPresent()) {
+                logger.info("Creating new config file " + configFilePathUsed);
+            }
+            initializedConfig = configOptional.orElse(new Config());
+        } catch (DataLoadingException e) {
+            logger.warning("Config file at " + configFilePathUsed + " could not be loaded."
+                    + " Using default config properties.");
+            initializedConfig = new Config();
+        }
+
+        //Update config file in case it was missing to begin with or there are new/unused fields
+        try {
+            ConfigUtil.saveConfig(initializedConfig, configFilePathUsed);
+        } catch (IOException e) {
+            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+        }
+        return initializedConfig;
+    }
+
+    /**
+     * Returns a {@code UserPrefs} using the file at {@code storage}'s user prefs file path,
+     * or a new {@code UserPrefs} with default configuration if errors occur when
+     * reading from the file.
+     */
+    protected UserPrefs initPrefs(UserPrefsStorage storage) {
+        Path prefsFilePath = storage.getUserPrefsFilePath();
+        logger.info("Using preference file : " + prefsFilePath);
+
+        UserPrefs initializedPrefs;
+        try {
+            Optional<UserPrefs> prefsOptional = storage.readUserPrefs();
+            if (!prefsOptional.isPresent()) {
+                logger.info("Creating new preference file " + prefsFilePath);
+            }
+            initializedPrefs = prefsOptional.orElse(new UserPrefs());
+        } catch (DataLoadingException e) {
+            logger.warning("Preference file at " + prefsFilePath + " could not be loaded."
+                    + " Using default preferences.");
+            initializedPrefs = new UserPrefs();
+        }
+
+        //Update prefs file in case it was missing to begin with or there are new/unused fields
+        try {
+            storage.saveUserPrefs(initializedPrefs);
+        } catch (IOException e) {
+            logger.warning("Failed to save config file : " + StringUtil.getDetails(e));
+        }
+
+        return initializedPrefs;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        logger.info("Starting AddressBook " + MainApp.VERSION);
+        ui.start(primaryStage);
+    }
+
+    @Override
+    public void stop() {
+        logger.info("============================ [ Stopping Address Book ] =============================");
+        try {
+            storage.saveUserPrefs(model.getUserPrefs());
+        } catch (IOException e) {
+            logger.severe("Failed to save preferences " + StringUtil.getDetails(e));
+        }
+    }
+}
