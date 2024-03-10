b5c00699915a7faf722fc0fa0cf97637ea486a5c
kaiyi27 <e1070556@u.nus.edu>
2024-03-10 22:00:20 +0800

    Modify minor bug in Person class

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -30,7 +30,7 @@ public class Person {
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, Relationship relationship, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, relationship, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
