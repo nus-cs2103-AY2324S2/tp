ed46bcc240e84198230d5d8aebe57b19e89ff0ae
Jawad <e0969647@u.nus.edu>
2024-03-11 18:00:45 +0800

    CommandResult and company card ui updated

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -26,7 +26,7 @@ public class CommandResult {
     /**
      * Constructs a {@code CommandResult} with the specified fields.
      */
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit, boolean view) {
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
@@ -39,7 +39,7 @@ public class CommandResult {
      * and other fields set to their default value.
      */
     public CommandResult(String feedbackToUser) {
-        this(feedbackToUser, false, false, false);
+        this(feedbackToUser, false, false);
     }
 
     /**
