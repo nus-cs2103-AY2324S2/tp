7f34befdddc23e9b57a908482d0d1f8b15f7fa72
lihaoquan <lihaoquan.dev@gmail.com>
2024-03-11 18:44:01 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -11,8 +11,8 @@ import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Note;
 import seedu.address.model.Model;
+import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
 
 /**
