67d98ff1733455b0b5159186ad5dd4a51b8cb4ce
yespiggy <skylarye7@gmail.com>
2024-03-11 21:25:09 +0800

    feat: update list command to show industry, funding stage

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,6 +39,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private FlowPane industryAndFundingStage;
+    @FXML
     private FlowPane tags;
 
     /**
@@ -52,6 +54,9 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        industryAndFundingStage.getChildren().addAll(
+                new Label(person.getIndustry().value),
+                new Label(person.getFundingStage().value));
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
