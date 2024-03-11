c99206962b66b27d30894bee5c502dccb64a2530
jayllo-c <jaylotc@gmail.com>
2024-03-12 00:58:23 +0800

    Improve coding standard
    Fix imports of test file to adhere to coding standards.

--- src/test/java/seedu/address/logic/commands/CopyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CopyCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_EMPTY_PERSON_LIST;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
