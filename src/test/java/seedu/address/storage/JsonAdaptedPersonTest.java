ef5d56f5f0dc854c49577ea52b971e0ca7d2175a
alfaloo <zhiylu528@gmail.com>
2024-03-11 23:50:50 +0800

    Add more JUnit tests

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -108,3 +108,74 @@
 //    }
 //
 //}
+
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonAdaptedPersonTest {
+    private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_PHONE = "+651234";
+    private static final String INVALID_ADDRESS = " ";
+    private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_TAG = "#friend";
+
+    private static final String VALID_NAME = BENSON.getName().toString();
+    private static final String VALID_PHONE = BENSON.getPhone().toString();
+
+    @Test
+    public void toModelType_validPersonDetails_returnsPerson() throws Exception {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(BENSON);
+        assertEquals(BENSON, person.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_invalidPhone_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_nullPhone_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_invalidEmail_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_nullEmail_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_invalidAddress_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_nullAddress_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void toModelType_invalidTags_throwsIllegalValueException() {
+        assertTrue(true);
+    }
+
+}
