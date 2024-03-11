1450aaa9afdf0d6f9404a48c8aef51542feffb2a
whelan-low <e0958047@u.nus.edu>
2024-03-11 23:20:12 +0800

    Fix variable names

--- src/main/java/seedu/address/logic/parser/AddClassCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddClassCommandParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_CLASS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULECODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIALCLASS;
 
 import seedu.address.logic.commands.AddClassCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -21,11 +21,11 @@ public class AddClassCommandParser implements Parser<AddClassCommand> {
      */
     public AddClassCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_MODULE_CODE, PREFIX_TUTORIAL_CLASS);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_MODULECODE, PREFIX_TUTORIALCLASS);
 
-        String moduleCode = argMultimap.getValue(PREFIX_MODULE_CODE).orElse("");
-        String tutorialClass = argMultimap.getValue(PREFIX_TUTORIAL_CLASS).orElse("");
+        String moduleCode = argMultimap.getValue(PREFIX_MODULECODE).orElse("");
+        String tutorialClass = argMultimap.getValue(PREFIX_TUTORIALCLASS).orElse("");
 
-        return new AddClassCommand(new Module(new Name(moduleCode), new TutorialClass(new Name(tutorialClass))));
+        return new AddClassCommand(new Module(new Name(moduleCode), new TutorialClass(tutorialClass)));
     }
 }
