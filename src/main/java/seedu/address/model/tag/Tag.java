1479fbb5101a4dc6ce6dd08175e60af7667673d9
Jawad <e0969647@u.nus.edu>
2024-03-11 21:38:36 +0800

    EditCommandTest and EditCommandParserTest Updated

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -29,7 +29,7 @@ public class Tag {
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidTagName(String test) {
-        return test.matches(VALIDATION_REGEX) || test.isEmpty();
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
