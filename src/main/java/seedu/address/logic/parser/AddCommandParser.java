e01b29900e27f139ea98a93bcbc019f1ee0cf6ea
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-03-11 12:37:48 +0800

    Add CLI syntax prefix for income and family size

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,6 +6,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INCOME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FAMILYSIZE;
 
 import java.util.Set;
 import java.util.stream.Stream;
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,9 +7,16 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_INCOME = new Prefix("i/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    //To remove EMAIl field soon
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_FAMILYSIZE = new Prefix("f/");
+
+    //Possible rename to additional notes to fit feature spec
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+
+
 
 }
