626880870bfde931658fe6734b16e46d531fba91
whelan-low <e0958047@u.nus.edu>
2024-03-11 22:27:39 +0800

    Add Javadocs for methods in Module and TutorialClass

--- src/main/java/seedu/address/model/module/Module.java
+++ src/main/java/seedu/address/model/module/Module.java
@@ -6,17 +6,31 @@ import java.util.ArrayList;
 
 import seedu.address.model.person.Name;
 
-
+/**
+ * Class used to handle the representation of modules.
+ */
 public class Module {
     final Name name;
     private final ArrayList<TutorialClass> tutorialClasses;
 
+    /**
+     * A constructor for Module. Used to initialise a new module with no tutorial classes
+     *
+     * @param name of the module to be created
+     */
     public Module(Name name) {
         requireAllNonNull(name);
         this.name = name;
         this.tutorialClasses = new ArrayList<TutorialClass>();
     }
 
+    /**
+     * A constructor for Module. Used to initialise a new module with the tutorial class specified.
+     * This is the constructor used when /add_class is used.
+     *
+     * @param name of the module to be created
+     * @param tutorialClass to be added within the module
+     */
     public Module(Name name, TutorialClass tutorialClass) {
         requireAllNonNull(name);
         this.name = name;
@@ -24,6 +38,13 @@ public class Module {
         tutorialClasses.add(tutorialClass);
     }
 
+    /**
+     * A constructor for Module. Used to initialise a new module with the list of tutorial classes specified.
+     * Used to get the representation of the module from the list of classes and the name specified.
+     *
+     * @param name of the module to be created
+     * @param tutorialClasses of the module to be created
+     */
     public Module(Name name, ArrayList<TutorialClass> tutorialClasses) {
         requireAllNonNull(name);
         this.name = name;
@@ -51,10 +72,20 @@ public class Module {
         return name;
     }
 
+    /**
+     * Returns the ArrayList of tutorial classes under this module.
+     *
+     * @return the ArrayList of tutorial classes.
+     */
     public ArrayList<TutorialClass> getTutorialClasses() {
         return tutorialClasses;
     }
 
+    /**
+     * Outputs the list of tutorial classes under this module as a String.
+     *
+     * @return the list of tutorial classes.
+     */
     public String getTutorialClassesNames() {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < tutorialClasses.size(); i++) {
--- src/main/java/seedu/address/model/module/TutorialClass.java
+++ src/main/java/seedu/address/model/module/TutorialClass.java
@@ -7,16 +7,30 @@ import java.util.ArrayList;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
+/**
+ * Class used to handle representation of a tutorial class within a module.
+ */
 public class TutorialClass {
     final Name name;
     private final ArrayList<Person> students;
 
+    /**
+     * A constructor for TutorialClass. Creates an empty tutorial class with no students.
+     *
+     * @param name of tutorial to be added
+     */
     public TutorialClass(Name name) {
         requireAllNonNull(name);
         this.name = name;
         this.students = new ArrayList<Person>();
     }
 
+    /**
+     * A constructor for TutorialClass. Creates a tutorial with one student, that is specified.
+     *
+     * @param name of tutorial to be added
+     * @param student to be added
+     */
     public TutorialClass(Name name, Person student) {
         requireAllNonNull(name);
         this.name = name;
@@ -24,6 +38,12 @@ public class TutorialClass {
         students.add(student);
     }
 
+    /**
+     * A constructor for TutorialClass. Creates a tutorial with the list of students specified.
+     *
+     * @param name of tutorial to be added
+     * @param students to be in the added tutorial
+     */
     public TutorialClass(Name name, ArrayList<Person> students) {
         requireAllNonNull(name);
         this.name = name;
