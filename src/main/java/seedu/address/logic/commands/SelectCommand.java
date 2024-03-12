a4d5d4d7b2f5199f2ac750b7ae28cd7e6f778a79
ziyang27 <tanziyang39@gmail.com>
2024-03-12 12:02:35 +0800

    Create a new select command

---
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -0,0 +1,69 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Selects a person identified using it's displayed index from the address book.
+ */
+public class SelectCommand extends Command {
+
+    public static final String COMMAND_WORD = "select";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Selects the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Person: %1$s";
+
+    private final Index targetIndex;
+
+    public SelectCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToSelect = lastShownList.get(targetIndex.getZeroBased());
+        model.deletePerson(personToSelect);
+        return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, Messages.format(personToSelect)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof SelectCommand)) {
+            return false;
+        }
+
+        SelectCommand otherSelectCommand = (SelectCommand) other;
+        return targetIndex.equals(otherSelectCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
