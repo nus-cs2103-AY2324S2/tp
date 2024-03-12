9ff168c91574edd24da889236f7198c8de02c721
wallacexuhanxiao <wallacexuhanxiao@gmail.com>
2024-03-12 10:33:57 +0800

    Fix check style issue

--- src/test/java/seedu/address/model/person/IdMatchesPredicateTest.java
+++ src/test/java/seedu/address/model/person/IdMatchesPredicateTest.java
@@ -1,9 +1,12 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.testutil.PersonBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
 
 class IdMatchesPredicateTest {
 
