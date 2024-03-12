d83dc2be91763439f8333f10ed24d5256fc5407d
tsulim <limxiujia18@gmail.com>
2024-03-13 02:37:11 +0800

    Add Module data field to Person

--- src/main/java/staffconnect/model/person/Person.java
+++ src/main/java/staffconnect/model/person/Person.java
@@ -23,17 +23,19 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Module module;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Module module, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, module, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.module = module;
         this.tags.addAll(tags);
     }
 
@@ -51,6 +53,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Module getModule() {
+        return module;
     }
 
     /**
@@ -94,13 +100,14 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
+                && module.equals(otherPerson.module)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, module, tags);
     }
 
     @Override
@@ -110,6 +117,7 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("module", module)
                 .add("tags", tags)
                 .toString();
     }
