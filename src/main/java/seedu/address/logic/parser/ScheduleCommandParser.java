8b069498a25c24bd2a6da232322b4bdddab94af1
REGAN\regan <e0957884@u.nus.edu>
2024-03-13 01:38:35 +0800

    Add new test cases for all Parsers and Commands

--- src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SCHEDULE;
