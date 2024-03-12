1505831b1ce7459b614fa02fb34bc7db47535431
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-03-13 02:18:40 +0800

    Add test for Date and Order and AddOrderCommandParser

--- src/test/java/seedu/address/logic/parser/AddOrderCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddOrderCommandParserTest.java
@@ -1,4 +1,42 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.AddOrderCommand;
+import seedu.address.model.order.Date;
+
 public class AddOrderCommandParserTest {
+    private static final String NON_EMPTY_DATE = "2020-01-01";
+    private static final String NON_EMPTY_DESCRIPTION = "100 chicken wings";
+
+    private AddOrderCommandParser parser = new AddOrderCommandParser();
+
+    @Test
+    public void parse_indexSpecified_success() {
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_DATE + NON_EMPTY_DATE + " "
+                + PREFIX_DESCRIPTION + NON_EMPTY_DESCRIPTION;
+
+        AddOrderCommand expectedCommand = new AddOrderCommand(INDEX_FIRST_PERSON, new Date(NON_EMPTY_DATE),
+                NON_EMPTY_DESCRIPTION);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_missingDateField_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddOrderCommand.MESSAGE_USAGE);
+
+        // no parameters
+        assertParseFailure(parser, AddOrderCommand.COMMAND_WORD, expectedMessage);
+
+        // no index
+        assertParseFailure(parser, AddOrderCommand.COMMAND_WORD + " " + PREFIX_DATE + NON_EMPTY_DATE,
+                expectedMessage);
+    }
 }
--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -1,4 +1,46 @@
 package seedu.address.model.order;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class OrderTest {
+    private static final String VALID_DATE = "2020-01-01";
+    private static final String VALID_DESCRIPTION = "100 chicken wings";
+
+    private static final String INVALID_DATE = "2020-99-99";
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Order(null, null));
+    }
+
+    @Test
+    public void constructor_invalidDate_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Order(new Date(INVALID_DATE), VALID_DESCRIPTION));
+    }
+
+    @Test
+    public void equals() {
+        Order order = new Order(new Date(VALID_DATE), VALID_DESCRIPTION);
+
+        // same object -> returns true
+        assertTrue(order.equals(order));
+
+        // same values -> returns true
+        Order orderCopy = new Order(order.getDate(), order.getDescription());
+        assertTrue(order.equals(orderCopy));
+
+        // different types -> returns false
+        assertFalse(order.equals(1));
+
+        // null -> returns false
+        assertFalse(order.equals(null));
+
+        // different Order -> returns false
+        Order differentOrder = new Order(order.getDate(), "200 chicken wings");
+        assertFalse(order.equals(differentOrder));
+    }
 }
