9608f2adadf183b510801249087fbf278e1c8f40
dillontkh <e0958762@u.nus.edu>
2024-03-13 01:00:33 +0800

    Add confirmation step to clear address book

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
+/**/
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
@@ -9,15 +9,25 @@ import seedu.address.model.Model;
  * Clears the address book.
  */
 public class ClearCommand extends Command {
-
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "FitBook has been cleared!";
+    public static final String MESSAGE_CONFIRM = "Are you sure you want to clear ALL clients from FitBook? Enter "
+            + "'clear /confirm' to confirm.";
+    private final boolean confirmed;
 
+    public ClearCommand(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+
+        if (!confirmed) {
+            return new CommandResult(MESSAGE_CONFIRM);
+        } else {
+            model.setAddressBook(new AddressBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
     }
 }
