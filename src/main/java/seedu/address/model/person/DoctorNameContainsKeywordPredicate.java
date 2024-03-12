4bfcd334c9e93accd1321869f9ed28402faaf6ae
Alteqa <e0969816@u.nus.edu>
2024-03-12 23:10:08 +0800

    Revert "Revert "partial addition of query doctor""
    This reverts commit dd1567c1f0c68cbb1cbb61db457d67cca181efe3.

---
+++ src/main/java/seedu/address/model/person/DoctorNameContainsKeywordPredicate.java
@@ -0,0 +1,48 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Patient}'s {@code Name} matches any of the keywords given.
+ */
+public class DoctorNameContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public DoctorNameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person patient) {
+        if (patient.getType() != Type.PATIENT) {
+            return false;
+        }
+
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(patient.getName().fullName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PatientNameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        PatientNameContainsKeywordsPredicate otherPredicate = (PatientNameContainsKeywordsPredicate) other;
+        return keywords.equals(otherPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
