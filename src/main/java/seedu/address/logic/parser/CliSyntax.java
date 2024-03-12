ffb1e62302fd0f31af853a43d8f1af9e39552102
choowengyan <wengstudies0407@gmail.com>
2024-03-12 10:28:34 +0800

    Add 'Add Command' prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,9 +7,10 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_PRENAME = new Prefix("p/");
+    public static final Prefix PREFIX_FOOD = new Prefix("f/");
+    public static final Prefix PREFIX_FAMILY = new Prefix("c/");
+    public static final Prefix PREFIX_HOBBY = new Prefix("h/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
