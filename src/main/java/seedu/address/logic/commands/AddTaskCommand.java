815607a21ca22ad773a12d6be0f2d0db26d6ce6c
Kaya3842 <haichiendhnn1@gmail.com>
2024-03-13 05:34:38 +0800

    Create AddTaskCommand and AddTaskCommandParser classes
    Let's create an AddTaskCommand class to represent the command that
    adds a task to the book.
    
    Let's create an AddTaskCommand Parser class to process the user input
    for an add task command.

---
+++ src/main/java/seedu/address/logic/commands/AddTaskCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.task.Task;
+
+public class AddTaskCommand extends Command {
+
+    public static final String COMMAND_WORD = "add task";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddTaskCommand to add the specified {@code Task}
+     */
+    public AddTaskCommand(Task task) {
+        requireNonNull(task);
+        toAdd = task;
+    }
+
+    /**
+     * Executes the command and returns the result message.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     * @throws CommandException If an error occurs during command execution.
+     */
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddCommand)) {
+            return false;
+        }
+
+        AddTaskCommand otherAddTaskCommand = (AddTaskCommand) other;
+        return toAdd.equals(otherAddTaskCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/AddTaskCommandParser.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AddTaskCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AddTaskCommandParser implements Parser<AddTaskCommand> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     *
+     * @param userInput
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    @Override
+    public AddTaskCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
