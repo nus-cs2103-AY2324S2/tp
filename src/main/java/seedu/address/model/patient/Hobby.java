bf8226bd1fc93c2207e8251175c46447c4d05534
choowengyan <wengstudies0407@gmail.com>
2024-03-12 10:27:11 +0800

    Add Hobby.java

--- src/main/java/seedu/address/model/patient/Hobby.java
+++ src/main/java/seedu/address/model/patient/Hobby.java
@@ -4,56 +4,37 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Patient's email in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
+ * Represents a Patient's hobby in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidHobby(String)}
  */
-public class Email {
+public class Hobby {
 
-    private static final String SPECIAL_CHARACTERS = "+_.-";
-    public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
-            + "and adhere to the following constraints:\n"
-            + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
-            + "characters.\n"
-            + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
-            + "separated by periods.\n"
-            + "The domain name must:\n"
-            + "    - end with a domain label at least 2 characters long\n"
-            + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
-    // alphanumeric and special characters
-    private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
-    private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
-            + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_PART_REGEX = ALPHANUMERIC_NO_UNDERSCORE
-            + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
-    private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
-    public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
-    public final String value;
+    public static final String MESSAGE_CONSTRAINTS = "Hobby can take any values, and it should not be blank";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public final String hobby;
 
     /**
-     * Constructs an {@code Email}.
+     * Constructs a {@code Hobby}.
      *
-     * @param email A valid email address.
+     * @param description A valid hobby.
      */
-    public Email(String email) {
-        requireNonNull(email);
-        checkArgument(isValidEmail(email), MESSAGE_CONSTRAINTS);
-        value = email;
+    public Hobby(String description) {
+        requireNonNull(description);
+        checkArgument(isValidHobby(description), MESSAGE_CONSTRAINTS);
+        hobby = description;
     }
 
     /**
-     * Returns if a given string is a valid email.
+     * Returns true if a given string is a valid hobby.
      */
-    public static boolean isValidEmail(String test) {
+    public static boolean isValidHobby(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
     @Override
     public String toString() {
-        return value;
+        return hobby;
     }
 
     @Override
@@ -63,17 +44,17 @@ public class Email {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof Email)) {
+        if (!(other instanceof Hobby)) {
             return false;
         }
 
-        Email otherEmail = (Email) other;
-        return value.equals(otherEmail.value);
+        Hobby otherHobby = (Hobby) other;
+        return hobby.equals(otherHobby.hobby);
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return hobby.hashCode();
     }
 
 }
