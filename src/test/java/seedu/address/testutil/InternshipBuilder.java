2ef0a91270d7e5de47268a922a633369c090b8e6
jieqiboh <jieqiboh5836@gmail.com>
2024-03-12 19:06:09 +0800

    Add JsonAdaptedInternship and tests
    Also add InternshipBuilder and TypicalInternships, which support
    Internship creation for testing purposes

---
+++ src/test/java/seedu/address/testutil/InternshipBuilder.java
@@ -0,0 +1,131 @@
+package seedu.address.testutil;
+
+import seedu.address.model.internship.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
+/**
+ * A utility class to help with building Internship objects.
+ */
+public class InternshipBuilder {
+
+    public static final String DEFAULT_COMPANY_NAME = "Microsoft";
+    public static final String DEFAULT_CONTACT_NAME = "Alice Pauline";
+    public static final String DEFAULT_CONTACT_EMAIL = "alice@example.com";
+    public static final String DEFAULT_CONTACT_NUMBER = "94351253";
+    public static final String DEFAULT_LOCATION = "remote";
+    public static final String DEFAULT_APPLICATION_STATUS = "ongoing";
+    public static final String DEFAULT_DESCRIPTION = "Use Figma to design User-friendly web interfaces";
+    public static final String DEFAULT_ROLE = "Frontend Engineer";
+
+    private CompanyName companyName;
+    private ContactName contactName;
+    private ContactEmail contactEmail;
+    private ContactNumber contactNumber;
+    private Location location;
+    private ApplicationStatus applicationStatus;
+    private Description description;
+    private Role role;
+
+    /**
+     * Creates an {@code InternshipBuilder} with the default details.
+     */
+    public InternshipBuilder() {
+        companyName = new CompanyName(DEFAULT_COMPANY_NAME);
+        contactName = new ContactName(DEFAULT_CONTACT_NAME);
+        contactEmail = new ContactEmail(DEFAULT_CONTACT_EMAIL);
+        contactNumber = new ContactNumber(DEFAULT_CONTACT_NUMBER);
+        location = new Location(DEFAULT_LOCATION);
+        applicationStatus = new ApplicationStatus(DEFAULT_APPLICATION_STATUS);
+        description = new Description(DEFAULT_DESCRIPTION);
+        role = new Role(DEFAULT_ROLE);
+    }
+
+    /**
+     * Initializes the InternshipBuilder with the data of {@code internshipToCopy}.
+     */
+    public InternshipBuilder(Internship internshipToCopy) {
+        companyName = internshipToCopy.getCompanyName();
+        contactName = internshipToCopy.getContactName();
+        contactEmail = internshipToCopy.getContactEmail();
+        contactNumber = internshipToCopy.getContactNumber();
+        location = internshipToCopy.getLocation();
+        applicationStatus = internshipToCopy.getApplicationStatus();
+        description = internshipToCopy.getDescription();
+        role = internshipToCopy.getRole();
+    }
+
+    /**
+     * Sets the {@code CompanyName} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withCompanyName(String companyName) {
+        this.companyName = new CompanyName(companyName);
+        return this;
+    }
+
+    /**
+     * Sets the {@code ContactName} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withContactName(String contactName) {
+        this.contactName = new ContactName(contactName);
+        return this;
+    }
+
+    /**
+     * Sets the {@code ContactEmail} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withContactEmail(String contactEmail) {
+        this.contactEmail = new ContactEmail(contactEmail);
+        return this;
+    }
+
+    /**
+     * Sets the {@code ContactNumber} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withContactNumber(String contactNumber) {
+        this.contactNumber = new ContactNumber(contactNumber);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Location} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withLocation(String location) {
+        this.location = new Location(location);
+        return this;
+    }
+
+    /**
+     * Sets the {@code ApplicationStatus} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withApplicationStatus(String applicationStatus) {
+        this.applicationStatus = new ApplicationStatus(applicationStatus);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withDescription(String description) {
+        this.description = new Description(description);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Role} of the {@code Internship} that we are building.
+     */
+    public InternshipBuilder withRole(String role) {
+        this.role = new Role(role);
+        return this;
+    }
+
+    public Internship build() {
+        return new Internship(companyName, contactName, contactEmail, contactNumber, location, applicationStatus, description, role);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/TypicalInternships.java
@@ -0,0 +1,35 @@
+package seedu.address.testutil;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.internship.Internship;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static seedu.address.logic.commands.CommandTestUtil.*;
+
+public class TypicalInternships {
+
+    public static final Internship ALICE_MICROSOFT = new InternshipBuilder().withCompanyName("Microsoft")
+            .withContactName("Alice Pauline").withContactEmail("alice@example.com").withContactNumber("94351253")
+            .withApplicationStatus("ongoing").withLocation("remote")
+            .withDescription("Use Figma to design User-friendly web interfaces").withRole("Frontend Engineer")
+            .build();
+
+    public static final Internship BENSON_GOOGLE = new InternshipBuilder().withCompanyName("Google")
+            .withContactName("Benson Meier").withContactEmail("benson@example.com").withContactNumber("98765432")
+            .withLocation("local").withApplicationStatus("pending").withDescription("Write REST APIs for services")
+            .withRole("Backend Engineer").build();
+
+    public static final Internship CARL_OPENAI = new InternshipBuilder().withCompanyName("OpenAI")
+            .withContactName("Carl Kurz").withContactEmail("carl@google.com").withContactNumber("95352563")
+            .withLocation("overseas").withApplicationStatus("rejected").withDescription("Enter prompts into ChatGPT")
+            .withRole("Prompt Engineer").build();
+
+    private TypicalInternships() {} // prevents instantiation
+
+    public static List<Internship> getTypicalInternships() {
+        return new ArrayList<>(Arrays.asList(ALICE_MICROSOFT, BENSON_GOOGLE, CARL_OPENAI));
+    }
+}
