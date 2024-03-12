87a53143b31f6bf414568c4911c822e814dab141
tsulim <limxiujia18@gmail.com>
2024-03-13 04:17:29 +0800

    Update Messages with Module data field

--- src/main/java/staffconnect/logic/Messages.java
+++ src/main/java/staffconnect/logic/Messages.java
@@ -43,6 +43,8 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Module: ")
+                .append(person.getModule())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
