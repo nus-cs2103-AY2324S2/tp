05190eceec8877a7da0ecbc19529d97355e807a4
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-03-12 14:51:50 +0800

    Fix bug where PersonCard does not auto update order count when new order is added

--- src/main/java/seedu/address/logic/commands/AddOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/AddOrderCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
@@ -56,6 +57,7 @@ public class AddOrderCommand extends Command {
         Person person = lastShownList.get(index.getZeroBased());
         person.addOrder(this.arrivalDate, this.description);
 
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(person)));
     }
 
