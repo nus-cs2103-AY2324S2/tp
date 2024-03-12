8d1e79ac0ab27c1eb3cbe7f4ec52c41565b2b25f
lihaoquan <lihaoquan.dev@gmail.com>
2024-03-13 01:56:49 +0800

    Create blue print classes for Applicant, Role, and Stage

---
+++ src/main/java/seedu/address/model/application/Application.java
@@ -0,0 +1,21 @@
+package seedu.address.model.application;
+
+/**
+ * A class that represents a particular
+ * interview application.
+ */
+public class Application {
+
+    public static final Stage INITIAL_APPLICATION = new Stage("Initial Application");
+    public static final Stage TECHNICAL_ASSESSMENT = new Stage("Technical Assessment");
+    public static final Stage INTERVIEW = new Stage("Interview");
+    public static final Stage DECISION_AND_OFFER = new Stage("Decision & Offer");
+
+    // TODO: Add more required roles here.
+    public static final Role FRONT_END = new Role("Front End");
+    public static final Role BACK_END = new Role("Back End");
+
+    public Application(Applicant applicant, Role role) {
+        // TODO: Add application logic here.
+    }
+}
