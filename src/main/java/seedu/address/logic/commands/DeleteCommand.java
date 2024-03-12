24f789350d50db2f986c38b90280d884ebd7c6b8
jxunze <jinxunzejxz@gmail.com>
2024-03-12 21:26:57 +0800

    Fix commit errors

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,1 +1,70 @@
-package seedu.address.logic.commands;import static java.util.Objects.requireNonNull;import java.util.List;import seedu.address.model.person.Phone;import seedu.address.commons.core.index.Index;import seedu.address.commons.util.ToStringBuilder;import seedu.address.logic.Messages;import seedu.address.logic.commands.exceptions.CommandException;import seedu.address.model.Model;import seedu.address.model.person.Person;/** * Deletes a person identified using it's displayed index from the address book. */public class DeleteCommand extends Command {    public static final String COMMAND_WORD = "delete";    public static final String MESSAGE_USAGE = COMMAND_WORD            + ": Deletes the person identified by the phone number.\n"            + "Parameters: PHONE NUMBER\n"            + "Example: " + COMMAND_WORD + " 89348843";    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";    private final Phone targetPhone;    public DeleteCommand(Phone targetPhone) {        this.targetPhone = targetPhone;    }    @Override    public CommandResult execute(Model model) throws CommandException {        requireNonNull(model);        List<Person> lastShownList = model.getFilteredPersonList();        for (Person person : lastShownList) {            if (person.getPhone().equals(targetPhone)) {                model.deletePerson(person);                return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(person)));            }        }        return new CommandResult(Messages.MESSAGE_PERSON_NOT_FOUND);    }    @Override    public boolean equals(Object other) {        if (other == this) {            return true;        }        // instanceof handles nulls        if (!(other instanceof DeleteCommand)) {            return false;        }        DeleteCommand otherDeleteCommand = (DeleteCommand) other;        return targetPhone.equals(otherDeleteCommand.targetPhone);    }    @Override    public String toString() {        return new ToStringBuilder(this)                .add("targetIndex", targetPhone)                .toString();    }}
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.model.person.Phone;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Deletes a person identified using it's displayed index from the address book.
+ */
+public class DeleteCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the person identified by the phone number.\n"
+            + "Parameters: PHONE NUMBER\n"
+            + "Example: " + COMMAND_WORD + " 89348843";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+
+    private final Phone targetPhone;
+
+    public DeleteCommand(Phone targetPhone) {
+        this.targetPhone = targetPhone;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        for (Person person : lastShownList) {
+            if (person.getPhone().equals(targetPhone)) {
+                model.deletePerson(person);
+                return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(person)));
+            }
+        }
+        return new CommandResult(Messages.MESSAGE_PERSON_NOT_FOUND);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteCommand)) {
+            return false;
+        }
+
+        DeleteCommand otherDeleteCommand = (DeleteCommand) other;
+        return targetPhone.equals(otherDeleteCommand.targetPhone);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetPhone)
+                .toString();
+    }
+}
