1a761c7ffae2fde17f96c9c736732c4f794eb636
Carl <e0959394@u.nus.edu.sg>
2024-03-12 22:44:00 +0800

    Add new Level class
    This class is to be used by house class later on.

---
+++ src/main/java/seedu/address/model/house/Level.java
@@ -0,0 +1,61 @@
+package seedu.address.model.house;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+
+/**
+ * Represents a House's level.
+ * Guarantees: immutable; is valid as declared in {@link #isValidLevel(String)}
+ */
+public class Level {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Level should only contain numbers, and it should only be at most 2 digits long";
+    public static final String VALIDATION_REGEX = "\\d{1,2}";
+    public final String value;
+
+    /**
+     * Constructs a {@code Level}.
+     *
+     * @param level A valid level.
+     */
+    public Level(String level) {
+        requireNonNull(level);
+        checkArgument(isValidLevel(level), MESSAGE_CONSTRAINTS);
+        value = level;
+    }
+
+    /**
+     * Returns true if a given string is a valid level.
+     */
+    public static boolean isValidLevel(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Level)) {
+            return false;
+        }
+
+        Level otherLevel = (Level) other;
+        return value.equals(otherLevel.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
