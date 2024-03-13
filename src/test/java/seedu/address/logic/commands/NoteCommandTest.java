9d540f1f50f02121b0acfda0a96e11feb4de8c96
ruijietay <47778098+ruijietay@users.noreply.github.com>
2024-03-13 22:26:51 +0800

    Fix checkstyle violations

--- src/test/java/seedu/address/logic/commands/NoteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NoteCommandTest.java
@@ -10,8 +10,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Note;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 class NoteCommandTest {
