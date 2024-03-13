51241de9d0fef2ded2172d19baaae1645d7c17d8
Liu Jiayao <jiayao_liu@u.nus.edu>
2024-03-13 16:38:59 +0800

    Fix the typo in TypicalIds.java

--- src/test/java/seedu/address/testutil/TypicalIds.java
+++ src/test/java/seedu/address/testutil/TypicalIds.java
@@ -6,8 +6,8 @@ import seedu.address.model.person.Id;
  * A utility class containing a list of {@code Id} objects to be used in tests.
  */
 public class TypicalIds {
-    public static final Id IN_FIRST_PERSON = new Id("charliechua45");
-    public static final Id IN_SECOND_PERSON = new Id("bossbro69");
-    public static final Id IN_THIRD_PERSON = new Id("usedtobepotus");
+    public static final Id ID_FIRST_PERSON = new Id("charliechua45");
+    public static final Id ID_SECOND_PERSON = new Id("bossbro69");
+    public static final Id ID_THIRD_PERSON = new Id("usedtobepotus");
 }
 
