71aa12e8d87889df547a7fa0f3cc71868689cc38
Cedricaca <>
2024-03-13 16:39:24 +0800

    Checkstyle

--- src/main/java/seedu/address/logic/parser/AddCategoryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCategoryCommandParser.java
@@ -9,7 +9,6 @@ import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddCategoryCommand;
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Entry;
 
