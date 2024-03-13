5a7e576699181432f16b8fe3dff7326ce190ce7b
Chua Zi Long <e0957913@u.nus.edu>
2024-03-13 16:29:11 +0800

    Fix code to match checkstyle requirements

--- src/main/java/seedu/address/logic/commands/FindAndExportCommand.java
+++ src/main/java/seedu/address/logic/commands/FindAndExportCommand.java
@@ -12,18 +12,31 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
+/**
+ * Represents a command to find users based on specified criteria and export their information.
+ * The command allows filtering of users by tags, name, and address, and exports the
+ * filtered list to a specified file.
+ */
 public class FindAndExportCommand extends Command {
 
     public static final String COMMAND_WORD = "find_and_export";
-    private final String tag;
-    private final String name;
-    private final String address;
-    private final String filename;
     public static final String MESSAGE_USAGE = "find_and_export: Exports the users filtered by a tag "
             + "and other optional parameters.\n"
             + "Parameters: TAG [n/NAME] [a/ADDRESS] [o/FILENAME]\n"
             + "Example: find_and_export cs2103t n/john a/olive street 42 o/output1";
+    private final String tag;
+    private final String name;
+    private final String address;
+    private final String filename;
 
+    /**
+     * Constructs a FindAndExportCommand to find and export users' information.
+     *
+     * @param tag The tag by which users are filtered.
+     * @param name The name substring by which users are further filtered. Can be {@code null}.
+     * @param address The address substring by which users are further filtered. Can be {@code null}.
+     * @param filename The name of the file to which the filtered users are exported.
+     */
     public FindAndExportCommand(String tag, String name, String address, String filename) {
         this.tag = tag;
         this.name = name;
@@ -60,7 +73,8 @@ public class FindAndExportCommand extends Command {
             predicate = predicate.and(person -> person.getName().fullName.toLowerCase().contains(name.toLowerCase()));
         }
         if (address != null && !address.isBlank()) {
-            predicate = predicate.and(person -> person.getAddress().value.toLowerCase().contains(address.toLowerCase()));
+            predicate = predicate.and(person -> person.getAddress().value.
+                    toLowerCase().contains(address.toLowerCase()));
         }
 
         return predicate;
