6a571cafbcab9f7eacf8eb0dfb66d8301c6313e0
xiaorui-ui <xiaoruizhang9876@gmail.com>
2024-03-13 23:27:38 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/DeleteLoanCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteLoanCommandParser.java
@@ -2,7 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.stream.Stream;
 
