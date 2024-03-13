97682c8501319d7d79184f824d69d032a43e21ca
lihaoquan <lihaoquan.dev@gmail.com>
2024-03-13 18:04:15 +0800

    Update NoteCommandTest.java

--- src/test/java/seedu/address/logic/commands/NoteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NoteCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
 import seedu.address.model.AddressBook;
