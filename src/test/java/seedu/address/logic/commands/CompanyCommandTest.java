50deaf50fe4a6960b08430689710aedb4c5c212a
yapxuanxuan <e0969213@u.nus.edu>
2024-03-14 00:05:23 +0800

    Update

--- src/test/java/seedu/address/logic/commands/CompanyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CompanyCommandTest.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
