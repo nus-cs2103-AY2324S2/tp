8ded7e9e5e257d69a1215871ce9d8da53b9c73ad
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-03-13 17:12:00 +0800

    Address test issue with list command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -19,7 +19,7 @@ public class ListCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        if (model.getFilteredPersonList().size() <= 0) {
+        if (model.getFilteredPersonList().size() == 0) {
             return new CommandResult(MESSAGE_EMPTY);
         }
         return new CommandResult(MESSAGE_SUCCESS);
--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -67,7 +67,7 @@ public class LogicManagerTest {
     @Test
     public void execute_validCommand_success() throws Exception {
         String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        assertCommandSuccess(listCommand, ListCommand.MESSAGE_EMPTY, model);
     }
 
     @Test
