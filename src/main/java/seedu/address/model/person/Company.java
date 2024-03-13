84bc76c02523572ecf9a37a39b7b7a308b94770b
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-03-13 16:42:36 +0800

    Add Company first commit

---
+++ src/main/java/seedu/address/model/person/Company.java
@@ -0,0 +1,67 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's name in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class Company {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Company should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String companyName;
+
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param name A valid company name.
+     */
+    public Company(String name) {
+        requireNonNull(name);
+        checkArgument(isValidCompanyName(name), MESSAGE_CONSTRAINTS);
+        companyName = name;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidCompanyName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return companyName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Company)) {
+            return false;
+        }
+
+        Company otherName = (Company) other;
+        return companyName.equals(otherName.companyName);
+    }
+
+    @Override
+    public int hashCode() {
+        return companyName.hashCode();
+    }
+
+}
