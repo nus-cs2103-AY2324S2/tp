5539181d1c7b6010513dadca8b96f2174ccd0f8d
danielleloh <szehan.loh@gmail.com>
2024-03-13 21:38:14 +0800

    feat: add export feature to export data in json file to a csv file
    users may want to export all the data of persons in the addressbook to
    a csv file for better compatibility with other applications, or other
    uses. csv file exported will be found in a file created named
    addressbookdata.
    
    changes made include,
    * added new class ExportCommand with deals with exporting information
    stored in the address book json file to a csv file
    * new dependency in gradle to write CSV file
    
    other information,
    * inputting export at any time will update the same csv file in
    addressbookdata. a new csv file will not be created everytime the
    export command is called. users can choose to move the file out of
    addressbookdata, if they choose to store current data.
    * all information will be exported, not a filtered part of the
    information

---
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -0,0 +1,80 @@
+package seedu.address.logic.commands;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.util.Objects.requireNonNull;
+
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
+
+/**
+ * Exports the information stored in the AddressBook to a CSV file.
+ */
+public class ExportCommand extends Command {
+
+    public static final String COMMAND_WORD = "export";
+    public static final String MESSAGE_SUCCESS = "Exported all person's information to CSV file. \n"
+            + "CSV file can be found in addressbookdata file.";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        Path jsonFilePath = model.getAddressBookFilePath();
+        File jsonFile = jsonFilePath.toFile();
+        String csvFilePath = "./addressbookdata/addressbook.csv";
+        File csvFile = new File(csvFilePath);
+
+        File csvParentDirectory = csvFile.getParentFile();
+        if (!csvParentDirectory.exists()) {
+            boolean isCreated = csvParentDirectory.mkdir();
+            if (!isCreated) {
+                throw new CommandException("Could not create directory for CSV file.");
+            }
+        }
+
+        try {
+            JsonNode jsonTree = new ObjectMapper().readTree(jsonFile);
+
+            JsonNode personsArray = jsonTree.get("persons");
+
+            Builder csvSchemaBuilder = CsvSchema.builder();
+            JsonNode firstObject = personsArray.elements().next();
+            firstObject.fieldNames().forEachRemaining(fieldName -> {
+                csvSchemaBuilder.addColumn(fieldName);
+            });
+            CsvSchema csvSchema = csvSchemaBuilder
+                    .build()
+                    .withHeader();
+
+            CsvMapper csvMapper = new CsvMapper();
+            csvMapper.writerFor(JsonNode.class)
+                    .with(csvSchema)
+                    .writeValue(csvFile, personsArray);
+
+            return new CommandResult(MESSAGE_SUCCESS);
+
+        } catch (IOException e) {
+            if (e instanceof FileNotFoundException) {
+                throw new CommandException("Cannot find JSON file to export.");
+            } else if (e instanceof JsonParseException) {
+                throw new CommandException("Error parsing JSON data.");
+            } else if (e instanceof JsonMappingException) {
+                throw new CommandException("Error mapping JSON data to CSV schema.");
+            } else {
+                throw new CommandException("Error: " + e.getMessage());
+            }
+        }
+    }
+}
