3a0107e4464d9ac345855591977a990d2aa386cd
Liu Jiayao <jiayao_liu@u.nus.edu>
2024-03-13 18:50:19 +0800

    Update Tests regarding delete command

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -28,7 +28,7 @@ public class DeleteCommandParserTest {
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, INVALID_ID_DESC, String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    public void parse_invalidArgs_throwsIdNotFoundException() {
+        assertParseFailure(parser, "INVALID ID", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
