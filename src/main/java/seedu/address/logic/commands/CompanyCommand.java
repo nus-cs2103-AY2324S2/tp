0690f2e9a388be5187d297a1dfcfe90ca030f537
yapxuanxuan <e0969213@u.nus.edu>
2024-03-14 00:03:39 +0800

    Minor update

--- src/main/java/seedu/address/logic/commands/CompanyCommand.java
+++ src/main/java/seedu/address/logic/commands/CompanyCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Company;
