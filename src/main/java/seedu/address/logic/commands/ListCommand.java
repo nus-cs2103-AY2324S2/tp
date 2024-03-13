e91a02e930ae0e6dab18b814658156079044839a
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-03-13 16:18:16 +0800

    Address style issue in code of List Command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -19,8 +19,8 @@ public class ListCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        if(model.getFilteredPersonList().size() <= 0){
-           return new CommandResult(MESSAGE_EMPTY);
+        if (model.getFilteredPersonList().size() <= 0) {
+            return new CommandResult(MESSAGE_EMPTY);
         }
         return new CommandResult(MESSAGE_SUCCESS);
     }
