2f3b74d0d3f32f5d70225deb77e43f7d545bf840
Kyaw Swar Myint <kyawsm0161@gmail.com>
2024-03-13 16:13:53 +0800

    Change output message for list command
    The list command displays "Listed all persons" for all outputs.
    
    It is not specific enough for the output list.
    
    Let's add the message "The list is empty" for empty lists instead.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -12,13 +12,16 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all contacts";
 
-
+    public static final String MESSAGE_EMPTY = "The list is empty";
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        if(model.getFilteredPersonList().size() <= 0){
+           return new CommandResult(MESSAGE_EMPTY);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
