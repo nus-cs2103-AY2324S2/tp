9094d5b92a731ae2da52c4c9944fc1ab124fb150
Emberlynn-Loo <112895879+Emberlynn-Loo@users.noreply.github.com>
2024-03-14 14:12:06 +0800

    Update RoleBasedRelationshipTest.java

--- src/test/java/seedu/address/model/person/relationship/RoleBasedRelationshipTest.java
+++ src/test/java/seedu/address/model/person/relationship/RoleBasedRelationshipTest.java
@@ -47,4 +47,22 @@ public class RoleBasedRelationshipTest {
 
         assertThrows(IllegalStateException.class, () -> relationship.addRole(person2, role3));
     }
+    
+    @Test
+    public void testGetAllRoles() {
+        UUID person1 = UUID.randomUUID();
+        UUID person2 = UUID.randomUUID();
+        RoleBasedRelationship relationship = new RoleBasedRelationship(person1, person2);
+        String role1 = "role1";
+        String role2 = "role2";
+
+        relationship.addRole(person1, role1);
+        relationship.addRole(person2, role2);
+
+        assertEquals(2, relationship.getAllRoles().size());
+        assertTrue(relationship.getAllRoles().containsKey(person1));
+        assertTrue(relationship.getAllRoles().containsKey(person2));
+        assertEquals(role1, relationship.getAllRoles().get(person1));
+        assertEquals(role2, relationship.getAllRoles().get(person2));
+    }
 }
