e2739b54e6b1aa80d2e265d7939f8ed810717736
Hong Ray <119933763+HongRay@users.noreply.github.com>
2024-03-14 18:28:31 +0800

    Added instructions
    Added instructions to explain how the code works.

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -21,6 +21,7 @@ import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new EditCommand object
+ * Testing
  */
 public class EditCommandParser implements Parser<EditCommand> {
 
