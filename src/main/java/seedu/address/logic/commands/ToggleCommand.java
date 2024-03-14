e7ba180a854f66ec556ef36738f9eadc3baa1330
ShanylOng <shanylong33@gmail.com>
2024-03-14 16:21:42 +0800

    Add ToggleCommand Class
    Added ToggleCommand class, and edited the cases in the AddressBookParser to reflect the new command.
    The Address Book currently responds to the input of the '$' command by showing displaying the 'Display toggled' message.
    Actual display toggling to be implemented.

---
+++ src/main/java/seedu/address/logic/commands/ToggleCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+/**
+ * Toggles the display (shows/hides depending on whether the display is already being displayed or not).
+ */
+public class ToggleCommand extends Command {
+
+    public static final String COMMAND_WORD = "$";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Toggles the display on/off. ";
+    public static final String MESSAGE_TOGGLE_ACKNOWLEDGEMENT = "Display toggled";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(MESSAGE_TOGGLE_ACKNOWLEDGEMENT);
+    }
+
+}
