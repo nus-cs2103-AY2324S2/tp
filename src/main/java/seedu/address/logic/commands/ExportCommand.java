a180360bd06e562ac77e7fb3ed37459a6b53a116
danielleloh <szehan.loh@gmail.com>
2024-03-14 11:33:01 +0800

    fix: improve SLAP of the ExportCommand class
    Avoiding long methods improves the readability of the code. This makes
    it easier for future developers to read, understand and modify the code
    in the future.
    
    break down execute method into different methods.

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -27,15 +27,13 @@ public class ExportCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Exported all person's information to CSV file. \n"
             + "CSV file can be found in addressbookdata file.";
 
-    @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-
-        Path jsonFilePath = model.getAddressBookFilePath();
-        File jsonFile = jsonFilePath.toFile();
-        String csvFilePath = "./addressbookdata/addressbook.csv";
-        File csvFile = new File(csvFilePath);
-
+    /**
+     * Creates the directory for the CSV file if it does not exist.
+     *
+     * @param csvFile The CSV file for which the directory needs to be created.
+     * @throws CommandException If directory fails to be created.
+     */
+    public void createCsvDirectory(File csvFile) throws CommandException {
         File csvParentDirectory = csvFile.getParentFile();
         if (!csvParentDirectory.exists()) {
             boolean isCreated = csvParentDirectory.mkdir();
@@ -43,28 +41,19 @@ public class ExportCommand extends Command {
                 throw new CommandException("Could not create directory for CSV file.");
             }
         }
+    }
 
+    /**
+     * Read the JSON file and returns the contents as a JSON tree.
+     *
+     * @param jsonFile The JSON file to be read and mapped to the CSV schema.
+     * @return The JSON tree representing the content of the JSON file.
+     * @throws CommandException If an error occurs while reading the JSON file and mapping it to the CSV schema.
+     */
+    public JsonNode readJsonFile(File jsonFile) throws CommandException {
         try {
             JsonNode jsonTree = new ObjectMapper().readTree(jsonFile);
-
-            JsonNode personsArray = jsonTree.get("persons");
-
-            Builder csvSchemaBuilder = CsvSchema.builder();
-            JsonNode firstObject = personsArray.elements().next();
-            firstObject.fieldNames().forEachRemaining(fieldName -> {
-                csvSchemaBuilder.addColumn(fieldName);
-            });
-            CsvSchema csvSchema = csvSchemaBuilder
-                    .build()
-                    .withHeader();
-
-            CsvMapper csvMapper = new CsvMapper();
-            csvMapper.writerFor(JsonNode.class)
-                    .with(csvSchema)
-                    .writeValue(csvFile, personsArray);
-
-            return new CommandResult(MESSAGE_SUCCESS);
-
+            return jsonTree;
         } catch (IOException e) {
             if (e instanceof FileNotFoundException) {
                 throw new CommandException("Cannot find JSON file to export.");
@@ -75,6 +64,78 @@ public class ExportCommand extends Command {
             } else {
                 throw new CommandException("Error: " + e.getMessage());
             }
+        }
+    }
+
+    /**
+     * Reads the persons array in the JSON file and returns the contents as a JSON tree.
+     *
+     * @param jsonTree The JSON tree where the persons array should be read from.
+     * @return The JSON tree representing the content of the persons array in the release file.
+     * @throws CommandException If the persons array in the JSON file is empty.
+     */
+    public JsonNode readPersonsArray(JsonNode jsonTree) throws CommandException {
+        JsonNode personsArray = jsonTree.get("persons");
+        if (personsArray == null || personsArray.size() == 0) {
+            throw new CommandException("The JSON File is empty.");
+        }
+        return personsArray;
+    }
+
+    /**
+     * Builds the CSV schema from the JSON array.
+     *
+     * @param jsonTree The JSON array to derive the schema from.
+     * @return The CSV schema.
+     */
+    public CsvSchema buildCsvSchema(JsonNode jsonTree) {
+        Builder csvSchemaBuilder = CsvSchema.builder();
+        JsonNode firstObject = jsonTree.elements().next();
+        firstObject.fieldNames().forEachRemaining(fieldName -> {
+            csvSchemaBuilder.addColumn(fieldName);
+        });
+        CsvSchema csvSchema = csvSchemaBuilder
+                .build()
+                .withHeader();
+        return csvSchema;
+    }
+
+    /**
+     * Writes JSON data to a CSV file.
+     *
+     * @param csvFile The CSV file to write to.
+     * @param jsonTree The JSON array to be written to CSV.
+     * @throws IOException If an I/O error occurs during file writing.
+     */
+    public void writeToCsvFile(File csvFile, JsonNode jsonTree) throws IOException {
+        CsvSchema csvSchema = buildCsvSchema(jsonTree);
+
+        CsvMapper csvMapper = new CsvMapper();
+        csvMapper.writerFor(JsonNode.class)
+                .with(csvSchema)
+                .writeValue(csvFile, jsonTree);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        Path jsonFilePath = model.getAddressBookFilePath();
+        File jsonFile = jsonFilePath.toFile();
+        String csvFilePath = "./addressbookdata/addressbook.csv";
+        File csvFile = new File(csvFilePath);
+
+        createCsvDirectory(csvFile);
+
+        try {
+            JsonNode jsonTree = readJsonFile(jsonFile);
+            JsonNode personsArray = readPersonsArray(jsonTree);
+            writeToCsvFile(csvFile, personsArray);
+
+            return new CommandResult(MESSAGE_SUCCESS);
+
+        } catch (IOException e) {
+                throw new CommandException("Error: " + e.getMessage());
         }
     }
 }
