a423c0c7f57efa0ea710b636ed8c49656511e62a
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-03-14 15:02:54 +0800

    Company in Contact

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -27,7 +27,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_COMPANY + "COMPANY] "
+            + PREFIX_COMPANY + "COMPANY "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "Benjamin Lee "
--- src/main/java/seedu/address/model/person/Company.java
+++ src/main/java/seedu/address/model/person/Company.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's name in the address book.
+ * Represents the Company that a Person represents in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidCompanyName(String)}
  */
 public class Company {
