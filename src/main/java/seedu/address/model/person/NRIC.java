aa4cc314c5b7762ed9914d61ed63a4fc08a0c86e
laney0808 <e0969207@u.nus.edu>
2024-03-14 10:54:53 +0800

    Add class NRIC

---
+++ src/main/java/seedu/address/model/person/NRIC.java
@@ -0,0 +1,62 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class NRIC {
+    public static final String MESSAGE_CONSTRAINTS =
+            "NRIC number should contain a prefix of S or T, followed by 7 digits, and end with a letter. "
+                    + "There should not be blank.";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "^[ST]\\\\d{7}[A-Z]$";
+
+    public final String NRIC;
+
+    /**
+     * Constructs a {@code NRIC}.
+     *
+     * @param name A valid NRIC.
+     */
+    public NRIC(String name) {
+        requireNonNull(name);
+        checkArgument(isValidNric(name), MESSAGE_CONSTRAINTS);
+        this.NRIC = name;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidNric(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return this.NRIC;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NRIC)) {
+            return false;
+        }
+
+        Name otherName = (Name) other;
+        return NRIC.equals(otherName.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return NRIC.hashCode();
+    }
+}
