5f9c9d4f1f988f9ff3b530e704c273add5072b32
narwhalsilent <110954060+narwhalsilent@users.noreply.github.com>
2024-03-14 10:12:03 +0800

    Minor formatting change

--- src/main/java/seedu/address/ui/LoanCard.java
+++ src/main/java/seedu/address/ui/LoanCard.java
@@ -2,18 +2,19 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Loan;
-
-import java.util.Comparator;
 
 /**
  * An UI component that displays information of a {@code Loan}.
  */
 public class LoanCard extends UiPart<Region> {
     private static final String FXML = "LoanListCard.fxml";
+    private static final String DEFAULT_LOAN_PREFIX = "Loan No. ";
+    private static final String DEFAULT_AMOUNT_PREFIX = "Amount: ";
+    private static final String DEFAULT_START_DATE_PREFIX = "Start Date: ";
+    private static final String DEFAULT_END_DATE_PREFIX = "End Date: ";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -24,7 +25,6 @@ public class LoanCard extends UiPart<Region> {
      */
 
     public final Loan loan;
-    private static final String DEFAULT_LOAN_PREFIX = "Loan No. ";
 
     @javafx.fxml.FXML
     private HBox cardPane;
@@ -44,8 +44,8 @@ public class LoanCard extends UiPart<Region> {
         super(FXML);
         this.loan = loan;
         name.setText(DEFAULT_LOAN_PREFIX + displayedIndex);
-        amount.setText(String.valueOf(loan.getAmount()));
-        startDate.setText(loan.getStartDate().toString());
-        endDate.setText(loan.getEndDate().toString());
+        amount.setText(DEFAULT_AMOUNT_PREFIX + String.valueOf(loan.getValue()));
+        startDate.setText(DEFAULT_START_DATE_PREFIX + loan.getStartDate().toString());
+        endDate.setText(DEFAULT_END_DATE_PREFIX + loan.getReturnDate().toString());
     }
 }
