bad78554d771ebf413fe0655b876f0f43a7d112c
Emberlynn-Loo <E0960284@u.nus.edu>
2024-03-14 12:12:14 +0800

    Add a relationship package containing relationship classes

---
+++ src/main/java/seedu/address/model/person/relationship/RelationshipManager.java
@@ -0,0 +1,24 @@
+package seedu.address.model.person.relationship;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RelationshipManager {
+    private Map<String, List<Relationship>> relationships; //overall hashmap where string is the relationship type
+
+    public RelationshipManager() {
+        relationships = new HashMap<>();
+    }
+
+    public void addRelationship(String relationshipType, Relationship relationship) {
+        relationships.computeIfAbsent(relationshipType, k -> new ArrayList<>()).add(relationship);
+    }
+
+    // Get relationships by type
+    public List<Relationship> getRelationships(String relationshipType) {
+        return relationships.getOrDefault(relationshipType, Collections.emptyList());
+    }
+}
---
+++ src/main/java/seedu/address/model/person/relationship/RoleBasedRelationship.java
@@ -0,0 +1,25 @@
+package seedu.address.model.person.relationship;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+class RoleBasedRelationship extends Relationship {
+    private Map<UUID, String> roles; // Map of person UUID to their role in the relationship
+
+    public RoleBasedRelationship(UUID person1, UUID person2) {
+        super(person1, person2);
+        roles = new HashMap<>();
+    }
+
+    // Add role for a person
+    public void addRole(UUID personUUID, String role) {
+        roles.put(personUUID, role);
+    }
+
+    // Get role for a person
+    public String getRole(UUID personUUID) {
+        return roles.get(personUUID);
+    }
+}
+
