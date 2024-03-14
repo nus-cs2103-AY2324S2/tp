7bfd69770c4f9f73c69288624dee76b351f7f094
kokerinks <limkokliang01@gmail.com>
2024-03-15 01:14:30 +0800

    Fix checkstyle errors for ClearCommandParserTest

--- src/test/java/seedu/address/logic/parser/ClearCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ClearCommandParserTest.java
@@ -5,36 +5,34 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.parser.ClearCommandParser;
-
 
 public class ClearCommandParserTest {
-  private ClearCommandParser parser = new ClearCommandParser();
+    private ClearCommandParser parser = new ClearCommandParser();
 
-  @Test
-  public void parse_allFieldsPresent_success() {
-    assertParseSuccess(parser, " --force", new ClearCommand(true));
-  }
+    @Test
+    public void parse_allFieldsPresent_success() {
+        assertParseSuccess(parser, " --force", new ClearCommand(true));
+    }
 
-  @Test
-  public void parse_noFieldsPresent_success() {
-    assertParseSuccess(parser, "", new ClearCommand(false));
-  }
+    @Test
+    public void parse_noFieldsPresent_success() {
+        assertParseSuccess(parser, "", new ClearCommand(false));
+    }
 
-  @Test
-  public void parse_invalidFieldsPresent_success() {
-    assertParseSuccess(parser, " --force --force", new ClearCommand(true));
-  }
+    @Test
+    public void parse_invalidFieldsPresent_success() {
+        assertParseSuccess(parser, " --force --force", new ClearCommand(true));
+    }
 
-  @Test
-  public void parse_invalidFieldsPresent_success2() {
-    assertParseSuccess(parser, " -force", new ClearCommand(false));
-  }
+    @Test
+    public void parse_invalidFieldsPresent_success2() {
+        assertParseSuccess(parser, " -force", new ClearCommand(false));
+    }
 
-  @Test
-  public void parse_invalidFieldsPresent_success3() {
-    assertParseSuccess(parser, " -f", new ClearCommand(false));
-  }
+    @Test
+    public void parse_invalidFieldsPresent_success3() {
+        assertParseSuccess(parser, " -f", new ClearCommand(false));
+    }
 
-  
+
 }
