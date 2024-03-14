5ae114acaa7afbdd42fac2688ccd0c987d91e4c6
Wongqingyee <qingyeewong@gmail.com>
2024-03-14 15:10:11 +0800

    Update the checkstyle

--- src/main/java/seedu/address/model/person/StudentId.java
+++ src/main/java/seedu/address/model/person/StudentId.java
@@ -3,21 +3,34 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents a Person's studentId in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidStudentId(String)}
+ */
 public class StudentId {
 
     public static final String MESSAGE_CONSTRAINTS =
             "StudentId should start with A follow by 7 digits and ends with a letter, and it should not be blank.";
 
+    // The studentId must start with A followed by digits and end with a letter.
     public static final String VALIDATION_REGEX = "[\\p{Alnum}]+";
 
     public final String ID;
 
+    /**
+     * Constructs a {@code StudentId}.
+     *
+     * @param studentId A valid studentId.
+     */
     public StudentId(String studentId) {
         requireNonNull(studentId);
         checkArgument(isValidStudentId(studentId), MESSAGE_CONSTRAINTS);
         ID = studentId;
     }
 
+    /**
+     * Returns true if a given string is a valid studentId.
+     */
     public static boolean isValidStudentId(String test) {
         return test.matches(VALIDATION_REGEX);
     }
