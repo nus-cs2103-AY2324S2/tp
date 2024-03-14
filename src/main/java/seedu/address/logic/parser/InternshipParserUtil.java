1715ca6e90278e73079d47b67caa74ceecd704a2
LWS49 <e0970244@u.nus.edu>
2024-03-14 17:15:03 +0800

    add InternshipParserUtil

---
+++ src/main/java/seedu/address/logic/parser/InternshipParserUtil.java
@@ -0,0 +1,157 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.internship.ApplicationStatus;
+import seedu.address.model.internship.CompanyName;
+import seedu.address.model.internship.ContactEmail;
+import seedu.address.model.internship.ContactName;
+import seedu.address.model.internship.ContactNumber;
+import seedu.address.model.internship.Description;
+import seedu.address.model.internship.Location;
+import seedu.address.model.internship.Role;
+
+/**
+ * Contains utility methods used for parsing strings in the various *Parser classes.
+ */
+public class InternshipParserUtil {
+
+    public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
+
+    /**
+     * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
+     * trimmed.
+     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
+     */
+    public static Index parseIndex(String oneBasedIndex) throws ParseException {
+        String trimmedIndex = oneBasedIndex.trim();
+        if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
+            throw new ParseException(MESSAGE_INVALID_INDEX);
+        }
+        return Index.fromOneBased(Integer.parseInt(trimmedIndex));
+    }
+
+    /**
+     * Parses a {@code String companyName} into a {@code CompanyName}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code name} is invalid.
+     */
+    public static CompanyName parseCompanyName(String companyName) throws ParseException {
+        requireNonNull(companyName);
+        String trimmedName = companyName.trim();
+        if (!CompanyName.isValidCompanyName(trimmedName)) {
+            throw new ParseException(CompanyName.MESSAGE_CONSTRAINTS);
+        }
+        return new CompanyName(trimmedName);
+    }
+
+    /**
+     * Parses a {@code String location} into a {@code Location}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code location} is invalid.
+     */
+    public static Location parseLocation(String location) throws ParseException {
+        requireNonNull(location);
+        String trimmedLocation = location.trim();
+        if (!Location.isValidLocation(trimmedLocation)) {
+            throw new ParseException(Location.MESSAGE_CONSTRAINTS);
+        }
+        return new Location(trimmedLocation);
+    }
+
+    /**
+     * Parses a {@code String description} into a {@code Description}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code description} is invalid.
+     */
+    public static Description parseDescription(String description) throws ParseException {
+        requireNonNull(description);
+        String trimmedDescription = description.trim();
+        if (!Description.isValidDescription(trimmedDescription)) {
+            throw new ParseException(Description.MESSAGE_CONSTRAINTS);
+        }
+        return new Description(trimmedDescription);
+
+    }
+
+    /**
+     * Parses a {@code String role} into a {@code Role}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code role} is invalid.
+     */
+    public static Role parseRole(String role) throws ParseException {
+        requireNonNull(role);
+        String trimmedRole = role.trim();
+        if (!Role.isValidRole(trimmedRole)) {
+            throw new ParseException(Role.MESSAGE_CONSTRAINTS);
+        }
+        return new Role(trimmedRole);
+    }
+
+    /**
+     * Parses a {@code String contactName} into a {@code ContactName}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code contactName} is invalid.
+     */
+    public static ContactName parseContactName(String contactName) throws ParseException {
+        requireNonNull(contactName);
+        String trimmedContactName = contactName.trim();
+        if (!ContactName.isValidContactName(trimmedContactName)) {
+            throw new ParseException(ContactName.MESSAGE_CONSTRAINTS);
+        }
+        return new ContactName(trimmedContactName);
+    }
+
+    /**
+     * Parses a {@code String contactNumber} into a {@code ContactNumber}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code contactNumber} is invalid.
+     */
+    public static ContactNumber parseContactNumber(String contactNumber) throws ParseException {
+        requireNonNull(contactNumber);
+        String trimmedContactNumber = contactNumber.trim();
+        if (!ContactNumber.isValidContactNumber(trimmedContactNumber)) {
+            throw new ParseException(ContactNumber.MESSAGE_CONSTRAINTS);
+        }
+        return new ContactNumber(trimmedContactNumber);
+    }
+
+    /**
+     * Parses a {@code String contactEmail} into a {@code ContactEmail}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code contactEmail} is invalid.
+     */
+    public static ContactEmail parseContactEmail(String contactEmail) throws ParseException {
+        requireNonNull(contactEmail);
+        String trimmedContactEmail = contactEmail.trim();
+        if (!ContactEmail.isValidEmail(trimmedContactEmail)) {
+            throw new ParseException(ContactEmail.MESSAGE_CONSTRAINTS);
+        }
+        return new ContactEmail(trimmedContactEmail);
+    }
+
+    /**
+     * Parses a {@code String status} into a {@code ApplicationStatus}.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @throws ParseException if the given {@code status} is invalid.
+     */
+    public static ApplicationStatus parseStatus(String status) throws ParseException {
+        requireNonNull(status);
+        String trimmedStatus = status.trim();
+        if (!ApplicationStatus.isValidApplicationStatus(trimmedStatus)) {
+            throw new ParseException(ApplicationStatus.MESSAGE_CONSTRAINTS);
+        }
+        return new ApplicationStatus(trimmedStatus);
+    }
+}
