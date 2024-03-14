3845bf8fe9e58455c40f5dc94d761bde8be42d6e
DESKTOP-CP7BI1I\Eujin <an.eujin.12@gmail.com>
2024-03-14 19:14:15 +0800

    Add new parameters and change user command input to use backslash instead

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,13 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_NAME = new Prefix("n\\");
+    public static final Prefix PREFIX_PHONE = new Prefix("p\\");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e\\");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("a\\");
+    public static final Prefix PREFIX_TAG = new Prefix("t\\");
+    public static final Prefix PREFIX_DOB = new Prefix("dob\\");
+    public static final Prefix PREFIX_WARD_NO = new Prefix("w\\");
+    public static final Prefix PREFIX_IC = new Prefix("ic\\");
+    public static final Prefix PREFIX_ADMISSION_DATE = new Prefix("ad\\");
 }
