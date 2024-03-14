e58b4cb302a2ab4d608c2a4bf45b062d1e25e818
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-03-14 11:20:18 +0800

    Fix JavaDoc whitespace typo

--- src/main/java/seedu/address/model/student/UniqueStudentList.java
+++ src/main/java/seedu/address/model/student/UniqueStudentList.java
@@ -14,9 +14,9 @@ import seedu.address.model.student.exceptions.StudentNotFoundException;
 /**
  * A list of students that enforces uniqueness between its elements and does not allow nulls.
  * A student is considered unique by comparing using {@code Student#isSameStudent(Student)}.
- * As such, adding and updating of students uses Student#isSameStudent(Student) 
+ * As such, adding and updating of students uses Student#isSameStudent(Student)
  * for equality so as to ensure that the student being added or updated is
- * unique in terms of identity in the UniqueStudentList. 
+ * unique in terms of identity in the UniqueStudentList.
  * However, the removal of a student uses Student#equals(Object) so
  * as to ensure that the student with exactly the same fields will be removed.
  *
