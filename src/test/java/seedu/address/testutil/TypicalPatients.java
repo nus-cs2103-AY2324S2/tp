bda95b252b702db8972bdfe488fa5eed333dc75f
SherisseTJW <63995220+SherisseTJW@users.noreply.github.com>
2024-03-15 02:53:55 +0800

    fix: Build errors from checkstyle

--- src/test/java/seedu/address/testutil/TypicalPatients.java
+++ src/test/java/seedu/address/testutil/TypicalPatients.java
@@ -1,13 +1,25 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_IMPORTANT_DATE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_IMPORTANT_DATETIME;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_IMPORTANT_DATE_NAME;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.patient.Patient;
-
-import static seedu.address.logic.commands.CommandTestUtil.*;
 
 
 /**
