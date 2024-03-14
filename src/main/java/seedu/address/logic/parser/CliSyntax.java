575353b8d7e1048ee05f3a8af3d94b0d358d5acb
jmsandiegoo <jmsandiego0710@gmail.com>
2024-03-14 13:11:20 +0800

    Add new prefix with extra patient attributes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,6 +10,9 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_NRIC = new Prefix("ic/");
+    public static final Prefix PREFIX_GENDER = new Prefix("g/");
+    public static final Prefix PREFIX_BIRTHDATE = new Prefix("b/");
+    public static final Prefix PREFIX_ILLNESS = new Prefix("i/");
+    public static final Prefix PREFIX_DRUG_ALLERGIES = new Prefix("d/");
 }
