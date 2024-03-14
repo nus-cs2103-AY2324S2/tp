58c2459f4c867b6510dc8227f774d0e272e98436
kokerinks <limkokliang01@gmail.com>
2024-03-15 01:48:11 +0800

    Update SeedDataCommand and JUnit tests to use SampleDataUtil

--- src/main/java/seedu/address/logic/commands/SeedDataCommand.java
+++ src/main/java/seedu/address/logic/commands/SeedDataCommand.java
@@ -2,18 +2,11 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
 
 /**
  * Seed the address book with sample data.
@@ -22,50 +15,9 @@ public class SeedDataCommand extends Command {
     public static final String COMMAND_WORD = "seedData";
     public static final String MESSAGE_SUCCESS = "Added new members from seed data into the address book successfully!";
     public static final String MESSAGE_FAILURE = "Every member from seed data already exist in the address book!";
-    private static final Person[] sampleData = {
-        new Person(
-                new Name("Alice Yeoh"),
-                new Phone("87438807"),
-                new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                new HashSet<>(Set.of(new Tag("friends")))
-        ),
-        new Person(
-                new Name("Bernice Yu"),
-                new Phone("99272758"),
-                new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                new HashSet<>(Set.of(new Tag("colleagues"), new Tag("friends")))
-        ),
-        new Person(
-                new Name("Charlotte Oliveiro"),
-                new Phone("93210283"),
-                new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                new HashSet<>(Set.of(new Tag("neighbours")))
-        ),
-        new Person(
-                new Name("David Li"),
-                new Phone("91031282"),
-                new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                new HashSet<>(Set.of(new Tag("family")))
-        ),
-        new Person(
-                new Name("Irfan Ibrahim"),
-                new Phone("92492021"),
-                new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                new HashSet<>(Set.of(new Tag("classmates")))
-        ),
-        new Person(
-                new Name("Roy Balakrishnan"),
-                new Phone("92624417"),
-                new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                new HashSet<>(Set.of(new Tag("colleagues")))
-        )
-    };
+
+    private static final Person[] sampleData = SampleDataUtil.getSamplePersons();
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
--- src/test/java/seedu/address/logic/commands/SeedDataCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SeedDataCommandTest.java
@@ -15,62 +15,12 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
 
 public class SeedDataCommandTest {
 
-    public static final Person ALICE =
-        new Person(
-                new Name("Alice Yeoh"),
-                new Phone("87438807"),
-                new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                new HashSet<>(Set.of(new Tag("friends")))
-        );
+    private static final Person[] sampleData = SampleDataUtil.getSamplePersons();
 
-    public static final Person BERNICE =
-        new Person(
-            new Name("Bernice Yu"),
-            new Phone("99272758"),
-            new Email("berniceyu@example.com"),
-            new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-            new HashSet<>(Set.of(new Tag("colleagues"), new Tag("friends")))
-        );
-
-    public static final Person CHARLOTTE =
-        new Person(
-            new Name("Charlotte Oliveiro"),
-            new Phone("93210283"),
-            new Email("charlotte@example.com"),
-            new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-            new HashSet<>(Set.of(new Tag("neighbours")))
-        );
-
-    public static final Person DAVID =
-        new Person(
-            new Name("David Li"),
-            new Phone("91031282"),
-            new Email("lidavid@example.com"),
-            new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-            new HashSet<>(Set.of(new Tag("family")))
-        );
-
-    public static final Person IRFAN =
-        new Person(
-            new Name("Irfan Ibrahim"),
-            new Phone("92492021"),
-            new Email("irfan@example.com"),
-            new Address("Blk 47 Tampines Street 20, #17-35"),
-            new HashSet<>(Set.of(new Tag("classmates")))
-        );
-
-    public static final Person ROY =
-        new Person(
-            new Name("Roy Balakrishnan"),
-            new Phone("92624417"),
-            new Email("royb@example.com"),
-            new Address("Blk 45 Aljunied Street 85, #11-31"),
-            new HashSet<>(Set.of(new Tag("colleagues")))
-        );
     @Test
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
@@ -82,9 +32,9 @@ public class SeedDataCommandTest {
     @Test
     public void execute_addressBookWithSomeContacts_success() {
         Model model = new ModelManager();
-        model.addPerson(ALICE);
-        model.addPerson(BERNICE);
-        model.addPerson(DAVID);
+        model.addPerson(sampleData[0]);
+        model.addPerson(sampleData[1]);
+        model.addPerson(sampleData[3]);
         Model expectedModel = model;
 
         assertCommandSuccess(new SeedDataCommand(), model, SeedDataCommand.MESSAGE_SUCCESS, expectedModel);
@@ -93,12 +43,9 @@ public class SeedDataCommandTest {
     @Test
     public void execute_addressBookWithAllContacts_failure() {
         Model model = new ModelManager();
-        model.addPerson(ALICE);
-        model.addPerson(BERNICE);
-        model.addPerson(CHARLOTTE);
-        model.addPerson(DAVID);
-        model.addPerson(IRFAN);
-        model.addPerson(ROY);
+        for (Person person: sampleData) {
+            model.addPerson(person);
+        }
         Model expectedModel = model;
 
         assertCommandSuccess(new SeedDataCommand(), model, SeedDataCommand.MESSAGE_FAILURE, expectedModel);
