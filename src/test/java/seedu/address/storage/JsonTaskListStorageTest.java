74c69c4395b34ba9b5b05bc904cabde1ccb69a14
Kaya3842 <haichiendhnn1@gmail.com>
2024-03-14 18:18:19 +0800

    Add more tests
    Add more tests for more methods.

--- src/test/java/seedu/address/storage/JsonTaskListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonTaskListStorageTest.java
@@ -1,6 +1,7 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
@@ -9,14 +10,20 @@ import java.nio.file.Paths;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.model.TaskList;
 import seedu.address.model.task.Task;
+import seedu.address.testutil.Assert;
 
 public class JsonTaskListStorageTest {
 
     private static TaskList taskList = new TaskList();
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonTaskListStorageTest");
+
+    @TempDir
+    public Path testFolder;
+
     @Test
     public void readTaskList_missingFile_emptyResult() throws Exception {
         assertFalse(readTaskList("NonExistentFile.json").isPresent());
@@ -34,11 +41,6 @@ public class JsonTaskListStorageTest {
     private Optional<TaskList> readTaskList(String filePath) throws Exception {
         return new JsonTaskListStorage(Paths.get(filePath)).readTaskList(addToTestDataPathIfNotNull(filePath));
     }
-
-    private Optional<TaskList> readTaskList(Path filePath) throws Exception {
-        return new JsonTaskListStorage(filePath).readTaskList(filePath);
-    }
-
     private void saveTaskList(TaskList taskList, String filePath) throws IOException {
         try {
             new JsonTaskListStorage(Paths.get(filePath)).saveTaskList(taskList, addToTestDataPathIfNotNull(filePath));
@@ -47,12 +49,20 @@ public class JsonTaskListStorageTest {
         }
     }
 
-    private void saveTaskList(TaskList taskList, Path filePath) throws IOException {
-        try {
-            new JsonTaskListStorage(filePath).saveTaskList(taskList, filePath);
-        } catch (IOException ioe) {
-            throw new AssertionError("There should not be an error writing to the file.", ioe);
-        }
+    @Test
+    public void readAndSaveTaskList_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempTaskList.json");
+        TaskList sample = getTypicalTaskList();
+        JsonTaskListStorage jsonTaskListStorage = new JsonTaskListStorage(filePath);
+
+        // Save in new file and read back
+        jsonTaskListStorage.saveTaskList(sample, filePath);
+        TaskList readBack = jsonTaskListStorage.readTaskList(filePath).get();
+        assertNotNull(readBack);
+    }
+    @Test
+    public void saveTaskList_nullFilePath_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> saveTaskList(new TaskList(), null));
     }
 
     private static TaskList getTypicalTaskList() {
@@ -61,4 +71,6 @@ public class JsonTaskListStorageTest {
         taskList.addTask(new Task("test 3"));
         return taskList;
     }
+
 }
+
