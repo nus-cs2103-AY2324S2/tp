f5e223453a3fe5deafd42645257657b2b720142a
fy17ohhh <hester03sfy@gmail.com>
2024-03-14 19:24:34 +0800

    Add lines to improve readability of invalid add command

--- src/main/java/educonnect/logic/Messages.java
+++ src/main/java/educonnect/logic/Messages.java
@@ -13,7 +13,7 @@ import educonnect.model.student.Student;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n\n%1$s";
     public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student index provided is invalid";
     public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
