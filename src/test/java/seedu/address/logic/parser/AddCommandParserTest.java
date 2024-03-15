5d3886a955f0cfd88a79b2e9a08a88c2459f0420
larainezo <larainelwx@gmail.com>
2024-03-15 22:58:53 +0800

    Update fields

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -37,7 +37,6 @@ import static seedu.address.testutil.TypicalPersons.BOB;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.model.person.Birthday;
 import seedu.address.model.person.Email;
