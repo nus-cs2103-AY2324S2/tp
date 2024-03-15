6eb24a0d98d4f34a29be99913e7d81c2424a7cd3
Tejas Garrepally <code@gtejas.com>
2024-03-16 01:00:31 +0800

    Fix broken test case

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -69,7 +69,9 @@ public class LogicManagerTest {
     @Test
     public void execute_validCommand_success() throws Exception {
         String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String expectedMessage =
+                String.format(ListCommand.MESSAGE_LIST_ALL_SUCCESS, model.getFilteredPersonList().size());
+        assertCommandSuccess(listCommand, expectedMessage, model);
     }
 
     @Test
