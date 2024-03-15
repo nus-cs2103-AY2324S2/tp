570ca41deea98544d2e0149adcf06d7f72f08aa6
DESKTOP-CP7BI1I\Eujin <an.eujin.12@gmail.com>
2024-03-15 12:17:20 +0800

    Change prefix for ward number

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a\\");
     public static final Prefix PREFIX_TAG = new Prefix("t\\");
     public static final Prefix PREFIX_DOB = new Prefix("dob\\");
-    public static final Prefix PREFIX_WARD_NO = new Prefix("w\\");
+    public static final Prefix PREFIX_WARD = new Prefix("w\\");
     public static final Prefix PREFIX_IC = new Prefix("ic\\");
     public static final Prefix PREFIX_ADMISSION_DATE = new Prefix("ad\\");
 }
