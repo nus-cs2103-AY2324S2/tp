bbc733cec93304258e3f83627617dd2d4924985e
Wongqingyee <122243884+Wongqingyee@users.noreply.github.com>
2024-03-16 01:50:21 +0800

    Update checkstyle in JsonAdaptedPersonTest.java

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -56,8 +56,8 @@ public class JsonAdaptedPersonTest {
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedPerson person = 
-               new JsonAdaptedPerson(null, VALID_STUDENTID, VALID_PHONE, VALID_EMAIL,
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(null, VALID_STUDENTID, VALID_PHONE, VALID_EMAIL,
                        VALID_ADDRESS, VALID_GRADE, VALID_TAGS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
@@ -130,7 +130,7 @@ public class JsonAdaptedPersonTest {
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName());
         assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
     }
-  
+    
     @Test
     public void toModelType_invalidGrade_throwsIllegalValueException() {
         JsonAdaptedPerson person =
