b4cd1506e5b97dd2e3b343f8ed754d4119180d3f
tsulim <limxiujia18@gmail.com>
2024-03-15 10:51:58 +0800

    Fix error in LogicManagerTest

--- src/test/java/staffconnect/logic/LogicManagerTest.java
+++ src/test/java/staffconnect/logic/LogicManagerTest.java
@@ -168,7 +168,7 @@ public class LogicManagerTest {
         // Triggers the saveStaffBook method by executing an add command
         String addCommand = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY
                 + EMAIL_DESC_AMY + VENUE_DESC_AMY + MODULE_DESC_AMY;
-        Person expectedPerson = new PersonBuilder(AMY).withTags().build();
+        Person expectedPerson = new PersonBuilder(AMY).withTags().withAvailabilities().build();
         ModelManager expectedModel = new ModelManager();
         expectedModel.addPerson(expectedPerson);
         assertCommandFailure(addCommand, CommandException.class, expectedMessage, expectedModel);
