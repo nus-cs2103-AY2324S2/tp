de6bbd498457849ae8225473f953033e583b0538
yunchao <tanyunchao@gmail.com>
2024-03-15 16:42:15 +0800

    Update UI components

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -45,6 +45,8 @@ public class Messages {
                 .append(person.getAddress())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
+        builder.append(person.getUpcoming())
+                .append("; Upcoming: ");
         return builder.toString();
     }
 
