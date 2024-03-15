53c5d72e35a384c6449afddb14570c66adf25f1a
iamtr <jettzeong@gmail.com>
2024-03-15 15:02:04 +0800

    Change dob to contain String instead of LocalDate

--- src/main/java/seedu/address/model/person/Dob.java
+++ src/main/java/seedu/address/model/person/Dob.java
@@ -1,5 +1,8 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 import java.time.LocalDate;
 
 /**
@@ -7,21 +10,28 @@ import java.time.LocalDate;
  */
 public class Dob {
     public static final String MESSAGE_CONSTRAINTS = "Dates of birth takes in a date";
+    public static final String VALIDATION_REGEX = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}$";
 
-    public final LocalDate dob;
+    public final String dob;
 
     /**
      * Constructs a {@code Dob}.
      *
      * @param dob A valid date of birth.
      */
-    public Dob(LocalDate dob) {
+    public Dob(String dob) {
+        requireNonNull(dob);
+        checkArgument(isValidDob(dob), MESSAGE_CONSTRAINTS);
         this.dob = dob;
+    }
+
+    public static boolean isValidDob(String dob) {
+        return dob.matches(VALIDATION_REGEX);
     }
 
     @Override
     public String toString() {
-        return dob.toString();
+        return dob;
     }
 
     @Override
@@ -44,4 +54,5 @@ public class Dob {
     }
 
     // Todo: isValidDob
+
 }
