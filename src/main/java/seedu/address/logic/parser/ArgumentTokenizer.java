2c9f60fb73c7567cf03ed3fc663be041819dcd28
tzaph <maximilliano.utomo@gmail.com>
2024-03-15 12:49:28 +0800

    Change the prefix format

--- src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
+++ src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
@@ -7,10 +7,10 @@ import java.util.stream.Collectors;
 
 /**
  * Tokenizes arguments string of the form: {@code preamble <prefix>value <prefix>value ...}<br>
- *     e.g. {@code some preamble text t/ 11.00 t/12.00 k/ m/ July}  where prefixes are {@code t/ k/ m/}.<br>
- * 1. An argument's value can be an empty string e.g. the value of {@code k/} in the above example.<br>
+ *     e.g. {@code some preamble text -t 11.00 -t 12.00 -k -m July}  where prefixes are {@code -t -k -m}.<br>
+ * 1. An argument's value can be an empty string e.g. the value of {@code -k} in the above example.<br>
  * 2. Leading and trailing whitespaces of an argument value will be discarded.<br>
- * 3. An argument may be repeated and all its values will be accumulated e.g. the value of {@code t/}
+ * 3. An argument may be repeated and all its values will be accumulated e.g. the value of {@code -t}
  *    in the above example.<br>
  */
 public class ArgumentTokenizer {
