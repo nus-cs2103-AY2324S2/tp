2959dd0daadce502d9fe452cfa6a0e948d7ef306
Felix Chan You Yuan <111300022+felixchanyy@users.noreply.github.com>
2024-03-15 12:48:37 +0800

    Fix ParserUtilTest.java lexicographical order

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -14,9 +14,9 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.house.UnitNumber;
 import seedu.address.model.house.Block;
 import seedu.address.model.house.PostalCode;
+import seedu.address.model.house.UnitNumber;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
