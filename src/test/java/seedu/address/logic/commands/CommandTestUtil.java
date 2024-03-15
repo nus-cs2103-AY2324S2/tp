2083455c7920c58b6ac79bb43c171d70d1524e30
yunchao <tanyunchao@gmail.com>
2024-03-15 16:50:02 +0800

    Update logic manager test and command test util

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -50,6 +50,8 @@ public class CommandTestUtil {
     public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
+    public static final String UPCOMING_DESC_AMY = " " + PREFIX_UPCOMING + VALID_UPCOMING_AMY;
+    public static final String UPCOMING_DESC_BOB = " " + PREFIX_UPCOMING + VALID_UPCOMING_BOB;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
