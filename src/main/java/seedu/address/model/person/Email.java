102f59c4300239db69c12470c53b125b9ca7a36a
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-03-15 22:09:15 +0800

    Refactor email attribute

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -7,7 +7,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email extends Attribute<String> {
 
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
@@ -31,17 +31,15 @@ public class Email {
     private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
-    public final String value;
-
     /**
      * Constructs an {@code Email}.
      *
      * @param email A valid email address.
      */
     public Email(String email) {
+        super(email);
         requireNonNull(email);
         checkArgument(isValidEmail(email), MESSAGE_CONSTRAINTS);
-        value = email;
     }
 
     /**
@@ -51,9 +49,22 @@ public class Email {
         return test.matches(VALIDATION_REGEX);
     }
 
+    /**
+     * Determine if the email value stored is a match with a specified email string.
+     * Returns true if specified value is a substring of the email value stored.
+     *
+     * @param otherValue Other value to check against
+     *
+     * @return True if specified value is a match, False otherwise
+     */
+    @Override
+    public boolean isMatch(String otherValue) {
+        return this.getValue().toLowerCase().contains(otherValue.toLowerCase());
+    }
+
     @Override
     public String toString() {
-        return value;
+        return super.getValue();
     }
 
     @Override
@@ -68,12 +79,11 @@ public class Email {
         }
 
         Email otherEmail = (Email) other;
-        return value.equals(otherEmail.value);
+        return this.getValue().equals(otherEmail.getValue());
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return this.getValue().hashCode();
     }
-
 }
