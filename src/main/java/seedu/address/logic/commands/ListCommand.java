cb2ea45ff61bb1601a7d425cf165aff9c3a2d174
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-03-15 14:14:25 +0800

    Update src/main/java/seedu/address/logic/commands/ListCommand.java
    Co-authored-by: tankh99 <56617918+tankh99@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -25,7 +25,7 @@ public class ListCommand extends Command {
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + "[" + PREFIX_ALIAS + "TAG]...\n"
+            + "[" + PREFIX_ALIAS + "ALIAS]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
