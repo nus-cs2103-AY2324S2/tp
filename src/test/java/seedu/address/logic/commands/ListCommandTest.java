cd7866f3b91c6c96bcbef547a3391c31fc82b491
Tejas Garrepally <code@gtejas.com>
2024-03-16 01:00:51 +0800

    Add test cases for updated list command

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,9 +1,15 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalPersons;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +17,9 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ClassGroup;
+import seedu.address.model.person.ListCommandPredicate;
+import seedu.address.model.person.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -20,6 +29,39 @@ public class ListCommandTest {
     private Model model;
     private Model expectedModel;
 
+    private final ClassGroup classGroupA1 = new ClassGroup("A-1");
+
+    private final ClassGroup classGroupB1 = new ClassGroup("B-1");
+
+    private final ClassGroup nonExistentClassGroup = new ClassGroup("NonExistentClassGroup");
+
+    @Test
+    public void equals() {
+        ListCommandPredicate firstPredicate =
+                new ListCommandPredicate(Optional.of(Collections.singletonList(classGroupA1)));
+        ListCommandPredicate secondPredicate =
+                new ListCommandPredicate(Optional.of(Collections.singletonList(classGroupB1)));
+
+        ListCommand listFirstCommand = new ListCommand(firstPredicate);
+        ListCommand listSecondCommand = new ListCommand(secondPredicate);
+
+        // same object -> returns true
+        assertEquals(listFirstCommand, listFirstCommand);
+
+        // same values -> returns true
+        ListCommand listFirstCommandCopy = new ListCommand(firstPredicate);
+        assertEquals(listFirstCommand, listFirstCommandCopy);
+
+        // different types -> returns false
+        assertNotEquals(1, listFirstCommand);
+
+        // null -> returns false
+        assertNotEquals(null, listFirstCommand);
+
+        // different class group -> returns false
+        assertNotEquals(listFirstCommand, listSecondCommand);
+    }
+
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -28,12 +70,57 @@ public class ListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        int expectedSize = model.getFilteredPersonList().size();
+        String expectedMessage = String.format(ListCommand.MESSAGE_LIST_ALL_SUCCESS, expectedSize);
+        assertCommandSuccess(new ListCommand(new ListCommandPredicate(Optional.empty())),
+                model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_listIsFiltered_showsEverything() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    public void execute_singleClassGroup_showsSingleClassGroup() {
+        List<Person> expectedPersons = getTypicalPersons().stream()
+                .filter(person -> person.getClassGroup().equals(classGroupA1))
+                .collect(Collectors.toList());
+        String expectedMessage = String.format(ListCommand.MESSAGE_LIST_CLASS_SUCCESS,
+                expectedPersons.size(), classGroupA1);
+        ListCommandPredicate predicate = new ListCommandPredicate(Optional.of(List.of(classGroupA1)));
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(new ListCommand(predicate), model, expectedMessage, expectedModel);
     }
+
+    @Test
+    public void execute_multipleClassGroup_showsMultipleClassGroups() {
+        List<ClassGroup> classGroups = List.of(classGroupA1, classGroupB1);
+        ListCommandPredicate predicate = new ListCommandPredicate(Optional.of(classGroups));
+        List<Person> expectedPersons = getTypicalPersons().stream()
+                .filter(person -> classGroups.contains(person.getClassGroup()))
+                .collect(Collectors.toList());
+        String classGroupsString = classGroups.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        String expectedMessage = String.format(ListCommand.MESSAGE_LIST_CLASS_SUCCESS,
+                expectedPersons.size(), classGroupsString);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(new ListCommand(predicate), model, expectedMessage, expectedModel);
+        assertEquals(expectedPersons, model.getFilteredPersonList());
+    }
+
+    @Test
+    public void execute_nonExistentClassGroup_showsEmptyList() {
+        String expectedMessage = String.format(ListCommand.MESSAGE_LIST_CLASS_SUCCESS, 0, nonExistentClassGroup);
+        ListCommandPredicate predicate = new ListCommandPredicate(Optional.of(List.of(nonExistentClassGroup)));
+        ListCommand command = new ListCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void toStringMethod() {
+        ListCommandPredicate predicate = new ListCommandPredicate(Optional.of(List.of(classGroupA1)));
+        ListCommand listCommand = new ListCommand(predicate);
+        String expected = ListCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
+        assertEquals(expected, listCommand.toString());
+    }
+
 }
