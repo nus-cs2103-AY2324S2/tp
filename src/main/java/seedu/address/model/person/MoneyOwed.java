8e3b56ef155a0bbda7cd3c29da3cc94ee70ab221
alvinnzz <alvinng01@live.com>
2024-03-16 00:33:25 +0800

    Add javadoc to MoneyOwed class

--- src/main/java/seedu/address/model/person/MoneyOwed.java
+++ src/main/java/seedu/address/model/person/MoneyOwed.java
@@ -3,18 +3,32 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-
+/**
+ * Represents a Person's money owed in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidMoney(String)}
+ */
 public class MoneyOwed {
-    public final Float moneyOwed;
+
     public static final String MESSAGE_CONSTRAINTS =
             "Money Owed should be at most 2 decimal places in the following format 'xxx.xx' or '-xxx.xx'. ";
     public static final String VALIDATION_REGEX = "^(?:-)?\\d+(\\.\\d{0,2})?";
 
+    public final Float moneyOwed;
+
+    /**
+     * Constructs a {@code MoneyOwed}.
+     *
+     * @param money A valid amount of money owed.
+     */
     public MoneyOwed(String money) {
         requireNonNull(money);
         checkArgument(isValidMoney(money), MESSAGE_CONSTRAINTS);
         moneyOwed = Float.parseFloat(money);
     }
+
+    /**
+     * Returns true if a given string is a valid money amount.
+     */
     public static boolean isValidMoney(String test) {
         return test.matches(VALIDATION_REGEX);
     }
