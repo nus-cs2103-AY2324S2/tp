72fd0c72642313131a7c0423a08e0adebc4ed955
Patrick <pshin0001@gmail.com>
2024-03-15 13:43:01 +0800

    Fix bugs

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -39,8 +39,6 @@ public class CommandTestUtil {
     public static final String VALID_YEAR_JOINED_BOB = "2024";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
-    public static final String VALID_YEAR_JOINED_AMY = "2024";
-    public static final String VALID_YEAR_JOINED_BOB = "2024";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
@@ -54,8 +52,6 @@ public class CommandTestUtil {
     public static final String YEARJOINED_DESC_BOB = " " + PREFIX_YEAR_JOINED + VALID_YEAR_JOINED_BOB;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
-    public static final String YEARJOINED_DESC_AMY = " " + PREFIX_YEAR_JOINED + VALID_YEAR_JOINED_AMY;
-    public static final String YEARJOINED_DESC_BOB = " " + PREFIX_YEAR_JOINED + VALID_YEAR_JOINED_BOB;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
