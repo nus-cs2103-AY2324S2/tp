01b8befac935356dfea0669596d7e1af56885614
iamtr <jettzeong@gmail.com>
2024-03-15 12:19:34 +0800

    Create Ward class

---
+++ src/main/java/seedu/address/model/person/Ward.java
@@ -0,0 +1,43 @@
+package seedu.address.model.person;
+
+/**
+ * Represents a patient's ward address in the address book.
+ */
+public class Ward {
+    public static final String MESSAGE_CONSTRAINTS = "Ward addresses can take any values, and it should not be blank";
+
+    public final String ward;
+
+    /**
+    * Constructs a {@code Ward}.
+    *
+    * @param ward A valid ward address.
+    */
+    public Ward(String ward) {
+        this.ward = ward;
+    }
+
+    @Override
+    public String toString() {
+        return ward;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Ward)) {
+            return false;
+        }
+
+        Ward otherWard = (Ward) other;
+        return ward.equals(otherWard.ward);
+    }
+
+    @Override
+    public int hashCode() {
+        return ward.hashCode();
+    }
+}
