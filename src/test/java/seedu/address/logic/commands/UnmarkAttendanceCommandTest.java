db873906bae95743faf7b7eb03bd740db2ddea7f
belligerentbeagle <ethan.wei@u.nus.edu>
2024-03-15 11:54:49 +0800

    Build check fixes

--- src/test/java/seedu/address/logic/commands/UnmarkAttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkAttendanceCommandTest.java
@@ -8,16 +8,16 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.NusNet;
 import seedu.address.model.person.Person;
 import seedu.address.model.weeknumber.WeekNumber;
-
-import java.util.HashSet;
-
-import org.junit.jupiter.api.Test;
 
 
 /**
