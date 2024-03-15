5cd33bafa7c637686750ff5637005a027e43f18e
tankh99 <khanghou99@gmail.com>
2024-03-15 09:12:48 +0800

    Add stub tests

---
+++ src/test/java/seedu/address/commons/util/UiUtilTest.java
@@ -0,0 +1,16 @@
+package seedu.address.commons.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UiUtilTest {
+
+    @Test
+    void setShortcut() {
+    }
+
+    @Test
+    void setText() {
+    }
+}
