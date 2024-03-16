1bd2f3b4c2181126c91084681f77404752b0b6d8
Shauryan Agrawal <Shauryan100@gmail.com>
2024-03-16 20:59:07 +0800

    Update test files PersonBuilder and PersonUitl by adding default relationship

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -8,8 +8,10 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.relationship.Relationship;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
+
 
 /**
  * A utility class to help with building Person objects.
@@ -21,11 +23,16 @@ public class PersonBuilder {
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
 
+    public static final String DEFAULT_RELATIONSHIP = "partner";
+
+
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
     private Set<Tag> tags;
+
+    private Relationship relationship;
 
     /**
      * Creates a {@code PersonBuilder} with the default details.
@@ -35,6 +42,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        relationship = new Relationship(DEFAULT_RELATIONSHIP);
         tags = new HashSet<>();
     }
 
@@ -46,6 +54,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        relationship = personToCopy.getRelationship();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -89,8 +98,17 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code Relationship} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withRelationship(String relationship) {
+        this.relationship = new Relationship(relationship);
+        return this;
+    }
+
+
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, relationship, tags);
     }
 
 }
