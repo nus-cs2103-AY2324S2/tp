096fd31e707c2b98991a57a67bfa189c75c7d84c
Miffi <miffi@decadent.rest>
2024-03-16 13:53:56 +0800

    Increase code coverage

--- src/test/java/seedu/address/logic/commands/UntagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UntagCommandTest.java
@@ -1,21 +1,12 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -24,13 +15,21 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 /**
  * Contains integration tests (interactions with the Model) and unit tests for UntagCommand.
  */
 class UntagCommandTest {
-    private static final Tag TAG_FRIEND = new Tag(VALID_TAG_FRIEND);
-    private static final Tag TAG_HUSBAND = new Tag(VALID_TAG_HUSBAND);
-    private static final Collection<Tag> TAGS = List.of(TAG_FRIEND, TAG_HUSBAND);
+    private static final Tag TAG_OWES_MONEY = new Tag("owesMoney");
+    private static final Tag TAG_FRIENDS = new Tag("friends");
+    private static final Collection<Tag> TAGS = List.of(TAG_OWES_MONEY, TAG_FRIENDS);
 
     private Model model;
 
@@ -41,10 +40,8 @@ class UntagCommandTest {
 
     @Test
     public void execute_multipleTagsSpecified_success() {
-        List<Tag> tags = List.of(new Tag("owesMoney"), new Tag("friends"));
         var index = INDEX_SECOND_PERSON;
-        var command = new UntagCommand(index, tags);
-
+        var command = new UntagCommand(index, TAGS);
         var actualPerson = model.getFilteredPersonList().get(index.getZeroBased());
 
         var editedPerson = new PersonBuilder(actualPerson).withTags().build();
@@ -62,11 +59,8 @@ class UntagCommandTest {
     @Test
     public void execute_tagMissing_failure() {
         var index = INDEX_FIRST_PERSON;
-        List<Tag> tags = List.of(new Tag("owesMoney"), new Tag("friends"));
-
         var personName = model.getFilteredPersonList().get(index.getZeroBased()).getName();
-
-        var command = new UntagCommand(index, tags);
+        var command = new UntagCommand(index, TAGS);
 
         var expectedMessage = String.format(
                 Messages.MESSAGE_MISSING_TAG,
@@ -77,15 +71,27 @@ class UntagCommandTest {
     }
 
     @Test
+    public void execute_indexOutOfBounds_failure() {
+        var index = Index.fromOneBased(999);
+        var command = new UntagCommand(index, TAGS);
+
+        var expectedMessage = Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+        assertCommandFailure(command, model, expectedMessage);
+    }
+
+    @Test
     public void equals() {
         final UntagCommand standardCommand = new UntagCommand(INDEX_FIRST_PERSON, TAGS);
+
+        // same object -> equal
+        assertEquals(standardCommand, standardCommand);
 
         // same values -> equal
         var commandWithSameValues = new UntagCommand(INDEX_FIRST_PERSON, TAGS);
         assertEquals(standardCommand, commandWithSameValues);
 
         // tags in different order -> equal
-        var tagsInDifferentOrder = List.of(TAG_HUSBAND, TAG_FRIEND);
+        var tagsInDifferentOrder = List.of(TAG_FRIENDS, TAG_OWES_MONEY);
         assertEquals(standardCommand, new UntagCommand(INDEX_FIRST_PERSON, tagsInDifferentOrder));
 
         // null -> not equal
@@ -98,6 +104,6 @@ class UntagCommandTest {
         assertNotEquals(standardCommand, new UntagCommand(INDEX_SECOND_PERSON, TAGS));
 
         // different tags -> not equal
-        assertNotEquals(standardCommand, new UntagCommand(INDEX_FIRST_PERSON, List.of(TAG_FRIEND)));
+        assertNotEquals(standardCommand, new UntagCommand(INDEX_FIRST_PERSON, List.of(TAG_OWES_MONEY)));
     }
 }
--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -22,9 +22,11 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.UntagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
@@ -86,6 +88,16 @@ public class AddressBookParserTest {
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseCommand_untag() throws Exception {
+        var index = INDEX_FIRST_PERSON;
+        var expectedCommand = new UntagCommand(index, List.of(new Tag("alpha"), new Tag("beta")));
+        assertEquals(expectedCommand, parser.parseCommand(UntagCommand.COMMAND_WORD
+                + " "
+                + INDEX_FIRST_PERSON.getOneBased()
+                + " t/alpha t/beta"));
     }
 
     @Test
