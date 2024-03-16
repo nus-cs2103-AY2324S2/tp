6770bb14692960a9aa20b64faa0ef0f45e576c2b
LimZiJia <zijialim@gmail.com>
2024-03-16 16:22:47 +0800

    Add base classes of Client and Housekeer

---
+++ src/main/java/seedu/address/model/person/Housekeeper.java
@@ -0,0 +1,20 @@
+package seedu.address.model.person;
+
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
+public class Housekeeper extends Person {
+    /**
+     * Every field must be present and not null.
+     *
+     * @param name
+     * @param phone
+     * @param email
+     * @param address
+     * @param tags
+     */
+    public Housekeeper(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        super(name, phone, email, address, tags);
+    }
+}
