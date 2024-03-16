6fd3aa111e68db7b52f10c3cd27ef14a2db8aa19
Ty-stan0417 <yisheng0417@gmail.com>
2024-03-17 01:52:57 +0800

    Add Donate Command

--- src/main/java/seedu/address/logic/parser/DonateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DonateCommandParser.java
@@ -2,7 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_BOOKLIST;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
