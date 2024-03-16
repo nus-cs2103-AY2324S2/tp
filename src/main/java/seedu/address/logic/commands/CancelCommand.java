72ab99731e059d6dace15e05bd60933a89f0066e
chonghaoen <cocopowder999@gmail.com>
2024-03-17 01:02:51 +0800

    Fix Style

--- src/main/java/seedu/address/logic/commands/CancelCommand.java
+++ src/main/java/seedu/address/logic/commands/CancelCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
--- src/main/java/seedu/address/model/order/Order.java
+++ src/main/java/seedu/address/model/order/Order.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+
 import seedu.address.model.person.Person;
 
 /**
