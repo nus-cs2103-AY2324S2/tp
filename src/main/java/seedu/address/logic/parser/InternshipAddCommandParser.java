0a78f53eaa2a8acdfcd8205379f1eb19aa87c83a
HM33-Stu <120201372+HM33-Stu@users.noreply.github.com>
2024-03-16 10:31:14 +0800

    update-add-command

---
+++ src/main/java/seedu/address/logic/parser/InternshipAddCommandParser.java
@@ -0,0 +1,73 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.InternshipMessages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_NUMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.InternshipAddCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.internship.ApplicationStatus;
+import seedu.address.model.internship.CompanyName;
+import seedu.address.model.internship.ContactEmail;
+import seedu.address.model.internship.ContactName;
+import seedu.address.model.internship.ContactNumber;
+import seedu.address.model.internship.Description;
+import seedu.address.model.internship.Location;
+import seedu.address.model.internship.Role;
+import seedu.address.model.internship.Internship;
+
+/**
+ * Parses input arguments and creates a new InternshipAddCommand object
+ */
+public class InternshipAddCommandParser implements Parser<InternshipAddCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the InternshipAddCommand
+     * and returns an InternshipAddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+
+    public InternshipAddCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_COMPANY, PREFIX_CONTACT_NAME, PREFIX_CONTACT_EMAIL, PREFIX_CONTACT_NUMBER,
+                        PREFIX_LOCATION, PREFIX_STATUS, PREFIX_DESCRIPTION, PREFIX_ROLE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_COMPANY, PREFIX_DESCRIPTION, PREFIX_STATUS)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, InternshipAddCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_COMPANY, PREFIX_CONTACT_NAME, PREFIX_CONTACT_EMAIL, PREFIX_CONTACT_NUMBER,
+                PREFIX_LOCATION, PREFIX_STATUS, PREFIX_DESCRIPTION, PREFIX_ROLE);
+
+        CompanyName com = InternshipParserUtil.parseCompanyName(argMultimap.getValue(PREFIX_COMPANY).get());
+        ContactName poc = InternshipParserUtil.parseContactName(argMultimap.getValue(PREFIX_CONTACT_NAME).get());
+        ContactEmail email = InternshipParserUtil.parseContactEmail(argMultimap.getValue(PREFIX_CONTACT_EMAIL).get());
+        ContactNumber phone = InternshipParserUtil.parseContactNumber(argMultimap.getValue(PREFIX_CONTACT_NUMBER).get());
+        Location loc = InternshipParserUtil.parseLocation(argMultimap.getValue(PREFIX_LOCATION).get());
+        ApplicationStatus status = InternshipParserUtil.parseStatus(argMultimap.getValue(PREFIX_STATUS).get());
+        Description desc = InternshipParserUtil.parseDescription(argMultimap.getValue( PREFIX_DESCRIPTION).get());
+        Role role = InternshipParserUtil.parseRole(argMultimap.getValue(PREFIX_ROLE).get());
+
+        Internship internship = new Internship(com, poc, email, phone, loc, status, desc, role);
+
+        return new InternshipAddCommand(internship);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
---
+++ src/test/java/seedu/address/logic/commands/InternshipAddCommandTest.java
@@ -0,0 +1,204 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalInternships.ALICE_MICROSOFT;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.InternshipMessages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.InternshipData;
+import seedu.address.model.InternshipModel;
+import seedu.address.model.ReadOnlyInternshipData;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.internship.Internship;
+import seedu.address.testutil.InternshipBuilder;
+
+public class InternshipAddCommandTest {
+
+    @Test
+    public void constructor_nullInternship_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new InternshipAddCommand(null));
+    }
+
+    @Test
+    public void execute_InternshipAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingInternshipAdded modelStub = new ModelStubAcceptingInternshipAdded();
+        Internship validInternship = new InternshipBuilder().build();
+
+        CommandResult commandResult = new InternshipAddCommand(validInternship).execute(modelStub);
+
+        assertEquals(String.format(InternshipAddCommand.MESSAGE_SUCCESS, InternshipMessages.format(validInternship)),
+                commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validInternship), modelStub.InternshipsAdded);
+    }
+
+    @Test
+    public void execute_duplicateInternship_throwsCommandException() {
+        Internship validInternship = new InternshipBuilder().build();
+        InternshipAddCommand InternshipAddCommand = new InternshipAddCommand(validInternship);
+        ModelStub modelStub = new ModelStubWithInternship(validInternship);
+
+        assertThrows(CommandException.class, InternshipAddCommand.MESSAGE_DUPLICATE_INTERNSHIP, () -> InternshipAddCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        Internship alice = new InternshipBuilder().withCompanyName("Microsoft").build();
+        Internship bob = new InternshipBuilder().withCompanyName("Google").build();
+        InternshipAddCommand addAliceCommand = new InternshipAddCommand(alice);
+        InternshipAddCommand addBobCommand = new InternshipAddCommand(bob);
+
+        // same object -> returns true
+        assertTrue(addAliceCommand.equals(addAliceCommand));
+
+        // same values -> returns true
+        InternshipAddCommand addAliceCommandCopy = new InternshipAddCommand(alice);
+        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addAliceCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addAliceCommand.equals(null));
+
+        // different Internship -> returns false
+        assertFalse(addAliceCommand.equals(addBobCommand));
+    }
+
+    @Test
+    public void toStringMethod() {
+        InternshipAddCommand InternshipAddCommand = new InternshipAddCommand(ALICE_MICROSOFT);
+        String expected = InternshipAddCommand.class.getCanonicalName() + "{toAdd=" + ALICE_MICROSOFT + "}";
+        assertEquals(expected, InternshipAddCommand.toString());
+    }
+
+    /**
+     * A default model stub that have all methods failing.
+     */
+    private class ModelStub implements InternshipModel {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getInternshipDataFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setInternshipDataFilePath(Path InternshipDataFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addInternship(Internship Internship) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setInternshipData(ReadOnlyInternshipData newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyInternshipData getInternshipData() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasInternship(Internship Internship) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteInternship(Internship target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setInternship(Internship target, Internship editedInternship) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Internship> getFilteredInternshipList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredInternshipList(Predicate<Internship> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single internship entry.
+     */
+    private class ModelStubWithInternship extends ModelStub {
+        private final Internship internship;
+
+        ModelStubWithInternship(Internship internship) {
+            requireNonNull(internship);
+            this.internship = internship;
+        }
+
+        @Override
+        public boolean hasInternship(Internship internship) {
+            requireNonNull(internship);
+            return this.internship.isSameInternship(internship);
+        }
+    }
+
+    /**
+     * A Model stub that always accept the Internship being added.
+     */
+    private class ModelStubAcceptingInternshipAdded extends ModelStub {
+        final ArrayList<Internship> internshipsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasInternship(Internship internship) {
+            requireNonNull(internship);
+            return internshipsAdded.stream().anyMatch(internship::isSameInternship);
+        }
+
+        @Override
+        public void addInternship(Internship internship) {
+            requireNonNull(internship);
+            internshipsAdded.add(internship);
+        }
+
+        @Override
+        public ReadOnlyInternshipData getInternshipData() {
+            return new InternshipData();
+        }
+    }
+
+}
