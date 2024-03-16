1091c5003cc1b083ff1b4cc134052864d3378d63
whitesnowx <whitesnowx88@gmail.com>
2024-03-16 08:10:44 +0800

    add new function in SortCommandParserTest to code cover for different attribute parsing in Sort

--- src/test/java/staffconnect/logic/parser/SortCommandParserTest.java
+++ src/test/java/staffconnect/logic/parser/SortCommandParserTest.java
@@ -2,14 +2,20 @@ package staffconnect.logic.parser;
 
 
 import static staffconnect.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static staffconnect.logic.parser.CliSyntax.PREFIX_MODULE;
+import static staffconnect.logic.parser.CliSyntax.PREFIX_NAME;
+import static staffconnect.logic.parser.CliSyntax.PREFIX_PHONE;
 import static staffconnect.logic.parser.CliSyntax.PREFIX_VENUE;
 import static staffconnect.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static staffconnect.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static staffconnect.model.person.comparators.ModuleComparator.MODULE_COMPARATOR;
+import static staffconnect.model.person.comparators.NameComparator.NAME_COMPARATOR;
+import static staffconnect.model.person.comparators.PhoneComparator.PHONE_COMPARATOR;
+import static staffconnect.model.person.comparators.VenueComparator.VENUE_COMPARATOR;
 
 import org.junit.jupiter.api.Test;
 
 import staffconnect.logic.commands.SortCommand;
-import staffconnect.model.person.comparators.VenueComparator;
 
 public class SortCommandParserTest {
 
@@ -23,7 +29,7 @@ public class SortCommandParserTest {
     @Test
     public void parse_validArgs_returnsSortCommand() {
 
-        SortCommand expectedSortCommand = new SortCommand(VenueComparator.VENUE_COMPARATOR);
+        SortCommand expectedSortCommand = new SortCommand(VENUE_COMPARATOR);
 
         // no leading and no trailing whitespaces
         assertParseSuccess(parser, "" + PREFIX_VENUE, expectedSortCommand);
@@ -36,6 +42,16 @@ public class SortCommandParserTest {
 
         // multiple whitespaces before and after keywords
         assertParseSuccess(parser, "   " + PREFIX_VENUE + "    ", expectedSortCommand);
+
+    }
+
+    @Test
+    public void parse_validArgs_returnsSortCorrectAttribute() {
+
+        assertParseSuccess(parser, "" + PREFIX_MODULE, new SortCommand(MODULE_COMPARATOR));
+        assertParseSuccess(parser, "" + PREFIX_NAME, new SortCommand(NAME_COMPARATOR));
+        assertParseSuccess(parser, "" + PREFIX_PHONE, new SortCommand(PHONE_COMPARATOR));
+        assertParseSuccess(parser, "" + PREFIX_VENUE, new SortCommand(VENUE_COMPARATOR));
 
     }
 
