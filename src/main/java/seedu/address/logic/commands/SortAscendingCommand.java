c47435588c56e55d5ed43dd39c6be2610e0b078b
R-Laksh <ramulakshmanan12@gmail.com>
2024-03-16 21:21:41 +0800

    Update Model Interface to allow for sort functionality

---
+++ src/main/java/seedu/address/logic/commands/SortAscendingCommand.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+/**
+ * Sorts all students in the address book to the user.
+ */
+public class SortCommandAscending extends Command {
+
+    public static final String COMMAND_WORD = "list";
+    public static final String MESSAGE_SUCCESS = "Listed all persons";
+}
