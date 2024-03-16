fc827edbb7061383040c7d6fa51cbb5e456d7bb4
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-03-16 16:44:44 +0800

    Refactor find command parser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,33 +1,62 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Arrays;
-
+import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.predicates.AddressContainsSubstringPredicate;
+import seedu.address.model.person.predicates.CombinedPredicates;
+import seedu.address.model.person.predicates.EmailContainsSubstringPredicate;
+import seedu.address.model.person.predicates.NameContainsSubstringPredicate;
+import seedu.address.model.person.predicates.NoteContainsSubstringPredicate;
+import seedu.address.model.person.predicates.PhoneContainsSubstringPredicate;
+import seedu.address.model.person.predicates.TagSetContainsAnyTagPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
-
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * Parses the given {@code String} of arguments in the context of the
+     * FindCommand
      * and returns a FindCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
+                PREFIX_ADDRESS, PREFIX_NOTE, PREFIX_TAG);
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        NameContainsSubstringPredicate namePredicate = new NameContainsSubstringPredicate(
+                ParserUtil.parseSearchString(argMultimap.getValue(PREFIX_NAME).get()));
+        PhoneContainsSubstringPredicate phonePedicate = new PhoneContainsSubstringPredicate(
+                ParserUtil.parseSearchString(argMultimap.getValue(PREFIX_PHONE).get()));
+        EmailContainsSubstringPredicate emailPedicate = new EmailContainsSubstringPredicate(
+                ParserUtil.parseSearchString(argMultimap.getValue(PREFIX_EMAIL).get()));
+        AddressContainsSubstringPredicate addressPedicate = new AddressContainsSubstringPredicate(
+                ParserUtil.parseSearchString(argMultimap.getValue(PREFIX_ADDRESS).get()));
+        NoteContainsSubstringPredicate notePedicate = new NoteContainsSubstringPredicate(ParserUtil
+                .parseSearchString(argMultimap.getValue(PREFIX_NOTE).get()));
+        TagSetContainsAnyTagPredicate tagsPredicate = new TagSetContainsAnyTagPredicate(
+                ParserUtil.parseSearchTagSet(ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG))));
+
+        CombinedPredicates predicates = new CombinedPredicates(namePredicate, phonePedicate, emailPedicate,
+                addressPedicate, notePedicate, tagsPredicate);
+
+        return new FindCommand(predicates);
     }
 
 }
