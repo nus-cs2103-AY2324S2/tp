2ef47f091383178fb4370c9d91c0c6e95e39a06c
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-03-16 22:29:52 +0800

    Edit CommandTestUtil static to add new fields

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -8,6 +8,7 @@ import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Family;
+import seedu.address.model.person.Income;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
