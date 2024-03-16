59279b668dbebd49f1f2b2f482fbe0438fd84cb1
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-03-16 16:17:47 +0800

    Refactor find command
    Find command should be able to search multiple fields in one command

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,35 +1,53 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.predicates.CombinedPredicates;
+import seedu.address.model.person.predicates.SearchPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all persons in address book whose name contains any of the
+ * argument keywords.
  * Keyword matching is case insensitive.
  */
-public class FindCommand extends Command {
+public class FindCommand<T extends Object> extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Finds all persons whose specified attribute contains the specified keyword"
+            + "and displays them as a list with index numbers."
+            + "Searches name field if no prefix is specified.\n"
+            + "Parameters: "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_NOTE + "NOTE] "
+            + "[" + PREFIX_TAG + "TAG] "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_EMAIL + "example.com";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final CombinedPredicates predicates;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    @SafeVarargs
+    public FindCommand(SearchPredicate<? super T>... predicates) {
+        this.predicates = new CombinedPredicates(predicates);
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredPersonList(predicates);
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
@@ -45,14 +63,14 @@ public class FindCommand extends Command {
             return false;
         }
 
-        FindCommand otherFindCommand = (FindCommand) other;
-        return predicate.equals(otherFindCommand.predicate);
+        FindCommand<?> otherFindCommand = (FindCommand<?>) other;
+        return predicates.equals(otherFindCommand.predicates);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("predicate", predicate)
+                .add("predicates", predicates)
                 .toString();
     }
 }
---
+++ src/main/java/seedu/address/model/person/predicates/CombinedPredicates.java
@@ -0,0 +1,44 @@
+package seedu.address.model.person.predicates;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Person;
+
+/**
+ * Defines a Predicate which combines multiple {@code SearchPredicates}.
+ */
+public class CombinedPredicates implements Predicate<Person> {
+    private final SearchPredicate<? extends Object>[] predicates;
+
+    public CombinedPredicates(SearchPredicate<? extends Object>[] predicates) {
+        this.predicates = predicates;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return Arrays.stream(predicates).allMatch(predicate -> predicate.test(person));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof CombinedPredicates)) {
+            return false;
+        }
+
+        CombinedPredicates otherCombinedPredicate = (CombinedPredicates) other;
+        return predicates.equals(otherCombinedPredicate.predicates);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("predicates", predicates).toString();
+    }
+
+}
