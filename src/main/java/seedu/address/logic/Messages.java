b3b7b73d631c9a4df894429a8fa687718ad78bf5
belligerentbeagle <ethan.wei@u.nus.edu>
2024-03-17 02:41:04 +0800

    Minor bug fix

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -20,8 +20,6 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
-    public static final String MESSAGE_MISSING_NUSNET =
-            "There is no student with this NUSNet ID!";
     public static final String MESSAGE_MARK_ATTENDANCE_SUCCESS = "Marked Attendance for Person: ";
     public static final String MESSAGE_MARK_EXISTING_ATTENDANCE_SUCCESS =
             "Re-marked Attendance for Person: ";
