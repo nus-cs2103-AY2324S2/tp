5c4e67d468eb9a7aea0c63a7f0fb049a5ff4bf7b
Chua Zi Long <e0957913@u.nus.edu>
2024-03-16 19:48:05 +0800

    Fix code to pass checkstyle

--- src/test/java/seedu/address/logic/commands/FindAndExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindAndExportCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
