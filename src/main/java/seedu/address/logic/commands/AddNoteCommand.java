665392252e1bf9f57f32851c3b3d129c5456494c
tengcharmaine <e0969596@u.nus.edu>
2024-03-16 16:03:40 +0800

    Start addnote from scratch again to prevent mixup

--- src/main/java/seedu/address/logic/commands/AddNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/AddNoteCommand.java
@@ -1,35 +1,16 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_IC;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
-import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.IdentityCardNumberMatchesPredicate;
-import seedu.address.model.person.Person;
 
 /**
- * Adds a note to the person in the address book whose IC matches the argument IC.
- * Keyword matching is case-insensitive.
+ * Changes the remark of an existing person in the address book.
  */
 public class AddNoteCommand extends Command {
 
     public static final String COMMAND_WORD = "addnote";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Adds a note to the person identified by their IC (case-insensitive).\n"
-            + "Parameters: "
-            + PREFIX_IC + "IC "
-            + PREFIX_NOTE + "NOTE "
-            + "Example: " + COMMAND_WORD
-            + PREFIX_IC + " S0123456Q"
-            + PREFIX_NOTE + " Healthy";
-
-    public static final String MESSAGE_SUCCESS = "New note added to person : %1$s";
-
-    private final IdentityCardNumberMatchesPredicate icNumber;
-
-    public AddNoteCommand(IdentityCardNumberMatchesPredicate icNumber) {
-        this.icNumber = icNumber;
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Hello from addnote");
     }
 }
