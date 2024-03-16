2c4bc2a4ed320fa3a63c0fb9d24c43ba31f3db26
RyanNgWH <ryan.ng.dev@tinynamoo.com>
2024-03-16 16:44:28 +0800

    Create parserr for find command attributes

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -16,18 +16,23 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.TagSet;
 
 /**
- * Contains utility methods used for parsing strings in the various *Parser classes.
+ * Contains utility methods used for parsing strings in the various *Parser
+ * classes.
  */
 public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
 
     /**
-     * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
+     * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading
+     * and trailing whitespaces will be
      * trimmed.
-     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
+     *
+     * @throws ParseException if the specified index is invalid (not non-zero
+     *                        unsigned integer).
      */
     public static Index parseIndex(String oneBasedIndex) throws ParseException {
         String trimmedIndex = oneBasedIndex.trim();
@@ -132,5 +137,32 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    /**
+     * Parses a {@code searchString} into a string.
+     * Leading and trailing whitespaces will be trimmed.
+     *
+     * @param searchString String to search
+     *
+     * @return The string ready to be used for searching
+     */
+    public static String parseSearchString(String searchString) {
+        requireNonNull(searchString);
+        String trimmedAddress = searchString.trim();
+
+        return trimmedAddress;
+    }
+
+    /**
+     * Parses a {@code Set<Tag>} into a TagSet.
+     *
+     * @param tags Set of tags
+     *
+     * @return TagSet representing the set of tags
+     */
+    public static TagSet parseSearchTagSet(Set<Tag> tags) {
+        requireNonNull(tags);
+        return new TagSet(tags);
     }
 }
