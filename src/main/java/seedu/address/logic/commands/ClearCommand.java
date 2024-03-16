cc1f5bdc7a8e656da3865ad6c84fb33296e6a9c9
zhuuyicheng <zhu.yicheng@yahoo.com>
2024-03-16 23:42:00 +0800

    Update ClearCommand and ClearCommandTest classes

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -11,13 +11,36 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
+    public static final String MESSAGE_CONFIRMATION = "Are you sure you want to clear ALL contacts? [y/n]";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_CANCELLED = "Clear cancelled!";
+
+    private static boolean awaitingConfirmation = false;
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (!model.isAwaitingClear()) {
+            model.setAwaitingClear(true);
+            return new CommandResult(MESSAGE_CONFIRMATION);
+        } else {
+            model.setAwaitingClear(false); // Reset the confirmation state
+            if (model.isConfirmClear()) {
+                model.setConfirmClear(false);
+                model.setAddressBook(new AddressBook());
+                return new CommandResult(MESSAGE_SUCCESS);
+            } else {
+                return new CommandResult(MESSAGE_CANCELLED);
+            }
+        }
+    }
+
+    public static boolean isAwaitingConfirmation() {
+        return awaitingConfirmation;
+    }
+
+    public static void setAwaitingConfirmation(boolean awaitingConfirmation) {
+        ClearCommand.awaitingConfirmation = awaitingConfirmation;
     }
 }
