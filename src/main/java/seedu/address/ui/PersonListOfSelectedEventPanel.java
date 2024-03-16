e933a10c9bedbcd6b39346bad7a2ff8d2ff10ce2
Max Yang <>
2024-03-16 15:22:35 +0800

    Remove unnecessary tests

--- src/main/java/seedu/address/ui/PersonListOfSelectedEventPanel.java
+++ src/main/java/seedu/address/ui/PersonListOfSelectedEventPanel.java
@@ -58,13 +58,7 @@ public class PersonListOfSelectedEventPanel extends UiPart<Region> {
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
-
-            if (empty || person == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
-            }
+            setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
         }
     }
 }
