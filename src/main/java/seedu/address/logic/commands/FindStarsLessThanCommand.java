238da71dc83419b69f6fb28d41b6fdc01084bdf8
evedaktyl <evedaktyl@gmail.com>
2024-03-16 14:11:58 +0800

    Add FindStarsLessThan command
    Let's allow users to filter persons based on the number of participation stars.

--- src/main/java/seedu/address/logic/commands/FindStarsLessThanCommand.java
+++ src/main/java/seedu/address/logic/commands/FindStarsLessThanCommand.java
@@ -1,12 +1,17 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.StarsLessThanPredicate;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book whose number of stars is less than the argument.
  * Comparator is strictly less than.
  */
-public class FindStarsLessThanCommand {
+public class FindStarsLessThanCommand extends Command {
     public static final String COMMAND_WORD = "findStarsLessThan";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose number of stars is less than "
@@ -20,4 +25,35 @@ public class FindStarsLessThanCommand {
         this.predicate = predicate;
     }
 
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindStarsLessThanCommand)) {
+            return false;
+        }
+
+        FindStarsLessThanCommand otherFindStarsLessThanCommand = (FindStarsLessThanCommand) other;
+        return predicate.equals(otherFindStarsLessThanCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+
 }
+
