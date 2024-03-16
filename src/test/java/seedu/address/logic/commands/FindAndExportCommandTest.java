3c4d174fa9f38e1c2bbb2bed91240050d671df1c
Chua Zi Long <e0957913@u.nus.edu>
2024-03-16 18:50:02 +0800

    Fix code to match checkstyle

--- src/test/java/seedu/address/logic/commands/FindAndExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindAndExportCommandTest.java
@@ -3,19 +3,20 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.nio.file.Files;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Files;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class FindAndExportCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
     @TempDir
     java.nio.file.Path testFolder;
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void execute_exportSuccess_writesFile() throws Exception {
--- src/test/java/seedu/address/logic/parser/FindAndExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindAndExportCommandParserTest.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.FindAndExportCommand;
 
 public class FindAndExportCommandParserTest {
