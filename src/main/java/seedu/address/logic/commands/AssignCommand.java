6c3612946f6a8350fc614b61555b3177179a2ca9
Chong Chin Herng <chongahzong@gmail.com>
2024-03-16 19:12:02 +0800

    Change AssignCommand to throw an exception

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +10,20 @@ public class AssignCommand extends Command {
 
     public static final String COMMAND_WORD = "assign";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Assigns the task identified "
+            + "by the index number used in the last task listing "
+            + "to the person identified "
+            + "by the index number used in the last person listing. "
+            + "Does nothing if the task is already assigned to the person.\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "/to 2";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Assign command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from assign");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
