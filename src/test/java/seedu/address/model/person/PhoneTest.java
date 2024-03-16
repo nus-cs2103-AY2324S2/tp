c401b7bc287229d88998a3b085df500d017507c7
tankh99 <khanghou99@gmail.com>
2024-03-17 07:19:46 +0800

    Fix phone number format in tests

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -31,11 +31,14 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
+        assertFalse(Phone.isValidPhone("9234")); // More than 3 numbers
+        assertFalse(Phone.isValidPhone("599")); // Doesn't start with 6, 8 or 9
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
-        assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+        assertTrue(Phone.isValidPhone("63121534")); // Starts with 6, 8-digits
+        assertTrue(Phone.isValidPhone("83121534")); // Starts with 8, 8-digits
+        assertTrue(Phone.isValidPhone("93121534")); // Starts with 9, 8-digits
     }
 
     @Test
