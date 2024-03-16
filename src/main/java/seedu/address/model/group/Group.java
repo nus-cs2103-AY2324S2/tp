c98269233f12270092f85de7f5f4fff73cdf06cc
Yuhi Narita <yuhi.narita26@gmail.com>
2024-03-16 19:03:42 +0800

    add group card

--- src/main/java/seedu/address/model/group/Group.java
+++ src/main/java/seedu/address/model/group/Group.java
@@ -1,4 +1,35 @@
 package seedu.address.model.group;
 
+import seedu.address.model.coursemate.CourseMate;
+import seedu.address.model.coursemate.Name;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents a Group in the group list.
+ */
 public class Group {
+
+    // temporary
+    private final Name name;
+    private final Set<CourseMate> members = new HashSet<>();
+
+    public Group(Name name, Set<CourseMate> members) {
+        this.name = name;
+        this.members.addAll(members);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    /**
+     * Returns an immutable coursemate set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<CourseMate> getMembers() {
+        return Collections.unmodifiableSet(members);
+    }
 }
---
+++ src/main/java/seedu/address/ui/GroupCard.java
@@ -0,0 +1,40 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.group.Group;
+
+import java.util.Comparator;
+
+/**
+ * An UI component that displays information of a {@code Group}.
+ */
+public class GroupCard extends UiPart<Region> {
+
+    private static final String FXML = "GroupListCard.fxml";
+
+    private final Group group;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private FlowPane groupMembers;
+
+    /**
+     * Creates a {@code GroupCard} with the given {@code Group}.
+     * @param group
+     */
+    public GroupCard(Group group) {
+        super(FXML);
+        this.group = group;
+        name.setText(group.getName().fullName);
+        group.getMembers().stream()
+                .sorted(Comparator.comparing(courseMate -> courseMate.getName().fullName))
+                .forEach(courseMate -> groupMembers.getChildren().add(new Label(courseMate.getName().fullName)));
+    }
+}
