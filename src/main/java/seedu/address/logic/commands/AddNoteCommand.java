fba4dade8a26169a869463e43b0054dc894086bb
tengcharmaine <e0969596@u.nus.edu>
2024-03-16 16:06:49 +0800

    Update main body of the addnote command class

--- src/main/java/seedu/address/logic/commands/AddNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/AddNoteCommand.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +12,18 @@ public class AddNoteCommand extends Command {
 
     public static final String COMMAND_WORD = "addnote";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the note of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_NOTE + "[REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_NOTE + "Healthy.";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Addnote command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from addnote");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
