d2f117abe56f2772fb4f3d50d8743d4a7a7e03b8
jskimdev <kim.junseo1128@gmail.com>
2024-03-18 06:11:04 +0800

    Add testcases for sex field

---
+++ src/test/java/seedu/address/model/person/SexTest.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class SexTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Sex(null));
+    }
+
+    @Test
+    public void constructor_invalidSex_throwsIllegalArgumentException() {
+        String invalidSex = "";
+        assertThrows(IllegalArgumentException.class, () -> new Sex(invalidSex));
+    }
+
+    @Test
+    public void isValidSex() {
+        // null sex
+        assertThrows(NullPointerException.class, () -> Sex.isValidSex(null));
+
+        // invalid sex
+        assertFalse(Sex.isValidSex("")); // empty string
+        assertFalse(Sex.isValidSex(" ")); // spaces only
+        assertFalse(Sex.isValidSex("akldjkldf")); // random string
+        assertFalse(Sex.isValidSex("Malet")); // similar to valid string
+
+        // valid sex
+        assertTrue(Sex.isValidSex("Male"));
+        assertTrue(Sex.isValidSex("Female"));
+    }
+}
