cebf7cc73ae82b7164799385ea79f1119adc51e5
aureliony <39163684+aureliony@users.noreply.github.com>
2024-03-17 16:44:33 +0800

    Update javadocs

--- src/main/java/seedu/address/model/person/PersonSearchPredicate.java
+++ src/main/java/seedu/address/model/person/PersonSearchPredicate.java
@@ -7,7 +7,7 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code Name} or {@code Tags} matches any of the keywords given.
  */
 public class PersonSearchPredicate implements Predicate<Person> {
     private final List<String> keywords;
