84d8a9eda8a100a7b9bf178ef77ad2727b227c7f
aureliony <39163684+aureliony@users.noreply.github.com>
2024-03-17 16:45:07 +0800

    Add empty line after class declaration

--- src/main/java/seedu/address/model/person/PersonSearchPredicate.java
+++ src/main/java/seedu/address/model/person/PersonSearchPredicate.java
@@ -10,6 +10,7 @@ import seedu.address.commons.util.ToStringBuilder;
  * Tests that a {@code Person}'s {@code Name} or {@code Tags} matches any of the keywords given.
  */
 public class PersonSearchPredicate implements Predicate<Person> {
+
     private final List<String> keywords;
 
     public PersonSearchPredicate(List<String> keywords) {
