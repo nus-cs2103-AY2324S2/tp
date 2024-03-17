751288fd745b369aa271efe2292b06f5ac19af45
Archit <architg.sch@gmail.com>
2024-03-17 23:20:46 +0800

    Create tests for appt exceptions

--- src/main/java/seedu/address/model/appointment/exceptions/AppointmentNotFoundException.java
+++ src/main/java/seedu/address/model/appointment/exceptions/AppointmentNotFoundException.java
@@ -3,4 +3,8 @@ package seedu.address.model.appointment.exceptions;
 /**
  * Signals that the operation is unable to find the specified person.
  */
-public class AppointmentNotFoundException extends RuntimeException {}
+public class AppointmentNotFoundException extends RuntimeException {
+    public AppointmentNotFoundException() {
+        super("Unable to locate appointment");
+    }
+}
