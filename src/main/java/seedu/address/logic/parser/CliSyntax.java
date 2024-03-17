e1137c2439b7016fe73935058585f43160d578d8
Chua Zi Long <e0957913@u.nus.edu>
2024-03-17 19:19:42 +0800

    Fix code to pass checkstyle

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,7 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_FILENAME = new Prefix("f/");
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final Prefix PREFIX_FILENAME = new Prefix("f/");
 
 }
