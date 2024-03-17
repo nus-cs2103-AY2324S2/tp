ea35b8b7b86a0e392e9f5302cbf189a9d2245d03
aureliony <39163684+aureliony@users.noreply.github.com>
2024-03-17 17:18:38 +0800

    Update DG and delete unused test data

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -13,7 +13,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.storage.UniquePersonListDeserializer;
 
 /**
  * A list of persons that enforces uniqueness between its elements and does not allow nulls.
