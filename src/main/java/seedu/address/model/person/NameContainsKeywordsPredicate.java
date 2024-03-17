3244cc5bc516e5392d494bfae5c2db26db6693c9
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-03-17 20:01:38 +0800

    Allow the find command to be able to search the Address, Email, Name, phone number and tags

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -2,6 +2,7 @@ package seedu.address.model.person;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
@@ -12,14 +13,29 @@ import seedu.address.commons.util.ToStringBuilder;
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
 
+
     public NameContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Person person) {
+        Predicate<String> personName = keyword -> StringUtil.containsWordIgnoreCase(person.getName().toString(), keyword);
+        Predicate<String> personNumber = keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().toString(), keyword);
+        Predicate<String> personAddress = keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().toString(), keyword);
+        Predicate<String> personEmail = keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().toString(), keyword);
+        Predicate<String> personTag = keyword -> StringUtil.containsWordIgnoreCase(person.getTags().stream()
+                .map(tag -> tag.tagName).collect(Collectors.joining()), keyword);
+        //System.out.println(person.getName());
+        //Set<Tag> tags = person.getTags();
+        //for (Tag tag : tags) {
+        //    System.out.println(tag.tagName);
+        //}
+        //System.out.println();
+        //System.out.println(person.getTags().stream().map(tag -> tag.tagName));
+        Predicate<String> combinedCondition = personName.or(personNumber).or(personAddress).or(personEmail).or(personTag);
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(combinedCondition);
     }
 
     @Override
