a599d813b3b96f489d7137c432399073354f0bb4
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-17 15:43:44 +0800

    Delete redundant import

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -13,7 +13,6 @@ import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
