1e04e768c382a0bd38551a62e0339d36b228bae3
yisiox <yisiong.y22@gmail.com>
2024-03-18 02:00:14 +0800

    Fix tests broken by asset

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -86,7 +86,8 @@ public class PersonTest {
     @Test
     public void toStringMethod() {
         String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
-                + ", email=" + ALICE.getEmail() + ", address=" + ALICE.getAddress() + ", tags=" + ALICE.getTags() + "}";
+                + ", email=" + ALICE.getEmail() + ", address=" + ALICE.getAddress() + ", tags=" + ALICE.getTags()
+                + ", assets=" + ALICE.getAssets() + "}";
         assertEquals(expected, ALICE.toString());
     }
 }
