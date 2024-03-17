d30b11101c2493634217abbfc25f0fa0181f5cac
Archit <architg.sch@gmail.com>
2024-03-18 01:01:33 +0800

    Update ParserUtilTest

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.logic.parser.ParserUtil.parseAppointmentDate;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
@@ -14,6 +15,7 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.AppointmentDate;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -193,4 +195,26 @@ public class ParserUtilTest {
 
         assertEquals(expectedTagSet, actualTagSet);
     }
+
+    @Test
+    public void parseAppointmentDate_validDate_returnsAppointmentDate() throws ParseException{
+        String date = "2024-09-02";
+        AppointmentDate ad = new AppointmentDate(date);
+        assertEquals(ad, ParserUtil.parseAppointmentDate(date));
+    }
+
+    @Test
+    public void parseAppointmentDate_invalidDate_throwsParseException() {
+        String date = "2024-00-00";
+        assertThrows(ParseException.class, () -> ParserUtil.parseAppointmentDate(date));
+    }
+
+    @Test
+    public void parseAppointmentDate_whitespacedDate_returnsAppointmentDate() throws ParseException {
+        String date = " 2024-09-02  ";
+        AppointmentDate ad = new AppointmentDate(date.strip());
+        assertEquals(ad, ParserUtil.parseAppointmentDate(date));
+    }
+
+    //TODO: test parseAppointmentDate with null input once caught
 }
