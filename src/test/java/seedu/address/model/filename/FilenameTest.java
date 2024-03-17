8b74f111836f316b10be49b82e67ac5c559e24ed
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-17 13:53:28 +0800

    Add filename test

---
+++ src/test/java/seedu/address/model/filename/FilenameTest.java
@@ -0,0 +1,32 @@
+package seedu.address.model.filename;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.tag.Tag;
+
+public class FilenameTest {
+    private static final String VALID_FILENAME = "hello";
+    private static final String VALID_TAG = "tag";
+
+    @Test
+    public void testEquals_sameObject_success() {
+        Filename filename = new Filename(VALID_FILENAME);
+        assertEquals(filename, filename);
+    }
+
+    @Test
+    public void testEquals_differentObjectType_failure() {
+        Filename filename = new Filename(VALID_FILENAME);
+        Tag tag = new Tag(VALID_TAG);
+        assertFalse(filename.equals(tag));
+    }
+
+    @Test
+    public void test_toString_success() {
+        Filename filename = new Filename(VALID_FILENAME);
+        assertEquals(VALID_FILENAME, filename.toString());
+    }
+}
