c252ba093c532b40a3b0b0b96552a6d0abb3b7d7
Howlong11 <e1093040@u.nus.edu>
2024-03-17 18:55:06 +0800

    Add Article classes
    Article classes are seperate from Person classes.
    
    Article class, and an ArticleBook is also created in order to save article in a different addressbook from existing addressbook.
    
    Doing so allows us to have more features related to articles compared to saving articles as an attribute to a person.

---
+++ src/main/java/seedu/address/model/article/Article.java
@@ -0,0 +1,82 @@
+package seedu.address.model.article;
+
+import java.time.LocalDateTime;
+
+/**
+ * Represents an article in the address book.
+ */
+public class Article {
+    private final String title;
+    private final String[] authors;
+    private final LocalDateTime publicationDate;
+    private final String[] source;
+    private final String category;
+    private enum Status {DRAFT, PUBLISHED, ARCHIVED}
+    private final Status status;
+
+    /**
+     * Constructs an Article object.
+     *
+     * @param title
+     * @param authors
+     * @param publicationDate
+     * @param source
+     * @param category
+     * @param status
+     */
+    public Article(String title, String[] authors, LocalDateTime publicationDate,
+                   String[] source, String category, Status status) {
+        this.title = title;
+        this.authors = authors;
+        this.publicationDate = publicationDate;
+        this.source = source;
+        this.category = category;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String[] getAuthors() {
+        return this.authors;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return this.publicationDate;
+    }
+
+    public String[] getSource() {
+        return this.source;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    /**
+     * Returns true if all the attributes of Article class are identical to the attributes of an existing Article.
+     *
+     * @param otherArticle
+     * @return
+     */
+    public boolean isSameArticle(Article otherArticle) {
+        if (otherArticle == this) {
+            return true;
+
+        /**
+         * Same authors may have many drafts of same article. If it is not draft and has same title and authors,
+         * consider it as same article
+         */
+        } else if (otherArticle.getStatus() != Status.DRAFT && this.getStatus() != Status.DRAFT
+                && otherArticle.getTitle().equals(this.title) && otherArticle.getAuthors() == this.authors) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
