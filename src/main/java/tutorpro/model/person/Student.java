c17bbf099ba36582eab76a45303a459ca4f2c8cc
Wyrkx <107403096+Wyrkx@users.noreply.github.com>
2024-03-17 21:10:42 +0800

    Fix styling

--- src/main/java/tutorpro/model/person/Student.java
+++ src/main/java/tutorpro/model/person/Student.java
@@ -1,13 +1,19 @@
 package tutorpro.model.person;
 
-import tutorpro.model.tag.Tag;
-
 import java.util.Set;
 
+import tutorpro.model.tag.Tag;
+
+/**
+ * Represents a Student in TutorPro.
+ */
 public class Student extends Person {
 
     public static final Tag STUDENT_TAG = new Tag("Student");
 
+    /**
+     * Every field must be present and not null.
+     */
     public Student(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
         addTags(STUDENT_TAG);
