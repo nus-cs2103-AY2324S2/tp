1f69b743faa7a41be4f2e9d4ad577e266c5c57df
GERARDJM018 <e0985922@u.nus.edu>
2024-03-17 20:52:36 +0800

    Update ListCommand.java
    Update the implementation of List Command.
    
    New feature to view list only the given type of person/contact.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -4,6 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.TypePredicate;
 
 /**
  * Lists all persons in the address book to the user.
@@ -14,11 +16,21 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Show list of the contacts with the given type.\n"
+            + "Parameters: TYPE\n"
+            + "Example: " + COMMAND_WORD + " housekeeper";
+    private final TypePredicate predicate;
+    public ListCommand() {
+        this.predicate = (TypePredicate) PREDICATE_SHOW_ALL_PERSONS;
+    }
 
+    public ListCommand(TypePredicate predicate) {
+        this.predicate = predicate;
+    }
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        model.updateFilteredPersonList(predicate);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
---
+++ src/main/java/seedu/address/model/person/TypePredicate.java
@@ -0,0 +1,50 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.ClearCommand;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class TypePredicate implements Predicate<Person> {
+    private final String type;
+
+    public TypePredicate(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        if (type.equals("housekeeper")) {
+            return person instanceof Housekeeper;
+        } else if (type.equals("client")) {
+            return person instanceof Client;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        TypePredicate otherNameContainsKeywordsPredicate = (TypePredicate) other;
+        return type.equals(otherNameContainsKeywordsPredicate.type);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("type", type).toString();
+    }
+}
