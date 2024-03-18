246afcd2220a393848da4a86b067307ed38e84f4
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-03-18 16:58:35 +0800

    Add parsing for RemarkCommand

--- src/main/java/seedu/findvisor/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/findvisor/logic/parser/RemarkCommandParser.java
@@ -1,11 +1,34 @@
 package seedu.findvisor.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.findvisor.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.findvisor.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.findvisor.commons.core.index.Index;
 import seedu.findvisor.logic.commands.RemarkCommand;
 import seedu.findvisor.logic.parser.exceptions.ParseException;
 
 public class RemarkCommandParser implements Parser<RemarkCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of RemarkCommand
+     * and returns a RemarkCommand object for execution.
+     * @throws ParseException if the user does not conform to the expected format
+     */
     public RemarkCommand parse(String args) throws ParseException {
-        return new RemarkCommand();
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), pe);
+        }
+
+        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+
+        return new RemarkCommand(index, remark);
     }
 }
