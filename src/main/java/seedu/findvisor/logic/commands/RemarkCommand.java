8c8a76bb0e2a1875f3aba530b3627ff347d8ac78
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-03-18 16:58:35 +0800

    Add parsing for RemarkCommand

--- src/main/java/seedu/findvisor/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/findvisor/logic/commands/RemarkCommand.java
@@ -1,5 +1,8 @@
 package seedu.findvisor.logic.commands;
 
+import static seedu.findvisor.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.findvisor.commons.core.index.Index;
 import seedu.findvisor.logic.commands.exceptions.CommandException;
 import seedu.findvisor.model.Model;
 
@@ -20,8 +23,37 @@ public class RemarkCommand extends Command {
 
     public static final String MESSAGE_WORK_IN_PROGRESS = "Command is still a work in progress.";
 
+    private final Index index;
+    private final String remark;
+
+    /**
+     * @param index of the person in the filtered person list to edit the remark
+     * @param remark new remark of the person
+     */
+    public RemarkCommand(Index index, String remark) {
+        requireAllNonNull(index, remark);
+
+        this.index = index;
+        this.remark = remark;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         throw new CommandException(MESSAGE_WORK_IN_PROGRESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RemarkCommand)) {
+            return false;
+        }
+
+        RemarkCommand otherRemarkCommand = (RemarkCommand) other;
+        return index.equals(otherRemarkCommand.index) && remark.equals(otherRemarkCommand.remark);
     }
 }
