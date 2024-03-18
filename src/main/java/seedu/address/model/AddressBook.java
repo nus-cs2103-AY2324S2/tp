21a838fc32355809ceb062e50769b1fea22ac940
Li_Zizhen <e1075569@u.nus.edu>
2024-03-18 15:38:55 +0800

    Fix minor style problems.

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.person.Id;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
 
