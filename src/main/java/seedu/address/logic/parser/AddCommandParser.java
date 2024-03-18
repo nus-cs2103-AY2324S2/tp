3c181e052730149d9d0ef03d4cfe5eaddca0881b
Lim Zi Jia <zijialim@gmail.com>
2024-03-18 13:42:24 +0800

    Added client and housekeeper prembles

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -24,7 +24,6 @@ import seedu.address.model.tag.Tag;
  * Parses input arguments and creates a new AddCommand object
  */
 public class AddCommandParser implements Parser<AddCommand> {
-
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
@@ -35,22 +34,31 @@ public class AddCommandParser implements Parser<AddCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
-                || !argMultimap.getPreamble().isEmpty()) {
+                || !preambleIsAllowed(argMultimap.getPreamble())) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+        String type = argMultimap.getPreamble();
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
         Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        //Person person = new Person(name, phone, email, address, tagList);
-        // I just changed the above line to the following line, @zijia u need to check if user input is a client or housekeeper
-        Client client = new Client(name, phone, email, address, tagList);
+        Person person;
+        switch (type) {
+            case "client":
+                person = new Client(name, phone, email, address, tagList);
+                break;
+            case "housekeeper":
+                person = new Housekeeper(name, phone, email, address, tagList);
+                break;
+            default:
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
 
-        return new AddCommand(client);
+        return new AddCommand(person);
     }
 
     /**
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+
 /**
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
@@ -12,4 +13,14 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
+    public static final String[] ALLOWED_PREAMBLES = new String[] {"client", "housekeeper"};
+
+    public static boolean preambleIsAllowed(String preamble) {
+        for (String s : ALLOWED_PREAMBLES) {
+            if (s.equals(preamble)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
