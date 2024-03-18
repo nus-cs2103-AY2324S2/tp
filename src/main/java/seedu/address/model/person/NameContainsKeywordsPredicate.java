fb6917a624f943cf11409b2534e142c5e8bf4f7a
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-03-18 23:51:07 +0800

    Minor style check

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
