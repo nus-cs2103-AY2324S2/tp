2bcd58ad6b8b27ac1584b39f7f60db3f465be848
shavonneg <>
2024-03-18 14:48:36 +0800

    Refactor classes to ensure proper viewing of orders
    This commit includes modifications to several classes to ensure that
    orders are properly managed within the application. Changes include
    updates to the AddressBook class to handle the retrieval of orders.
    Additionally, adjustments have been made to other relevant classes
    to integrate all changes.

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.order.Order;
+import seedu.address.model.order.OrderList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
 
@@ -17,6 +18,7 @@ import seedu.address.model.person.UniquePersonList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    private final OrderList orders;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -27,6 +29,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         persons = new UniquePersonList();
+        orders = new OrderList();
     }
 
     public AddressBook() {}
@@ -111,8 +114,7 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     @Override
     public ObservableList<Order> getOrderList() {
-        //PLEASE EDIT THIS
-        return null;
+        return orders.asUnmodifiableObservableList();
     }
 
     @Override
