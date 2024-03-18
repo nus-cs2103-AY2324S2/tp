bb44062b2a15185690944e7e8e036e17d68c1460
chinzhening <ning.chin03@gmail.com>
2024-03-18 10:45:08 +0800

    Add start time and end time and day to Appointment

--- src/test/java/seedu/address/model/person/AppointmentTest.java
+++ src/test/java/seedu/address/model/person/AppointmentTest.java
@@ -27,34 +27,37 @@ public class AppointmentTest {
         // invalid Appointments
         assertFalse(Appointment.isValidAppointment("")); // empty string
         assertFalse(Appointment.isValidAppointment(" ")); // does not match HH:mm DAY
-        assertFalse(Appointment.isValidAppointment("FRI")); // time missing
-        assertFalse(Appointment.isValidAppointment("23:15")); // day missing
-        assertFalse(Appointment.isValidAppointment("3:15 FRI")); // H digit missing
-        assertFalse(Appointment.isValidAppointment("3:15 fri")); // DAY not capitalised
-        assertFalse(Appointment.isValidAppointment("12:34 FOB")); // not a day of the week
-        assertFalse(Appointment.isValidAppointment("00:00FRI")); // no space between time and day
-        assertFalse(Appointment.isValidAppointment("0000FRI")); // no colon between hour and minute
-        assertFalse(Appointment.isValidAppointment("24:59 MON")); // HH not in range 00-23
-        assertFalse(Appointment.isValidAppointment("23:60 MON")); // mm not in range 00-59
-        assertFalse(Appointment.isValidAppointment("25:61 MON")); // invalid range for HH and mm
+        assertFalse(Appointment.isValidAppointment("FRI")); // times missing
+        assertFalse(Appointment.isValidAppointment("23:15-23:16")); // day missing
+        assertFalse(Appointment.isValidAppointment("3:15-4:30 FRI")); // H digit missing
+        assertFalse(Appointment.isValidAppointment("3:15-04:30 FRI")); // H digit missing
+        assertFalse(Appointment.isValidAppointment("03:15-04:30 fri")); // DAY not capitalised
+        assertFalse(Appointment.isValidAppointment("12:34-13:33 FOB")); // not a day of the week
+        assertFalse(Appointment.isValidAppointment("00:00-00:12FRI")); // no space between times and day
+        assertFalse(Appointment.isValidAppointment("0000-1234 FRI")); // no colon between hour and minute
+        assertFalse(Appointment.isValidAppointment("23:59-24:59 MON")); // HH not in range 00-23
+        assertFalse(Appointment.isValidAppointment("22:60-23:40 MON")); // mm not in range 00-59
+        assertFalse(Appointment.isValidAppointment("25:61-26:20 MON")); // invalid range for HH and mm
+        assertFalse(Appointment.isValidAppointment("22:30-26:70 MON")); // invalid range for HH and mm
+        assertFalse(Appointment.isValidAppointment("23:00-22:00 MON")); // start time not before end time
 
         // valid Appointments
-        assertTrue(Appointment.isValidAppointment("23:59 MON")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("13:59 TUE")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("03:59 WED")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("02:00 THU")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("12:00 FRI")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("23:59 SAT")); // matches HH:mm DAY
-        assertTrue(Appointment.isValidAppointment("13:30 SUN")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("23:59-23:59 MON")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("13:59-14:00 TUE")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("03:59-04:59 WED")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("02:00-03:00 THU")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("12:00-13:00 FRI")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("23:59-23:59 SAT")); // matches HH:mm DAY
+        assertTrue(Appointment.isValidAppointment("13:30-14:00 SUN")); // matches HH:mm DAY
 
     }
 
     @Test
     public void equals() {
-        Appointment appointment = new Appointment("13:30 SUN");
+        Appointment appointment = new Appointment("13:30-14:00 SUN");
 
         // same values -> returns true
-        assertTrue(appointment.equals(new Appointment("13:30 SUN")));
+        assertTrue(appointment.equals(new Appointment("13:30-14:00 SUN")));
 
         // same object -> returns true
         assertTrue(appointment.equals(appointment));
@@ -66,6 +69,8 @@ public class AppointmentTest {
         assertFalse(appointment.equals(5.0f));
 
         // different values -> returns false
-        assertFalse(appointment.equals(new Appointment("13:30 FRI")));
+        assertFalse(appointment.equals(new Appointment("13:30-14:30 FRI")));
+        // different values -> returns false
+        assertFalse(appointment.equals(new Appointment("13:30-14:30 SAT")));
     }
 }
