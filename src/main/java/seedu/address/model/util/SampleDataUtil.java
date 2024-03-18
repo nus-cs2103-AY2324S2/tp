d07801b7108ff78c4e4be11c40425d44fa945f86
jskimdev <kim.junseo1128@gmail.com>
2024-03-19 01:53:24 +0800

    Fix import issue

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -2,7 +2,13 @@ package seedu.address.model.util;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Sex;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
