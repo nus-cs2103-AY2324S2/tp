4e2fa1e820e168de60e43f39c8d57fbc2e1c8a82
NeoHW <neohaowei@gmail.com>
2024-03-18 14:59:43 +0800

    Update and add JavaDocs to DeleteTagsCommand

--- src/main/java/seedu/address/logic/commands/DeleteTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTagsCommand.java
@@ -28,7 +28,7 @@ public class DeleteTagsCommand extends Command {
     public static final String COMMAND_WORD = "deletet";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the specified tag to the patient identified "
-            + "by the index number used in the last person listing. "
+            + "by the index number used in the last patient listing. "
             + "Parameters: INDEX (must be a positive integer) "
             // + PREFIX_TAG + "TAG \n"
             + PREFIX_TAG + "[TAG]+ \n"
@@ -58,6 +58,13 @@ public class DeleteTagsCommand extends Command {
         this.editPatientDescriptor = new EditPatientDescriptor();
     }
 
+    /**
+     * Executes the delete tag command to delete one or more tags to the patient.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return A command result message indicating the success of the operation.
+     * @throws CommandException If there is an error executing the command.
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
@@ -119,6 +126,12 @@ public class DeleteTagsCommand extends Command {
         return new Pair<>(tagSet, commandOutcome.toString());
     }
 
+    /**
+     * Returns true if both delete tag commands have the same index and tags to delete.
+     *
+     * @param other Another object to compare to.
+     * @return True if the other object is an DeleteTagsCommand with the same index and tags to add.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
