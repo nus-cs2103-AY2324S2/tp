123ec8ffae5ca31dff505288aa128c4935910eef
Ty-stan0417 <yisheng0417@gmail.com>
2024-03-18 22:13:23 +0800

    Bug fixed about test cases

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -64,6 +64,8 @@ public class EditPersonDescriptorTest {
                 + editPersonDescriptor.getName().orElse(null) + ", phone="
                 + editPersonDescriptor.getPhone().orElse(null) + ", email="
                 + editPersonDescriptor.getEmail().orElse(null) + ", address="
+                + editPersonDescriptor.getMeritScore().orElse(null) + ", meritScore="
+                + editPersonDescriptor.getBookList().orElse(null) + ", bookTitle="
                 + editPersonDescriptor.getAddress().orElse(null) + ", tags="
                 + editPersonDescriptor.getTags().orElse(null) + "}";
         assertEquals(expected, editPersonDescriptor.toString());
