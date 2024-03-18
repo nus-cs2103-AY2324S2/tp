98c29e2135b380ed7dfd9794d2d3ee984d284c40
Yadunand Prem <yadunand@yadunut.com>
2024-03-18 13:09:43 +0800

    Remove storage deps from ModuleMap

--- src/main/java/seedu/address/model/module/ModuleMap.java
+++ src/main/java/seedu/address/model/module/ModuleMap.java
@@ -1,39 +1,25 @@
 package seedu.address.model.module;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-
-import seedu.address.commons.exceptions.DataLoadingException;
-import seedu.address.commons.util.JsonUtil;
 
 /**
  * Wrapper for a hashmap that stores all Module objects
  */
 public class ModuleMap implements ReadOnlyModuleMap {
-    public static final Path DEFAULT_MODULE_DATA_FILE = Paths.get("src/main/resources/module_data.json");
-    private HashMap<String, Module> moduleMap;
+    private final HashMap<ModuleCode, Module> moduleMap;
 
-    /**
-     * Initialises ModuleMap object, which stores a mapping from the module name to the Module object.
-     * @throws DataLoadingException Throws an error if there is an issue getting the module data.
-     */
-    public ModuleMap() throws DataLoadingException {
+    public ModuleMap() {
         this.moduleMap = new HashMap<>();
+    }
 
-        Optional<Module[]> optionalModuleArray = JsonUtil.readJsonArrayFile(DEFAULT_MODULE_DATA_FILE, Module[].class);
-        Module[] moduleArray = optionalModuleArray.orElse(new Module[]{});
-
-        for (Module module : moduleArray) {
-            this.moduleMap.put(module.getModuleCode().getCode(), module);
-        }
+    public void addModule(Module module) {
+        moduleMap.put(module.getModuleCode(), module);
     }
 
     @Override
-    public Map<String, Module> getModuleMap() {
+    public Map<ModuleCode, Module> getModuleMap() {
         return Collections.unmodifiableMap(moduleMap);
     }
 }
--- src/main/java/seedu/address/model/module/ReadOnlyModuleMap.java
+++ src/main/java/seedu/address/model/module/ReadOnlyModuleMap.java
@@ -3,5 +3,5 @@ package seedu.address.model.module;
 import java.util.Map;
 
 public interface ReadOnlyModuleMap {
-    Map<String, Module> getModuleMap();
+    Map<ModuleCode, Module> getModuleMap();
 }
