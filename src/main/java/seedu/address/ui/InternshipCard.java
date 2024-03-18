73430c58a0b34c4c024b1d105effde4d8153f480
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-03-19 04:40:22 +0800

    Remove unused import

--- src/main/java/seedu/address/ui/InternshipCard.java
+++ src/main/java/seedu/address/ui/InternshipCard.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
 import seedu.address.model.internship.ApplicationStatus;
 import seedu.address.model.internship.ApplicationStatus.StatusEnum;
 import seedu.address.model.internship.CompanyName;
