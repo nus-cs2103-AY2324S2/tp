9dda757ccf37fb63a40a5288b4113a51bfb9a52a
Yadunand Prem <yadunand@yadunut.com>
2024-03-18 13:08:38 +0800

    Add validation for Description and Title

--- src/main/java/seedu/address/model/module/Description.java
+++ src/main/java/seedu/address/model/module/Description.java
@@ -1,19 +1,25 @@
 package seedu.address.model.module;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents a Module's description in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidDescription(String)}
  */
 public class Description {
     private final String value;
+    public static final String MESSAGE_CONSTRAINTS = "";
 
     public Description(String value) {
+        requireNonNull(value);
+        checkArgument(isValidDescription(value), MESSAGE_CONSTRAINTS);
         this.value = value;
     }
 
     //    TODO(yadunut): Add validation for description
     public static boolean isValidDescription(String test) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return true;
     }
 
     public String getValue() {
--- src/main/java/seedu/address/model/module/Title.java
+++ src/main/java/seedu/address/model/module/Title.java
@@ -1,5 +1,8 @@
 package seedu.address.model.module;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents a Module's title in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidTitle(String)}
@@ -9,12 +12,14 @@ public class Title {
     private final String title;
 
     public Title(String title) {
+        requireNonNull(title);
+        checkArgument(isValidTitle(title), MESSAGE_CONSTRAINTS);
         this.title = title;
     }
 
     //    TODO(yadunut): Validate title
     public static boolean isValidTitle(String test) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return true;
     }
 
     public String getTitle() {
