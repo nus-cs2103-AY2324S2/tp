62d0b19bd0a6f8ebe931964cc864d4de9dd40dca
Joshy837 <joshuayipsujun@gmail.com>
2024-03-18 14:10:37 +0800

    Add SearchCommandTest testcases

---
+++ src/test/java/seedu/address/logic/commands/SearchCommandTest.java
@@ -0,0 +1,92 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.KeywordPredicate;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.testutil.TypicalPersons.*;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindCommand}.
+ */
+public class SearchCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        String firstKeyword = " ; name : Poochie";
+        ArgumentMultimap firstToken = ArgumentTokenizer.tokenize(firstKeyword, PREFIX_NAME);
+        String secondKeyword = " ; product : Dog Food";
+        ArgumentMultimap secondToken = ArgumentTokenizer.tokenize(secondKeyword, PREFIX_NAME);
+
+        KeywordPredicate firstPredicate = new KeywordPredicate(firstToken);
+        KeywordPredicate secondPredicate = new KeywordPredicate(secondToken);
+
+        SearchCommand findFirstCommand = new SearchCommand(firstPredicate);
+        SearchCommand findSecondCommand = new SearchCommand(secondPredicate);
+
+        // same object -> returns true
+        assertEquals(findFirstCommand, findFirstCommand);
+
+        // same values -> returns true
+        SearchCommand findFirstCommandCopy = new SearchCommand(firstPredicate);
+        assertEquals(findFirstCommand, findFirstCommandCopy);
+
+        // different types -> returns false
+        assertNotEquals(1, findFirstCommand);
+
+        // null -> returns false
+        assertNotEquals(null, findFirstCommand);
+
+        // different person -> returns false
+        assertNotEquals(findFirstCommand, findSecondCommand);
+    }
+
+    @Test
+    public void execute_zeroKeywords_noPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        KeywordPredicate predicate = preparePredicate(" ; name : Alicea");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void execute_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 4);
+        KeywordPredicate predicate = preparePredicate(" ; name : a");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ALICE, CARL, DANIEL, FIONA), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void toStringMethod() {
+        KeywordPredicate predicate = new KeywordPredicate(ArgumentTokenizer.tokenize("keyword"));
+        SearchCommand findCommand = new SearchCommand(predicate);
+        String expected = SearchCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
+        assertEquals(expected, findCommand.toString());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code KeywordPredicate}.
+     */
+    private KeywordPredicate preparePredicate(String userInput) {
+        return new KeywordPredicate(ArgumentTokenizer.tokenize(userInput, PREFIX_NAME));
+    }
+}
