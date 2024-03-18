25bdcac4f211c5e3fb39c9e098657218fb757102
radeon2525 <e1070572@u.nus.edu>
2024-03-18 22:51:27 +0800

    Fix import statement ordering

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static java.util.Objects.requireNonNull;
 
 /**
  * Reverts the {@code model}'s address book to its previous state.
