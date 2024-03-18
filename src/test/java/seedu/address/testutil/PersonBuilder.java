4c57903101829efc4b1ca5de75bba09b4ef4f4dc
redcolorbicycle <liuzhengyang10@gmail.com>
2024-03-18 17:16:13 +0800

    Updated checkstyle

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -26,7 +26,7 @@ public class PersonBuilder {
     private Name name;
     private Phone phone;
     private Email email;
-    private Address address;
+    private String housingType;
     private PostalCode postalCode;
     private Set<Tag> tags;
 
@@ -37,8 +37,7 @@ public class PersonBuilder {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
-        address = new Address(DEFAULT_ADDRESS);
-        postalCode = new PostalCode(DEFAULT_POSTALCODE);
+        housingType = DEFAULT_ADDRESS;
         tags = new HashSet<>();
     }
 
@@ -49,8 +48,7 @@ public class PersonBuilder {
         name = personToCopy.getName();
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
-        address = personToCopy.getAddress();
-        postalCode = personToCopy.getPostalCode();
+        housingType = personToCopy.getHousingType();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -73,8 +71,8 @@ public class PersonBuilder {
     /**
      * Sets the {@code Address} of the {@code Person} that we are building.
      */
-    public PersonBuilder withAddress(String address) {
-        this.address = new Address(address);
+    public PersonBuilder withHousingType(String housingType) {
+        this.housingType = housingType;
         return this;
     }
 
@@ -103,7 +101,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, postalCode, tags);
+        return new Person(name, phone, email, housingType, tags);
     }
 
 }
