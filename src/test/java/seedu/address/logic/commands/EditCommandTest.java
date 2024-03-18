3f0bdb78fcaa32556dc23c839493dd4ff8f8d51c
jinhanfromNUS <e1083004@u.nus.edu>
2024-03-18 20:15:24 +0800

    Fix checkstyle and bugs

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -65,7 +65,7 @@ public class EditCommandTest {
 
         PersonBuilder personInList = new PersonBuilder(lastPerson);
         Person editedPerson =personInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                        .withTags(VALID_TAG_HUSBAND).build();
+                .withTags(VALID_TAG_HUSBAND).build();
 
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
