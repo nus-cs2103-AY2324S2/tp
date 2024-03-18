044970482df3b31ff6aa55c541e19b1206c0f0a6
MarcusGitty <igndunno@gmail.com>
2024-03-18 12:31:07 +0800

    Create the ViewCommand Class

---
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Views the Summary Stats of the address book.
+ */
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": shows the summary stats"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Viewing the stats of students";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return new CommandResult("Skeleton code for ViewCommand");
+    }
+
+
+}
---
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ViewCommand ;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ViewCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ViewCommand
+     * and returns a ViewCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public Command parse(String arguments) throws ParseException {
+        if (arguments.trim().equals("-all")) {
+            return new ListCommand();
+        } else if (arguments.trim().equals("-statistics")) {
+            return new ViewCommand();
+        } else {
+            throw new ParseException("Invalid arguments for 'view' command");
+        }
+    }
+    
+}
