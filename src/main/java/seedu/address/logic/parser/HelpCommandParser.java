1e64af0267aaf0af7a9bff51fd06e16702c12540
Rick <rick.kohjiaxuan@gmail.com>
2024-03-18 20:42:21 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -3,19 +3,21 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new HelpCommand object
+ */
 public class HelpCommandParser implements Parser<HelpCommand> {
 
-  /**
-   * Parses the given {@code String} of arguments in the context of the
-   * HelpCommand and returns a HelpCommand object for execution.
-   *
-   * @throws ParseException if the user input does not conform the expected format
-   */
-  @Override
-  public HelpCommand parse(String args) throws ParseException {
-    String commandKeyword = args.trim();
-
-    return new HelpCommand(commandKeyword);
-  }
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * HelpCommand and returns a HelpCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public HelpCommand parse(String args) throws ParseException {
+        String commandKeyword = args.trim();
+        return new HelpCommand(commandKeyword);
+    }
 
 }
