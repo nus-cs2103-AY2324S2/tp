f77a61cd5ee5d251e755a9f8ed8152427e09b44c
nigel <nigellim27022001@gmail.com>
2024-03-18 13:29:27 +0800

    FIx styleCheck

--- src/test/java/seedu/address/model/order/OrderListTest.java
+++ src/test/java/seedu/address/model/order/OrderListTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalOrders.CUPCAKES_AND_COOKIES;
 import static seedu.address.testutil.TypicalOrders.CUPCAKES_ONLY;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 
