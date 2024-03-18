6576cc1c2364d3468e0c922caf7b6b5bd4874b40
DESKTOP-5H6OF5G\Owen <owenong@gmail.com>
2024-03-18 18:25:22 +0800

    Added functionality for Import command, and sample import template

---
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.ImportCommand.MESSAGE_SUCCESS;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ImportCommand.
+ */
+public class ImportCommandTest {
+
+}
