125c9126934e4aa3170fa2c8b8d6ccaafddec549
Tsenrae <E0957554@u.nus.edu>
2024-03-18 23:19:45 +0800

    Fix test cases and original addressbook.json file to include attributes

--- src/test/java/seedu/address/testutil/TypicalPersonsUuid.java
+++ src/test/java/seedu/address/testutil/TypicalPersonsUuid.java
@@ -1,5 +1,16 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +30,10 @@ public class TypicalPersonsUuid {
     public static final String ELLE_UUID = "00000000-0000-0000-0000-000000000005";
     public static final String FIONA_UUID = "00000000-0000-0000-0000-000000000006";
     public static final String GEORGE_UUID = "00000000-0000-0000-0000-000000000007";
+    public static final String HOON_UUID = "00000000-0000-0000-0000-000000000008";
+    public static final String IDA_UUID = "00000000-0000-0000-0000-000000000009";
+    public static final String AMY_UUID = "00000000-0000-0000-0000-000000000010";
+    public static final String BOB_UUID = "00000000-0000-0000-0000-000000000011";
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
@@ -37,6 +52,18 @@ public class TypicalPersonsUuid {
             .withEmail("lydia@example.com").withAddress("little tokyo").buildWithUuid(FIONA_UUID);
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").buildWithUuid(GEORGE_UUID);
+
+    // Manually added
+    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
+            .withEmail("stefan@example.com").withAddress("little india").buildWithUuid(HOON_UUID);
+    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
+            .withEmail("hans@example.com").withAddress("chicago ave").buildWithUuid(IDA_UUID);
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).buildWithUuid(AMY_UUID);
+    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).buildWithUuid(BOB_UUID);
 
     private TypicalPersonsUuid() {} // prevents instantiation
 
