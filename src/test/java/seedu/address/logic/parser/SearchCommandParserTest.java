6678994b642b986ac05091e2f1e0be13fba2b84a
Joshy837 <joshuayipsujun@gmail.com>
2024-03-18 12:44:18 +0800

    Add SearchCommandParserTest tests

---
+++ src/test/java/seedu/address/logic/parser/SearchCommandParserTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.SearchCommand;
+import seedu.address.model.person.KeywordPredicate;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class SearchCommandParserTest {
+
+    private SearchCommandParser parser = new SearchCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, " ;   name :   Poochie", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        String keyword = " ; name : Poochie";
+        ArgumentMultimap token = ArgumentTokenizer.tokenize(keyword, PREFIX_NAME);
+
+        // no leading and trailing whitespaces
+        SearchCommand expectedSearchCommand =
+                new SearchCommand(new KeywordPredicate(token));
+        assertParseSuccess(parser, keyword, expectedSearchCommand);
+    }
+}
