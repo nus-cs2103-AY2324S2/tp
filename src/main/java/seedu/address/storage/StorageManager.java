9c580c81e94dac884bac95adbb29a5db9b3c058f
Ko-Khan <e0970313@u.nus.edu>
2024-03-18 22:00:29 +0800

    Update documentation

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -13,7 +13,7 @@ import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 
 /**
- * Manages storage of AddressBook data in local storage.
+ * Manages storage of AddressBook and ArticleBook data in local storage.
  */
 public class StorageManager implements Storage {
 
@@ -24,7 +24,8 @@ public class StorageManager implements Storage {
     private ArticleBookStorage articleBookStorage;
 
     /**
-     * Creates a {@code StorageManager} with the given {@code AddressBookStorage} and {@code UserPrefStorage}.
+     * Creates a {@code StorageManager} with the given {@code AddressBookStorage} and {@code UserPrefStorage}
+     * and {@code ArticleBookStorage}.
      */
     public StorageManager(AddressBookStorage addressBookStorage, UserPrefsStorage userPrefsStorage,
                           ArticleBookStorage articleBookStorage) {
