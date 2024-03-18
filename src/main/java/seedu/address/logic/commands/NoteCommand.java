5a2fc937fdca1e286c18fa28753abeac801f0fa5
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-03-18 15:04:26 +0800

    Note command takes in params

---
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Note;
+
+// test "/note ; name : Poochie ; note : ryan eat"
+
+/**
+ * Adds a note of an existing person in the address book.
+ */
+public class NoteCommand extends Command {
+
+    public static final String MESSAGE_ARGUMENTS = "Name: %1$s, Remark: %2$s";
+
+    private final Name name;
+    private final String note;
+    public static final String COMMAND_WORD = "/note";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds note to person.\n"
+            + "Parameters: "
+            + PREFIX_NAME + "NAME, "
+            + PREFIX_NOTE + "NOTE "
+            + "Example: " + COMMAND_WORD + " Moochie" + " Meet at 6pm tuesday";
+    /**
+     * @param name of the person in the filtered person list to edit the note
+     * @param note of the person to be updated to
+     */
+    public NoteCommand(Name name, String note) {
+        requireAllNonNull(name, note);
+
+        this.name = name;
+        this.note = note;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(
+                String.format(MESSAGE_ARGUMENTS, name, note));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NoteCommand)) {
+            return false;
+        }
+
+        NoteCommand e = (NoteCommand) other;
+        return name.equals(e.name)
+                && note.equals(e.note);
+    }
+}
+
