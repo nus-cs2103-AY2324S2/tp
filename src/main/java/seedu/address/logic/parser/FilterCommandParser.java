9ad4863894c1cb2aeea85347d5267f1dad26b126
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-03-18 20:15:02 +0800

    Edited FilterCommandParser.java

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,16 +1,12 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
 
-import java.util.Set;
-
+import java.util.Arrays;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 /**
  * Parses input arguments and creates a new FilterCommand object
@@ -24,6 +20,14 @@ public class FilterCommandParser {
      */
 
     public FilterCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
 
+        String[] tagKeywords = trimmedArgs.split("\\s+");
+
+        return new FilterCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
     }
 }
