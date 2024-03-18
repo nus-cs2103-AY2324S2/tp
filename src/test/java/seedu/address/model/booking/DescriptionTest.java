9c7d833702064d572c624dcc2bacfb88f900be27
Joseph Chan <cyqjoseph@gmail.com>
2024-03-19 00:58:12 +0800

    Add test cases for StartTime and EndTime

--- src/test/java/seedu/address/model/booking/DescriptionTest.java
+++ src/test/java/seedu/address/model/booking/DescriptionTest.java
@@ -1,11 +1,12 @@
 package seedu.address.model.booking;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.person.Name;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Name;
 
 public class DescriptionTest {
     @Test
