db7c9a0b5ba6749191d93d477a578d2b5935dddf
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-03-19 15:21:44 +0800

    Fix build bug

--- src/main/java/seedu/address/logic/parser/NoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NoteCommandParser.java
@@ -21,7 +21,6 @@ public class NoteCommandParser implements Parser<NoteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public NoteCommand parse(String args) throws ParseException {
-        // requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
                 PREFIX_NAME, PREFIX_NOTE);
         Name name;
@@ -29,7 +28,6 @@ public class NoteCommandParser implements Parser<NoteCommand> {
         try {
             name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
             note = ParserUtil.parseNote(argMultimap.getValue(PREFIX_NOTE).get());
-            //note = argMultimap.getValue(PREFIX_NOTE).orElse("");
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     NoteCommand.MESSAGE_USAGE), ive);
