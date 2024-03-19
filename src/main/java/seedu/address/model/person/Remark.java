91ba862fcf467c732ac6eab4ac5328104f55dfdb
jocelyn-soh <s.sohjoce@gmail.com>
2024-03-19 14:10:39 +0800

    Update Remark.java to remove unnecessary checking

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,14 +1,12 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a remark for a Person in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidRemark(String)}
  */
 public class Remark {
-    public static final String MESSAGE_CONSTRAINTS = "Remark should be alphanumeric or empty";
 
     public final String value;
 
@@ -20,22 +18,18 @@ public class Remark {
      */
     public Remark(String remark) {
         requireNonNull(remark);
-            checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
-            this.value = remark;
+        this.value = remark;
     }
-    
     /**
      * Returns true if a given string is a valid remark.
      */
     public static boolean isValidRemark(String test) {
         return true;
     }
-
     @Override
     public String toString() {
         return value;
     }
-
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -50,7 +44,6 @@ public class Remark {
         Remark otherRemark = (Remark) other;
         return this.value.equals(otherRemark.value);
     }
-
     @Override
     public int hashCode() {
         return value.hashCode();
