131f27575b1dc9d77c8ce9d5a8f1a2cf7b61f3e1
0-yibai <2802756281@qq.com>
2024-03-19 16:48:03 +0800

    Fix missing import for order test

--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.order;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalOrders.COOKIES_ONLY;
