5fb8458c4169f01cd8a53f52673b2b0b542de285
Ikhoon <e0908299@u.nus.edu>
2024-03-19 13:36:49 +0800

    addcommand output string bug fixed

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -33,7 +33,6 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_COUNTRY + "SG "
-            + PREFIX_STATUS + "ACCEPTED"
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
