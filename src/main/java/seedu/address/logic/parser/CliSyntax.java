f4bda967f79a3ff6a6181812670a304e10669093
Chin Rui Bin <52031118+camille-readbean@users.noreply.github.com>
2024-03-19 17:49:55 +0800

    Add puml and replace trailing space with '=' signs
    Add PlantUML for AddCommand sequence diagram.
    Still need to update user guide and developer guide.
    Replace command format with '=' for better UX.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,10 +6,9 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("--name");
-    public static final Prefix PREFIX_PHONE = new Prefix("--phone");
-    public static final Prefix PREFIX_EMAIL = new Prefix("--email");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("--addr");
-    public static final Prefix PREFIX_TAG = new Prefix("--tags");
-
+    public static final Prefix PREFIX_NAME = new Prefix("--name=");
+    public static final Prefix PREFIX_PHONE = new Prefix("--phone=");
+    public static final Prefix PREFIX_EMAIL = new Prefix("--email=");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("--addr=");
+    public static final Prefix PREFIX_TAG = new Prefix("--tags=");
 }
