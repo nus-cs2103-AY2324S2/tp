1b0424dfdb6e5edfe275c5bf2a0dd8dfef9b5b7b
Ragnapop <jinhunl@gmail.com>
2024-03-20 02:11:30 +0800

    Update GroupCommand.java

--- src/main/java/seedu/address/logic/commands/GroupCommand.java
+++ src/main/java/seedu/address/logic/commands/GroupCommand.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
