562fe424fe809661149576cc8dbb3ad04658e2bc
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-03-19 13:58:51 +0800

    Update src/main/java/seedu/address/logic/commands/DeleteCommand.java
    Co-authored-by: tankh99 <56617918+tankh99@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -24,7 +24,7 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Student: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Patient: %1$s";
 
     private final Index targetIndex;
 
