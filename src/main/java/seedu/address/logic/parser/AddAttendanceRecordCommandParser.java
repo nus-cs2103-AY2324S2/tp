d5553aeb6a5aeefd8b9f1ed04ea5e6a490ba378d
AndrewOng2066 <e0959899@u.nus.edu>
2024-03-19 19:29:39 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddAttendanceRecordCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAttendanceRecordCommandParser.java
@@ -13,8 +13,13 @@ import seedu.address.model.person.AttendanceDate;
 /**
  * Parses input arguments and creates a new AddAttendanceRecordCommand object
  */
-public class AddAttendanceRecordCommandParser  implements Parser<AddAttendanceRecordCommand> {
+public class AddAttendanceRecordCommandParser implements Parser<AddAttendanceRecordCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddAttendanceRecordCommand
+     * and returns an AddAttendanceRecordCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public AddAttendanceRecordCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ATTENDANCE_RECORD);
