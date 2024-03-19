52f8a0fd771be6c167448e650d09c2c37f1e3a71
Chee Jing Jie <jingjie.chee@u.nus.edu>
2024-03-19 14:39:19 +0800

    Update phone validation

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,8 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+        "Phone numbers should only contain numbers, starts with either '8' or '9' and be a total of 8 digits long";
+    public static final String VALIDATION_REGEX = "[89]\\d{7}";
     public final String value;
 
     /**
