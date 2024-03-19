ed24a0693635adddcc613cb71a0d5b764495f382
Abdul Khaliq <48424170+breezetall@users.noreply.github.com>
2024-03-19 22:12:15 +0800

    Remove unused imports

--- src/main/java/seedu/address/model/TaskList.java
+++ src/main/java/seedu/address/model/TaskList.java
@@ -3,7 +3,6 @@ package seedu.address.model;
 import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
