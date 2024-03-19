ec38dcb8cff1fad9c2e0049629193240b1530423
radeon2525 <e1070572@u.nus.edu>
2024-03-19 11:25:53 +0800

    Remove redundant methods

--- src/main/java/seedu/address/commons/util/InvalidationListenerManager.java
+++ src/main/java/seedu/address/commons/util/InvalidationListenerManager.java
@@ -13,38 +13,10 @@ public class InvalidationListenerManager {
     private final ArrayList<InvalidationListener> listeners = new ArrayList<>();
 
     /**
-     * Calls {@link InvalidationListener#invalidated(Observable)} on all added listeners.
-     * Any modifications to the listeners list during the invocation of this method
-     * will only take effect on the next invocation of this method.
-     *
-     * @param observable The {@code Observable} that became invalid.
+     * Adds a listener to the list of listeners.
      */
-    public void callListeners(Observable observable) {
-        // Make a copy of listeners such that any modifications to the listeners list during
-        // the invocation of this method will only take effect on the next invocation of this method.
-        ArrayList<InvalidationListener> listenersCopy = new ArrayList<>(listeners);
-
-        for (InvalidationListener listener : listenersCopy) {
-            listener.invalidated(observable);
-        }
-    }
-
-    /**
-     * Adds {@code listener} to the list of listeners.
-     * If the same listener is added more that once, then it will be notified more than once.
-     */
-    public void addListener(InvalidationListener listener) {
+    public void add(InvalidationListener listener) {
         requireNonNull(listener);
         listeners.add(listener);
-    }
-
-    /**
-     * Removes {@code listener} from the list of listeners.
-     * If the given listener was not previously added, then this method call is a no-op.
-     * If the given listener was added more than once, then only the first occurrence in the list will be removed.
-     */
-    public void removeListener(InvalidationListener listener) {
-        requireNonNull(listener);
-        listeners.remove(listener);
     }
 }
