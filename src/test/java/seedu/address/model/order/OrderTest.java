54273de4f7b951fb95c2c561524d2711e5a355a1
bertrandong <bertrandongchangheng@gmail.com>
2024-03-19 15:41:56 +0800

    Add OrderTest

--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -3,8 +3,9 @@ package seedu.address.model.order;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalOrders.*;
+import static seedu.address.testutil.TypicalOrders.COOKIES_ONLY;
+import static seedu.address.testutil.TypicalOrders.CUPCAKES_AND_COOKIES;
+import static seedu.address.testutil.TypicalOrders.CUPCAKES_ONLY;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
