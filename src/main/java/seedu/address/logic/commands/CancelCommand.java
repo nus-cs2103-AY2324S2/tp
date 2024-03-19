83bacea59e788729597486a796c8131ec53f2884
zhenghuil <e0959119@u.nus.edu>
2024-03-19 22:00:00 +0800

    Update checkstyle

--- src/main/java/seedu/address/logic/commands/CancelCommand.java
+++ src/main/java/seedu/address/logic/commands/CancelCommand.java
@@ -1,20 +1,20 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.booking.Booking;
 
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-
 /**
  * Deletes a booking identified using it's displayed index from the address book.
  */
 
-public class CancelCommand extends Command{
+public class CancelCommand extends Command {
 
     public static final String COMMAND_WORD = "cancel";
 
--- src/main/java/seedu/address/logic/parser/CancelCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CancelCommandParser.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CancelCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 /**
  * Parses input arguments and creates a new CancelCommand object
@@ -14,6 +14,7 @@ public class CancelCommandParser implements Parser<CancelCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the CancelCommand
      * and returns a CancelCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public CancelCommand parse(String args) throws ParseException {
