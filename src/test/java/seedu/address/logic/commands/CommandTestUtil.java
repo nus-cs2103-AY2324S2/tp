255672ab608c7b79b59a10ee2b1a3d0071ec1c57
GERARDJM018 <e0985922@u.nus.edu>
2024-03-19 22:40:46 +0800

    Update UserGuide.md

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -39,6 +39,8 @@ public class CommandTestUtil {
     public static final String VALID_TYPE_AMY = "client";
     public static final String VALID_TYPE_BOB = "housekeeper";
 
+    public static final String TYPE_DESC_AMY = " " + VALID_TYPE_AMY;
+
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
