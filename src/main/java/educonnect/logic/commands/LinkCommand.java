b04c683b39481b87f15174fbcb03bf576f073e6e
matthewken19 <148679013+matthewken19@users.noreply.github.com>
2024-03-19 21:12:15 +0800

    Update the import statement with respect to the refactoring of the Tag class

--- src/main/java/educonnect/logic/commands/LinkCommand.java
+++ src/main/java/educonnect/logic/commands/LinkCommand.java
@@ -21,7 +21,7 @@ import educonnect.model.student.Student;
 import educonnect.model.student.StudentId;
 import educonnect.model.student.TelegramHandle;
 import educonnect.model.student.timetable.Timetable;
-import educonnect.model.tag.Tag;
+import educonnect.model.student.Tag;
 
 
 
