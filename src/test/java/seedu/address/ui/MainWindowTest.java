60e651daa76194b3b59a4352eef1243cd468608b
whelan-low <e0958047@u.nus.edu>
2024-03-20 00:54:46 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -1,9 +1,9 @@
 package seedu.address.ui;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class MainWindowTest {
 
