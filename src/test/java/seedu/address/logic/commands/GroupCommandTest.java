240e09462b8521f8f0a1a14568cd0313c4131ac1
Ragnapop <jinhunl@gmail.com>
2024-03-20 00:53:07 +0800

    Update GroupCommandTest.java

--- src/test/java/seedu/address/logic/commands/GroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GroupCommandTest.java
@@ -16,14 +16,15 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.testutil.GroupPersonDescriptorBuilder;
+import seedu.address.testutil.PersonBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.*;
-import seedu.address.testutil.GroupPersonDescriptorBuilder;
-import seedu.address.testutil.PersonBuilder;
+import seedu.address.model.person.NusId;
+import seedu.address.model.person.Person;
 
 import java.util.Set;
 
