8fd02ef2a90862689984b911aaa6b4612384afb8
bryanyee33 <bryanyee33@gmail.com>
2024-03-20 10:04:33 +0800

    ModelManager::filteredPersons: Change to final

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -21,7 +21,7 @@ public class ModelManager implements Model {
 
     private final AddressBook addressBook;
     private final UserPrefs userPrefs;
-    private FilteredList<Person> filteredPersons;
+    private final FilteredList<Person> filteredPersons;
 
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
