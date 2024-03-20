2343e37014b620b4bfd5405997fbbca2a8d07dc8
ivan24004 <ivan@ivant.com>
2024-03-20 19:52:31 +0800

    Break long line in two

--- src/main/java/seedu/address/storage/JsonAdaptedGroup.java
+++ src/main/java/seedu/address/storage/JsonAdaptedGroup.java
@@ -48,7 +48,8 @@ public class JsonAdaptedGroup {
      */
     public Group toModelType() throws IllegalValueException {
         if (name == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+            throw new IllegalValueException(
+                    String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
         }
         if (!Name.isValidName(name)) {
             throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
