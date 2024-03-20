47decc6bdf8d75f415a1a325a9274036a225a14c
Sia Zhong Qing <zhongqing2001@gmail.com>
2024-03-20 20:26:39 +0800

    Correct checkstyle

--- src/test/java/seedu/address/logic/commands/FindNumCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindNumCommandTest.java
@@ -1,21 +1,25 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.person.PhoneContainsDigitsPredicate;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.GEORGE;
+import static seedu.address.testutil.TypicalPersons.getTypicalNetConnect;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.PhoneContainsDigitsPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for
--- src/test/java/seedu/address/model/person/PhoneContainsDigitsPredicateTest.java
+++ src/test/java/seedu/address/model/person/PhoneContainsDigitsPredicateTest.java
@@ -1,14 +1,18 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.testutil.PersonBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
 
 public class PhoneContainsDigitsPredicateTest {
 
@@ -43,7 +47,8 @@ public class PhoneContainsDigitsPredicateTest {
     @Test
     public void test_numberContainsDigits_returnsTrue() {
         // One keyword
-        PhoneContainsDigitsPredicate predicate = new PhoneContainsDigitsPredicate(Collections.singletonList("94351253"));
+        PhoneContainsDigitsPredicate predicate =
+                new PhoneContainsDigitsPredicate(Collections.singletonList("94351253"));
         assertTrue(predicate.test(new PersonBuilder().withPhone("94351253").build()));
     }
 
