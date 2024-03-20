d66087abf854491044db665ce164c3056940ad70
jeong-jaeho <e0960435@u.nus.edu>
2024-03-20 15:39:53 +0800

    Edit prompt messages and set preliminary confirmation and invalid input alert prompts

--- src/main/java/seedu/address/logic/commands/DeleteAllCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAllCommand.java
@@ -4,20 +4,30 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.ui.UiManager;
 
 /**
- * Clears the address book.
+ * Deletes all entries in the address book.
  */
 public class DeleteAllCommand extends Command {
 
     public static final String COMMAND_WORD = "delete-all";
+    public static final String CONFIRMATION =
+            "Are you sure you want to delete all? This action is irreversible. Enter Y/N:";
+    public static final String INVALID_INPUT_ALERT = "Invalid input format";
     public static final String MESSAGE_SUCCESS = "Successfully deleted all data";
 
-
+    /**
+     * Executes the command and returns the result message.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     */
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
