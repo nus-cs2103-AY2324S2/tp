282e9843830a82c4ce25ab736167219c1c9ffe8e
justinlengch <justinleng.tech@gmail.com>
2024-03-21 05:04:56 +0800

    Fixed checkstyle issues

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -14,7 +14,6 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Lesson;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Remark;
 import seedu.address.model.person.Subject;
 
 /**
