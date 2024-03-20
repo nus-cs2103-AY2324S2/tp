693572c41f631de97ac62bb77535f1cbce6c1d82
nichee <E0957568@u.nus.edu>
2024-03-20 13:57:20 +0800

    Implement Select command with stubs for data access

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SelectClassCommand;
 import seedu.address.logic.commands.ViewClassesCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -88,6 +89,9 @@ public class AddressBookParser {
 
         case AddAttendanceRecordCommand.COMMAND_WORD:
             return new AddAttendanceRecordCommandParser().parse(arguments);
+
+        case SelectClassCommand.COMMAND_WORD:
+                return new SelectClassCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
