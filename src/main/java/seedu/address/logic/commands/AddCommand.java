3ac949a823450ea4d65bfc2814597596a8c70ae2
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-03-20 21:36:18 +0800

    Fix string formatting

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,7 +37,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ROLE + "STUDENT "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_COURSE + "CS2103T"
+            + PREFIX_COURSE + "CS2103T "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
