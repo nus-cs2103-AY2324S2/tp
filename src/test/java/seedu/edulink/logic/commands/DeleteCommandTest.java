f411f9d5f2e012dcc68eb18d652b78bf12faed3e
ValenciaLim <valencialimrt@gmail.com>
2024-03-21 00:37:21 +0800

    Modify testcase in DeleteCommandTest to replace custom userInput with CommandTestUtils helper variable

--- src/test/java/seedu/edulink/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/edulink/logic/commands/DeleteCommandTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.edulink.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.edulink.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.edulink.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_ID_AMY;
 import static seedu.edulink.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.edulink.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.edulink.testutil.TypicalPersons.ALICE;
@@ -98,7 +99,7 @@ public class DeleteCommandTest {
 
     @Test
     public void execute_nonExistentStudentIdUnfilteredList_throwsCommandException() {
-        Id nonExistentStudentId = new Id("A1234567Z");
+        Id nonExistentStudentId = new Id(VALID_ID_AMY);
         DeleteCommand deleteCommand = new DeleteCommand(nonExistentStudentId);
         assertCommandFailure(deleteCommand, model, Messages.MESSAGE_STUDENT_NOT_FOUND);
     }
@@ -162,7 +163,7 @@ public class DeleteCommandTest {
     }
 
     @Test
-    public void toStringMethod() {
+    public void toStringMethodForIndex() {
         Index targetIndex = Index.fromOneBased(1);
         DeleteCommand deleteCommand = new DeleteCommand(targetIndex);
         String expected = DeleteCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
