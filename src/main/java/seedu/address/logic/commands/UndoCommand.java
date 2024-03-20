5318345b13e51c72a96840cfe09f47464322a093
bryanyee33 <bryanyee33@gmail.com>
2024-03-20 09:54:38 +0800

    Change Undo response messages

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -4,17 +4,17 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
- * Undoes the latest command
+ * Undoes the latest modifying command
  */
 public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the latest command.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the latest modifying command.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Undid the latest command.";
-    public static final String MESSAGE_UNDO_EXCEPTION = "No previous list to return to.";
+    public static final String MESSAGE_SUCCESS = "Undid the latest modifying command.";
+    public static final String MESSAGE_UNDO_EXCEPTION = "No modifying command to reverse.";
 
     @Override
     public String execute(Model model) throws CommandException {
