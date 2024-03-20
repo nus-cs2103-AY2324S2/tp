0cdf48d994be8e068f9483cb4dda087907979c3d
getsquared <articferretles@gmail.com>
2024-03-20 22:14:07 +0800

    Update reminders classes

---
+++ src/main/java/seedu/address/ui/RemindersCard.java
@@ -0,0 +1,33 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class RemindersCard extends UiPart<Region> {
+
+    private static final String FXML = "RemindersCard.fxml";
+
+    // public final RemindersList remindersList;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label title;
+    @FXML
+    private Label remindersCardList;
+
+    /**
+     * Creates a {@code ClientListCard} with the given {@code Person} and index to display.
+     */
+    public RemindersCard(String remindersTitle) {
+        super(FXML);
+        // this.remindersList = remindersList;
+        title.setText(remindersTitle);
+        // remindersCardList.setText(remindersList.toString());
+    }
+}
--- src/main/java/seedu/address/ui/RemindersPanel.java
+++ src/main/java/seedu/address/ui/RemindersPanel.java
@@ -1,49 +1,52 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
-
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.person.Person;
-
 /**
- * Panel containing the list of reminders.
+ * A UI Class that encapsulates {@code RemindersCard} for Last Met and Appointments.
+ * To make updating them together easier.
  */
 public class RemindersPanel {
-    private static final String FXML = "RemindersPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(RemindersPanel.class);
 
-    // @FXML
-    // private ListView<xxx> remindersListView;
+    private static final String LAST_MET_TITLE = "Last Met";
+    private static final String APPOINTMENTS_TITLE = "Appointments";
 
-//    /**
-//     * Creates a {@code RemindersPanel} with the given {@code ObservableList}.
-//     */
-//    public RemindersPanel(ObservableList<xxx> remindersList) {
-//        super(FXML);
-//        remindersListView.setItems(personList);
-//        remindersListView.setCellFactory(listView -> new ClientListViewCell());
-//    }
-//
-//    /**
-//     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code ClientListCard}.
-//     */
-//    class ClientListViewCell extends ListCell<Person> {
-//        @Override
-//        protected void updateItem(Person person, boolean empty) {
-//            super.updateItem(person, empty);
-//
-//            if (empty || person == null) {
-//                setGraphic(null);
-//                setText(null);
-//            } else {
-//                setGraphic(new ClientListCard(person, getIndex() + 1).getRoot());
-//            }
-//        }
-//    }
-    
+    private RemindersCard lastMetCard;
+    private RemindersCard appointmentsCard;
+
+    /**
+     * Creates a {@code RemindersPanel} with the given {@code }.
+     */
+    public RemindersPanel() {
+        this.lastMetCard = new RemindersCard(LAST_MET_TITLE);
+        this.appointmentsCard = new RemindersCard(APPOINTMENTS_TITLE);
+    }
+
+    public RemindersCard getLastMetCard() {
+        return lastMetCard;
+    }
+
+    public RemindersCard getAppointmentsCard() {
+        return appointmentsCard;
+    }
+
+    /**
+     * Updates the {@code LastMetCard} with new {@code }.
+     */
+    public void updateLastMetCard() {
+        lastMetCard = new RemindersCard(LAST_MET_TITLE);
+    }
+
+    /**
+     * Updates the {@code AppointmentsCard} with new {@code }
+     */
+    public void updateAppointmentsCard() {
+        appointmentsCard = new RemindersCard(APPOINTMENTS_TITLE);
+    }
+
+    /**
+     * Updates the {@code RemindersPanel} with new {@code }.
+     */
+    public void updateRemindersPanel() {
+        updateLastMetCard();
+        updateAppointmentsCard();
+    }
 }
