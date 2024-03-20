e377c0a3c5d7391735e9410cd9978094903f451b
radeon2525 <e1070572@u.nus.edu>
2024-03-20 19:42:25 +0800

    fix

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -16,6 +16,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.InvalidationListenerManager;
@@ -23,8 +25,6 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.testutil.PersonBuilder;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 
 public class AddressBookTest {
 
