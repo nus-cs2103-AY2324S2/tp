78ca56bade089652df3b9e6a25bd1dbb7ea421ae
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2024-03-20 15:39:22 +0800

    Add GroupCommandParser tests

--- src/main/java/seedu/address/logic/commands/GroupCommand.java
+++ src/main/java/seedu/address/logic/commands/GroupCommand.java
@@ -1,28 +1,15 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.group.Group;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Grade;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.StudentId;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENTID;
 
 public class GroupCommand extends Command {
@@ -43,6 +30,10 @@ public class GroupCommand extends Command {
     private final Set<Group> group; //set with one group. for compatibility with edit command.
     private final Set<StudentId> studentIds;
 
+    public static final String MESSAGE_GROUP_SUCCESS = "All students were added!";
+
+    private final String STUDENTS_NOT_FOUND = "The following IDs were not found (and not added to the group): ";
+
     /**
      * @param group A Set of type Group with only one Group.
      * @param studentIds A Set of studentIds with possibly multiple Ids.
@@ -56,14 +47,24 @@ public class GroupCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        String missingIds = "";
         requireNonNull(model);
         for (StudentId studentId : studentIds) {
             EditCommand.EditPersonDescriptor editPersonDescriptor = new EditCommand.EditPersonDescriptor();
             editPersonDescriptor.setGroups(group);
             EditCommand editCommand = new EditCommand(studentId, editPersonDescriptor);
-            editCommand.execute(model);
+            try {
+                editCommand.execute(model);
+            } catch (CommandException e) {
+                missingIds += studentId + " ";
+            }
+            //there's a dependency on EditCommand, but this for loop shouldn't cause EditCommand to throw any exception.
         }
-        return new CommandResult("---");
+        if (missingIds.equals("")) {
+            return new CommandResult(MESSAGE_GROUP_SUCCESS);
+        } else {
+            return new CommandResult(STUDENTS_NOT_FOUND + missingIds);
+        }
     }
 
     @Override
