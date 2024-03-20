b1587b04bf7f39677933b13606a34ad87c8e7202
ivan24004 <ivan@ivant.com>
2024-03-20 19:44:22 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/storage/JsonSerializableGroupListTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableGroupListTest.java
@@ -1,9 +1,12 @@
 package seedu.address.storage;
 
 import static seedu.address.testutil.Assert.assertThrows;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 
