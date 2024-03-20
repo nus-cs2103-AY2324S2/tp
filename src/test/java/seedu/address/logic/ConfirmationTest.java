0e8d29fbc6375e2b7123fff7683b2b0ed6b0df21
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-03-21 01:44:15 +0800

    Add unit test for Confirmation.java

---
+++ src/test/java/seedu/address/logic/ConfirmationTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.ListCommand;
+
+public class ConfirmationTest {
+    @Test
+    public void assertNoConfirmation() {
+        Confirmation confirmation;
+
+        confirmation = new Confirmation(new AddCommand(ALICE));
+        assertTrue(confirmation.isToProceed());
+
+        // different command -> return true
+        confirmation = new Confirmation((new ListCommand()));
+        assertTrue(confirmation.isToProceed());
+    }
+
+    @Test
+    public void assertConfirmation() {
+        int RANDOM_INDEX = 1;
+        Confirmation confirmation = new Confirmation(new DeleteCommand(Index.fromOneBased(RANDOM_INDEX)));
+        assertFalse(confirmation.isToProceed());
+    }
+}
