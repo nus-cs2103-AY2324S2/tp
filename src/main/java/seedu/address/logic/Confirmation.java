cca3a66399c5a90170e0f03fd5e65cb70b23ff53
SIMKIANSENG\simki <simkianseng28@gmail.com>
2024-03-21 01:51:16 +0800

    Update JavaDocs for Confirmation.java

--- src/main/java/seedu/address/logic/Confirmation.java
+++ src/main/java/seedu/address/logic/Confirmation.java
@@ -15,10 +15,21 @@ public class Confirmation {
         this.isToProceed = !hasConfirmation(command);
     }
 
+    /**
+     * Checks if command requires comfirmation before executing.
+     *
+     * @param command The command to be executed.
+     * @return True if command requires confirmation. False if otherwise.
+     */
     public boolean hasConfirmation(Command command) {
         return command instanceof DeleteCommand;
     }
 
+    /**
+     * Whether the confirmation is successful or not.
+     *
+     * @return True if the command can be executed. False if otherwise.
+     */
     public boolean isToProceed() {
         return isToProceed;
     }
