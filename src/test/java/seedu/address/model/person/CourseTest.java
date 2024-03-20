09f659c446441b0df8337d05d70dc756172ac1df
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2024-03-20 23:30:10 +0800

    Remove whitespaces

--- src/test/java/seedu/address/model/person/CourseTest.java
+++ src/test/java/seedu/address/model/person/CourseTest.java
@@ -7,7 +7,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class CourseTest {
-    
+
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Course(null));
