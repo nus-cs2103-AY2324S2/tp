2ebeb0478fd4f9bf3bd80703a809da85edd046e3
kailinteoo <e0959354@u.nus.edu>
2024-03-20 13:52:04 +0800

    Add Testcase for LogicManager.java

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -87,6 +87,11 @@ public class LogicManagerTest {
         assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredPersonList().remove(0));
     }
 
+    @Test
+    public void getFilteredOrderList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredOrderList().remove(0));
+    }
+
     /**
      * Executes the command and confirms that:
      * - no exceptions are thrown <br>
