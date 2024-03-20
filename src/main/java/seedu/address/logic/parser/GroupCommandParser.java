4a8b036fffd30671637e53bdd6a8388e5a3bf7d0
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2024-03-20 13:50:58 +0800

    Update GroupCommandParser

--- src/main/java/seedu/address/logic/parser/GroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GroupCommandParser.java
@@ -1,2 +1,44 @@
-package seedu.address.logic.parser;public class GroupCommandParser {
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.GroupCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.group.Group;
+import seedu.address.model.person.StudentId;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENTID;
+
+public class GroupCommandParser implements Parser<GroupCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an GroupCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public GroupCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_GROUP, PREFIX_STUDENTID);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_GROUP, PREFIX_STUDENTID)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, GroupCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_GROUP);
+        Group group = ParserUtil.parseGroup(argMultimap.getValue(PREFIX_GROUP).get());
+        Set<StudentId> studentIds = ParserUtil.parseStudentIds(argMultimap.getAllValues(PREFIX_STUDENTID));
+        return new GroupCommand(group, studentIds);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
