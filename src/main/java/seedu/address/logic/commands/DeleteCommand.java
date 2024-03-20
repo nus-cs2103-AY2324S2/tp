b45d898c778c65f9a1ab472c6d317c8b59591481
dillontkh <e0958762@u.nus.edu>
2024-03-21 00:40:45 +0800

    Remove redundant QR code deletion

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -42,7 +42,6 @@ public class DeleteCommand extends Command {
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
-        personToDelete.deleteQrCode();
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
 
