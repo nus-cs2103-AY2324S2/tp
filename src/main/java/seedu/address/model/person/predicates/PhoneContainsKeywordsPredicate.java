efd2b0a0030d19959bb37a0873adb8302ea0654e
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-03-21 05:13:21 +0800

    Implement Find command to handle additional specific parameter(s)

---
+++ src/main/java/seedu/address/model/person/predicates/PhoneContainsKeywordsPredicate.java
@@ -0,0 +1,21 @@
+package seedu.address.model.person.predicates;
+
+import java.util.List;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.person.Person;
+
+/**
+ * Tests that a {@code Person}'s {@code Phone} matches any of the keywords given.
+ */
+public class PhoneContainsKeywordsPredicate extends ContainsKeywordsPredicate {
+    public PhoneContainsKeywordsPredicate(List<String> keywords) {
+        super(keywords);
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return super.keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().toString(), keyword));
+    }
+}
