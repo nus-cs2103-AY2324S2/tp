882ba1bee4f811f9960a8e13f46d49b08899d152
dedsecrattle <itsprabxxx@gmail.com>
2024-03-20 22:52:07 +0800

    Change Package Name to seedu.edulink

---
+++ src/test/java/seedu/edulink/logic/commands/EditStudentDescriptorTest.java
@@ -0,0 +1,108 @@
+package seedu.edulink.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.edulink.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.edulink.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_ID_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_INTAKE_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_MAJOR_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.edulink.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.edulink.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.edulink.model.student.Address;
+import seedu.edulink.model.student.Email;
+import seedu.edulink.model.student.Id;
+import seedu.edulink.model.student.Intake;
+import seedu.edulink.model.student.Major;
+import seedu.edulink.model.student.Name;
+import seedu.edulink.model.student.Phone;
+import seedu.edulink.model.tag.Tag;
+import seedu.edulink.testutil.EditPersonDescriptorBuilder;
+import seedu.edulink.testutil.TypicalPersons;
+
+public class EditStudentDescriptorTest {
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        EditPersonDescriptor descriptorWithSameValues = new EditPersonDescriptor(DESC_AMY);
+        assertTrue(DESC_AMY.equals(descriptorWithSameValues));
+
+        // same object -> returns true
+        assertTrue(DESC_AMY.equals(DESC_AMY));
+
+        // null -> returns false
+        assertFalse(DESC_AMY.equals(null));
+
+        // different types -> returns false
+        assertFalse(DESC_AMY.equals(5));
+
+        // different values -> returns false
+        assertFalse(DESC_AMY.equals(DESC_BOB));
+
+        // different name -> returns false
+        EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different phone -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different email -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withEmail(VALID_EMAIL_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different address -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withAddress(VALID_ADDRESS_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different tags -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+    }
+
+    @Test
+    public void toStringMethod() {
+        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        String expected = EditPersonDescriptor.class.getCanonicalName()
+            + "{id=" + editPersonDescriptor.getName().orElse(null)
+            + ", name=" + editPersonDescriptor.getName().orElse(null) + ", phone="
+            + editPersonDescriptor.getPhone().orElse(null) + ", email="
+            + editPersonDescriptor.getEmail().orElse(null) + ", address="
+            + editPersonDescriptor.getAddress().orElse(null) + ", major="
+            + editPersonDescriptor.getMajor().orElse(null) + ", intake="
+            + editPersonDescriptor.getIntake().orElse(null) + ", tags="
+            + editPersonDescriptor.getTags().orElse(null) + "}";
+        assertEquals(expected, editPersonDescriptor.toString());
+    }
+
+    @Test
+    public void fromPerson() {
+        EditPersonDescriptor expected = new EditPersonDescriptor();
+        expected.setId(new Id(VALID_ID_BOB));
+        expected.setName(new Name(VALID_NAME_BOB));
+        expected.setMajor(new Major(VALID_MAJOR_BOB));
+        expected.setAddress(new Address(VALID_ADDRESS_BOB));
+        expected.setIntake(new Intake(VALID_INTAKE_BOB));
+        expected.setPhone(new Phone(VALID_PHONE_BOB));
+        expected.setEmail(new Email(VALID_EMAIL_BOB));
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag(VALID_TAG_FRIEND));
+        tags.add(new Tag(VALID_TAG_HUSBAND));
+        expected.setTags(tags);
+        EditPersonDescriptor test = new EditPersonDescriptor(TypicalPersons.BOB);
+        assertEquals(expected, test);
+    }
+}
