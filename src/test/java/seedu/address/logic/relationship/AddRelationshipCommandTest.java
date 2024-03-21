5995edf1e9e0345a759c2b62dcd196f46a0a433a
DingBao-sys <dinghaosim@gmail.com>
2024-03-22 00:58:34 +0800

    Add test coverage for AddRelationshipCommand

--- src/test/java/seedu/address/logic/relationship/AddRelationshipCommandTest.java
+++ src/test/java/seedu/address/logic/relationship/AddRelationshipCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.relationship;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersonsUuid.getTypicalAddressBook;
@@ -61,20 +63,60 @@ class AddRelationshipCommandTest {
         assertCommandFailure(addRelationshipCommand, model,
                 "The UUID provided is invalid.");
     }
+        @Test
+    void testExecute_AddingExistingRelationshipThrowsException() {
+        String testOriginUuid = "0001";
+        String testTargetUuid = "0002";
+        String relationshipDescriptor = "family";
+        AddRelationshipCommand addRelationshipCommand =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        UUID person1Uuid = UUID.fromString("00000000-0000-0000-0000-000000000001");
+        UUID person2Uuid =  UUID.fromString("00000000-0000-0000-0000-000000000002");
+        String FAMILY_RELATIONSHIP_DESCRIPTOR = "family";
+        model.addRelationship(new Relationship(person1Uuid, person2Uuid, FAMILY_RELATIONSHIP_DESCRIPTOR));
+        assertCommandFailure(addRelationshipCommand, model,
+                "Sorry, 00000000-0000-0000-0000-000000000001 and 00000000-0000-0000-0000-000000000002 are family");
+
+    }
+    @Test
+    void testExecute_AddInvalidRelationshipDescriptorThrowsException() {
+        String testOriginUuid = "0001";
+        String testTargetUuid = "0002";
+        String relationshipDescriptor = "spouse";
+        AddRelationshipCommand addRelationshipCommand =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        assertThrows(IllegalArgumentException.class, () -> addRelationshipCommand.execute(model));
+    }
+    @Test
+    void testEqualsMethodWithSameArguments() {
+        String testOriginUuid = "0001";
+        String testTargetUuid = "0002";
+        String relationshipDescriptor = "family";
+        AddRelationshipCommand test1 =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        AddRelationshipCommand test2 =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        assertEquals(test1.equals(test2), true);
+    }
+    @Test
+    void testEqualsMethodWithNotAddRelationshipCommandInstance() {
+        String testOriginUuid = "0001";
+        String testTargetUuid = "0002";
+        String relationshipDescriptor = "family";
+        AddRelationshipCommand test1 =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        String test2 = "test";
+        assertEquals(test1.equals(test2), false);
+    }
+    @Test
+    void testEqualsMethodWithSameInstance() {
+        String testOriginUuid = "0001";
+        String testTargetUuid = "0002";
+        String relationshipDescriptor = "family";
+        AddRelationshipCommand test1 =
+                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
+        assertEquals(test1.equals(test1), true);
+    }
 }
-//    @Test
-//    void testExecute_AddingExistingRelationshipThrowsException() {
-//        String testOriginUuid = "0001";
-//        String testTargetUuid = "0002";
-//        String relationshipDescriptor = "family";
-//        AddRelationshipCommand addRelationshipCommand =
-//                new AddRelationshipCommand(testOriginUuid, testTargetUuid, relationshipDescriptor);
-//        UUID PERSON_1_UUID = UUID.fromString("00000000-0000-0000-0000-000000000001");
-//        UUID PERSON_2_UUID =  UUID.fromString("00000000-0000-0000-0000-000000000002");
-//        String FAMILY_RELATIONSHIP_DESCRIPTOR = "family";
-//        model.addRelationship(new Relationship(PERSON_1_UUID, PERSON_2_UUID, FAMILY_RELATIONSHIP_DESCRIPTOR));
-//        assertCommandFailure(addRelationshipCommand, model,
-//                "Sorry, 00000000-0000-0000-0000-000000000001 and 00000000-0000-0000-0000-000000000002 are family");
-//
-//    }
+
 
