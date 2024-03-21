3db1af36c98cf63cc0a5cdb5a849816343d7b80f
Sherwyn Ng <>
2024-03-21 15:11:09 +0800

    removed applicationDate class

--- src/main/java/seedu/address/model/person/ApplicationDate.java
+++
@@ -1,6 +0,0 @@
-package seedu.address.model.person;
-
-public class ApplicationDate {
-    private static final String DATE_CONSTRAINTS =
-            "Application date should be in the format dd/mm/yyyy";
-}
