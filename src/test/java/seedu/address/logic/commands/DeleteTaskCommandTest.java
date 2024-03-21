654c81a12a7dc5f61ecfbc4bfc257053b22c6cb6
ananya21 <shahiananya50@gmail.com>
2024-03-21 22:41:58 +0800

    Fixed gradle checkstyle issue

--- src/test/java/seedu/address/logic/commands/DeleteTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTaskCommandTest.java
@@ -1,6 +1,12 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -8,18 +14,15 @@ import seedu.address.model.person.Person;
 import seedu.address.model.project.Task;
 import seedu.address.testutil.PersonBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 class DeleteTaskCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-    Task tempTask1 = new Task("rehearse");
-    Task tempTask2 = new Task("Write script");
+    private Task tempTask1 = new Task("rehearse");
+    private Task tempTask2 = new Task("Write script");
 
-    Person presentation = new PersonBuilder().withName("Presentation").build();
+    private Person presentation = new PersonBuilder().withName("Presentation").build();
 
-    Person codingProject = new PersonBuilder().withName("Coding Project").build();
+    private Person codingProject = new PersonBuilder().withName("Coding Project").build();
 
 
     @Test
