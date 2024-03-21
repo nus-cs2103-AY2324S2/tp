b43d47910776133e1ccb7610aec4d905fa8dfa49
AwJoey <awjoey26@gmail.com>
2024-03-21 11:03:14 +0800

    Remove unused import in Model, ModelManager, AddSchedCommand

--- src/main/java/seedu/address/logic/commands/AddSchedCommand.java
+++ src/main/java/seedu/address/logic/commands/AddSchedCommand.java
@@ -7,21 +7,14 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
 import seedu.address.model.schedule.Schedule;
-import seedu.address.model.tag.Tag;
 
 /**
  * Adds a schedule to the address book.
