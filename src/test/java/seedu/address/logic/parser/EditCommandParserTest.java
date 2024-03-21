5fda85259547862fdd340d5bcfe1936b3e735bce
kaiyi27 <e1070556@u.nus.edu>
2024-03-21 16:49:17 +0800

    Remove unused import statement

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -46,7 +46,6 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Relationship;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
