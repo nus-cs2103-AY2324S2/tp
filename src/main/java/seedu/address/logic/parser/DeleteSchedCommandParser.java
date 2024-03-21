bf997566c768a4faa4a9f937e90bd1518caa8bae
Morninggloryyy <jinyanwu10@gmail.com>
2024-03-21 20:11:20 +0800

    Fix style issues

--- src/main/java/seedu/address/logic/parser/DeleteSchedCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteSchedCommandParser.java
@@ -4,12 +4,12 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SPECIFIC_PARTICIPANTS;
 
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteSchedCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.ArrayList;
-import java.util.stream.Stream;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
