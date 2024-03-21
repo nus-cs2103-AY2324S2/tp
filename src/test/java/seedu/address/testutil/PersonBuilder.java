60a93b33649279aef0e9172845f9bd2e8b13b3fd
Joelwang22 <omegabeeminecraft@gmail.com>
2024-03-22 03:00:51 +0800

    Fix Imports

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,11 +3,11 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
+import seedu.address.model.order.util.SampleDataUtil;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.order.util.SampleDataUtil;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
