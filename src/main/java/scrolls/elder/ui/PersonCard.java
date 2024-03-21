30866660a05517efe3cb65f5453f6a0471912bae
Jia En <tujiaen@gmail.com>
2024-03-21 19:27:22 +0800

    Revert to displaying by list index

--- src/main/java/scrolls/elder/ui/PersonCard.java
+++ src/main/java/scrolls/elder/ui/PersonCard.java
@@ -47,10 +47,10 @@ public class PersonCard extends UiPart<Region> {
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
-    public PersonCard(Person person) {
+    public PersonCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
-        id.setText(String.valueOf(person.getId()));
+        id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
