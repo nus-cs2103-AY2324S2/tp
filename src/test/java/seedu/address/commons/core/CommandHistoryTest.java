4fab5845a66723feb53f7186e942a38df8e0414f
tankh99 <khanghou99@gmail.com>
2024-03-21 11:39:08 +0800

    Add test to CommandHistory

---
+++ src/test/java/seedu/address/commons/core/CommandHistoryTest.java
@@ -0,0 +1,47 @@
+package seedu.address.commons.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandHistoryTest {
+
+    private CommandHistory commandHistory;
+    @BeforeEach
+    void setUp() {
+        commandHistory = new CommandHistory();
+    }
+    @Test
+    void addCommandToHistory() {
+        commandHistory.addCommandToHistory("Test");
+        assertEquals("Test", commandHistory.undo());
+    }
+
+    @Test
+    void undo_undoConsecutively_returnsPrevCommand() {
+        commandHistory.addCommandToHistory("test1");
+        commandHistory.addCommandToHistory("test2");
+        commandHistory.addCommandToHistory("test3");
+        assertEquals("test3", commandHistory.undo());
+        commandHistory.undo();
+        assertEquals("test1", commandHistory.undo());
+    }
+
+    @Test
+    void redo_undoAndRedo_returnsNextCommand() {
+        commandHistory.addCommandToHistory("test1");
+        commandHistory.addCommandToHistory("test2");
+        commandHistory.addCommandToHistory("test3");
+        assertEquals("test3", commandHistory.undo());
+        assertEquals("test3", commandHistory.redo());
+    }
+
+    @Test
+    void redo_redoAtEndOfHistory_returnsEmptyString() {
+        commandHistory.addCommandToHistory("test1");
+        commandHistory.addCommandToHistory("test2");
+        commandHistory.addCommandToHistory("test3");
+        assertEquals("", commandHistory.redo());
+    }
+}
