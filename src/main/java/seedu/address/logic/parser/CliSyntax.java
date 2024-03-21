d23637a6da9123e1d88cf2669e03d3aff6928b6a
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-03-21 22:55:07 +0800

    changed to single command word for deleteMeeting and change parser

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -16,5 +16,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
 
     public static final Prefix PREFIX_CLIENT_INDEX = new Prefix("client/");
+    public static final Prefix PREFIX_MEETING_INDEX = new Prefix("meeting/");
 
 }
