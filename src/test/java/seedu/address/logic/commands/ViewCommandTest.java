4dab12730cb5801fbfeda41249b4efd116e28d6e
Rah <sithanathan.rahul@gmail.com>
2024-03-21 14:45:56 +0800

    Checks for codecov ViewCommand test

--- src/test/java/seedu/address/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -2,7 +2,10 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
@@ -10,19 +13,60 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.ViewPredicate;
 import seedu.address.testutil.PersonBuilder;
 
 public class ViewCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model model;
 
-    @Test
-    public void execute_allView_success() {
-        Person viewPerson = new PersonBuilder().build();
-        model.addPerson(viewPerson);
-        ViewCommand viewCommand = new ViewCommand(Index.fromZeroBased(7));
-
-        String expectedMessage = String.format(viewCommand.MESSAGE_VIEW_SUCCESS, viewPerson.getName());
-        assertCommandSuccess(viewCommand, model, expectedMessage, model);
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     }
 
+    @Test
+    public void execute_viewValidPerson_success() {
+        // Prepare our model and expected model
+        Person personToView = new PersonBuilder().build();
+        model.addPerson(personToView);
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        // Assuming the person to view is the last one added
+        Index lastPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size());
+
+        // Set up expected model's filtered list according to ViewCommand's behavior
+        expectedModel.updateFilteredPersonList(new ViewPredicate(lastPersonIndex, personToView));
+
+        // Construct command to view the last added person
+        ViewCommand viewCommand = new ViewCommand(lastPersonIndex);
+        String expectedMessage = String.format(ViewCommand.MESSAGE_VIEW_SUCCESS, personToView.getName());
+
+        // Execute command and verify success
+        assertCommandSuccess(viewCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        Index indexFirst = Index.fromOneBased(1);
+        Index indexSecond = Index.fromOneBased(2);
+
+        ViewCommand viewFirstCommand = new ViewCommand(indexFirst);
+        ViewCommand viewSecondCommand = new ViewCommand(indexSecond);
+
+        // same object -> returns true
+        assertTrue(viewFirstCommand.equals(viewFirstCommand));
+
+        // same values -> returns true
+        ViewCommand viewFirstCommandCopy = new ViewCommand(indexFirst);
+        assertTrue(viewFirstCommand.equals(viewFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(viewFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(viewFirstCommand.equals(null));
+
+        // different index -> returns false
+        assertFalse(viewFirstCommand.equals(viewSecondCommand));
+    }
 }
