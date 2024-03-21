2d6ccd7754f9244d21f34cf5484321968e756b1f
ruijietay <ruijietay.github@arctoszero.com>
2024-03-21 09:53:50 +0800

    Add test cases for Note

---
+++ src/test/java/seedu/address/model/person/NoteTest.java
@@ -0,0 +1,35 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Note(null));
+    }
+
+    @Test
+    public void equals() {
+        Note note = new Note("Valid Note");
+
+        // same values -> returns true
+        assertTrue(note.equals(new Note("Valid Note")));
+
+        // same object -> returns true
+        assertTrue(note.equals(note));
+
+        // null -> returns false
+        assertFalse(note.equals(null));
+
+        // different types -> returns false
+        assertFalse(note.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(note.equals(new Note("Other Valid Note")));
+    }
+}
