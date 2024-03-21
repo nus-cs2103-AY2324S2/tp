7fc7099fa8502d6ac554dcbf111fbe10daebde88
S-Aishvarya <aish.varya1801@gmail.com>
2024-03-21 23:50:14 +0800

    no message

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,12 +3,14 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import seedu.address.model.Model;
-import seedu.address.model.patient.Patient;
+
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
+import seedu.address.model.Model;
+import seedu.address.model.patient.Patient;
 
 /**
  * Lists all persons in the address book to the user.
@@ -30,10 +32,10 @@ public class ListCommand extends Command {
             return patient1.getName().fullName.compareTo(patient2.getName().fullName);
         };
         sortedList.sort(comparator);
-        for(Patient patient : sortedList) {
+        for ( Patient patient : sortedList ) {
             model.deletePerson(patient);
         }
-        for(Patient patient : sortedList) {
+        for ( Patient patient : sortedList ) {
             model.addPerson(patient);
         }
         return new CommandResult(MESSAGE_SUCCESS);
