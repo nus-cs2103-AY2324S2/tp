10573665eeceddb7c8a5faf8816c592783d838fe
raysonchia <122922486+raysonchia@users.noreply.github.com>
2024-03-21 16:58:39 +0800

    Update whitespace

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -26,8 +26,7 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = 
-            COMMAND_WORD 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Finds all patients whose medical records contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: "
