d8c54ecd4102d1ed708885bb4f4f1dc8f9ecd8d8
GERARDJM018 <e0985922@u.nus.edu>
2024-03-21 13:40:43 +0800

    Update Test Cases

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -41,6 +41,7 @@ public class CommandTestUtil {
 
     public static final String TYPE_DESC_AMY = " " + VALID_TYPE_AMY;
 
+    public static final String TYPE_DESC_BOB = " " + VALID_TYPE_BOB;
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
