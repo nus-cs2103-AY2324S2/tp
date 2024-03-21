f92949ae64248ea5d5d29a4aea6cb27b33fb156c
jayne1010 <e0970489@u.nus.edu>
2024-03-21 17:01:27 +0800

    Update edit command test

--- src/test/java/seedu/address/testutil/TypicalIdentityPredicate.java
+++ src/test/java/seedu/address/testutil/TypicalIdentityPredicate.java
@@ -1,5 +1,14 @@
 package seedu.address.testutil;
 
-public class TypicalIdentityPredicate {
+import seedu.address.model.person.IdentityCardNumber;
+import seedu.address.model.person.IdentityCardNumberMatchesPredicate;
 
+/**
+ * A utility class containing a list of {@code IdentityCardNumberMatchesPredicate} objects to be used in tests.
+ */
+public class TypicalIdentityPredicate {
+    public static final IdentityCardNumberMatchesPredicate IC_AMY =
+            new IdentityCardNumberMatchesPredicate(new IdentityCardNumber("S1234567A"));
+    public static final IdentityCardNumberMatchesPredicate IC_BOB =
+            new IdentityCardNumberMatchesPredicate(new IdentityCardNumber("S1234567B"));
 }
