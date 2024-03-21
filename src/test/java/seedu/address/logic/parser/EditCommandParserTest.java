844faec79d44ebbb9fa49e97bbe9861d9716440e
Ashley Chua <ashleycxr2444@gmail.com>
2024-03-21 22:50:03 +0800

    Add more tests to ProgrammingLanguageTest.java

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -260,16 +260,4 @@ public class EditCommandParserTest {
 
         assertParseSuccess(parser, userInput, expectedCommand);
     }
-
-    /*
-    @Test
-    public void parse_resetProgrammingLanguages_success() {
-        Index targetIndex = INDEX_THIRD_PERSON;
-        String userInput = targetIndex.getOneBased() + PROGRAMMING_LANG_EMPTY;
-        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withProgrammingLanguages().build();
-        EditCommand expectedCommand = new EditCommand(targetIndex, descriptor);
-
-        assertParseSuccess(parser, userInput, expectedCommand);
-    }
-    */
 }
--- src/test/java/seedu/address/model/language/ProgrammingLanguageTest.java
+++ src/test/java/seedu/address/model/language/ProgrammingLanguageTest.java
@@ -1,5 +1,8 @@
 package seedu.address.model.language;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -23,4 +26,44 @@ public class ProgrammingLanguageTest {
         assertThrows(NullPointerException.class, () -> ProgrammingLanguage.isValidLanguageName(null));
     }
 
+    @Test
+    public void constructor_validLanguageName_equals() {
+        // Two instances with the same valid language name should be equal
+        String languageName = "Java";
+        ProgrammingLanguage language1 = new ProgrammingLanguage(languageName);
+        ProgrammingLanguage language2 = new ProgrammingLanguage(languageName);
+        assertEquals(language1, language2);
+    }
+
+    @Test
+    public void isValidLanguageName_validLanguageName_returnsTrue() {
+        // Valid language name
+        String validLanguageName = "Java";
+        assertTrue(ProgrammingLanguage.isValidLanguageName(validLanguageName));
+    }
+
+    @Test
+    public void equals_sameInstance_returnsTrue() {
+        ProgrammingLanguage language = new ProgrammingLanguage("Java");
+        assertTrue(language.equals(language));
+    }
+
+    @Test
+    public void equals_nullObject_returnsFalse() {
+        ProgrammingLanguage language = new ProgrammingLanguage("Java");
+        assertFalse(language.equals(null));
+    }
+
+    @Test
+    public void equals_differentClass_returnsFalse() {
+        ProgrammingLanguage language = new ProgrammingLanguage("Java");
+        assertFalse(language.equals("Java"));
+    }
+
+    @Test
+    public void hashCode_sameInstance_returnsEqualHashCodes() {
+        ProgrammingLanguage language1 = new ProgrammingLanguage("Java");
+        ProgrammingLanguage language2 = new ProgrammingLanguage("Java");
+        assertEquals(language1.hashCode(), language2.hashCode());
+    }
 }
