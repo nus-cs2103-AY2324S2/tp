e99bb38b6975dd4236992b034d490570203952a9
laney0808 <e0969207@u.nus.edu>
2024-03-22 22:54:01 +0800

    Change hasPerson() implementation in UpdateCommand.execute()

--- src/main/java/seedu/address/logic/commands/ReadCommand.java
+++ src/main/java/seedu/address/logic/commands/ReadCommand.java
@@ -7,15 +7,9 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.DateOfBirth;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Nric;
 import seedu.address.model.person.NricContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Sex;
-import seedu.address.model.person.Status;
 
 /**
  * Reads the details of an existing person in the address book.
