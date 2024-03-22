44f124ba33a5ca506a6d53c2634b20eaaa0a73c4
jamessinmaojun <jamessinmaojun@gmail.com>
2024-03-22 13:46:49 +0800

    Add findByName test cases

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -1,12 +1,9 @@
 package seedu.address.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.ALICE;
-import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,9 +12,18 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddCommandTest;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.messages.AddMessages;
+import seedu.address.logic.messages.DeleteMessages;
+import seedu.address.logic.LogicManagerTest;
+import seedu.address.logic.messages.EditMessages;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.AddressBookBuilder;
+import seedu.address.testutil.PersonBuilder;
 
 public class ModelManagerTest {
 
@@ -137,4 +143,97 @@ public class ModelManagerTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void findByName_success() {
+        AddressBook addressBook = new AddressBookBuilder().withPerson(ALICE).build();
+        UserPrefs userPrefs = new UserPrefs();
+        modelManager = new ModelManager(addressBook, userPrefs);
+
+        Name nameToFind = ALICE.getName();
+        Name nameFound = new Name("Not Alice Pauline");
+        try {
+            nameFound = modelManager.findByName(nameToFind).getName();
+        } catch (CommandException e) {
+            fail();
+        }
+        assertEquals(nameToFind, nameFound);
+    }
+
+    @Test
+    public void findByNameFailure_throwsCommandException() {
+        Name nameToFind = ALICE.getName();
+        assertThrows(CommandException.class,
+                EditMessages.MESSAGE_INVALID_EDIT_PERSON, () -> modelManager.findByName(nameToFind));
+    }
+
+    @Test
+    public void findMaintainerByName_success() {
+        AddressBook addressBook = new AddressBookBuilder().withPerson(ALICEMAINTAINER).build();
+        UserPrefs userPrefs = new UserPrefs();
+        modelManager = new ModelManager(addressBook, userPrefs);
+
+        Name nameToFind = ALICEMAINTAINER.getName();
+        Name nameFound = new Name("Not Alice Pauline");
+        try {
+            nameFound = modelManager.findMaintainerByName(nameToFind).getName();
+        } catch (CommandException e) {
+            fail();
+        }
+        assertEquals(nameToFind, nameFound);
+    }
+
+    @Test
+    public void findMaintainerByNameFailure_throwsCommandException() {
+        Name nameToFind = ALICEMAINTAINER.getName();
+        assertThrows(CommandException.class,
+                EditMessages.MESSAGE_INVALID_EDIT_MAINTAINER, () -> modelManager.findMaintainerByName(nameToFind));
+    }
+
+    @Test
+    public void findStaffByName_success() {
+        AddressBook addressBook = new AddressBookBuilder().withPerson(ALICESTAFF).build();
+        UserPrefs userPrefs = new UserPrefs();
+        modelManager = new ModelManager(addressBook, userPrefs);
+
+        Name nameToFind = ALICESTAFF.getName();
+        Name nameFound = new Name("Not Alice Pauline");
+        try {
+            nameFound = modelManager.findStaffByName(nameToFind).getName();
+        } catch (CommandException e) {
+            fail();
+        }
+        assertEquals(nameToFind, nameFound);
+    }
+
+    @Test
+    public void findStaffByNameFailure_throwsCommandException() {
+        Name nameToFind = ALICESTAFF.getName();
+        assertThrows(CommandException.class,
+                EditMessages.MESSAGE_INVALID_EDIT_STAFF, () -> modelManager.findStaffByName(nameToFind));
+    }
+
+    @Test
+    public void findSupplierByName_success() {
+        AddressBook addressBook = new AddressBookBuilder().withPerson(ALICESUPPLIER).build();
+        UserPrefs userPrefs = new UserPrefs();
+        modelManager = new ModelManager(addressBook, userPrefs);
+
+        Name nameToFind = ALICESUPPLIER.getName();
+        Name nameFound = new Name("Not Alice Pauline");
+        try {
+            nameFound = modelManager.findSupplierByName(nameToFind).getName();
+        } catch (CommandException e) {
+            fail();
+        }
+        assertEquals(nameToFind, nameFound);
+    }
+
+    @Test
+    public void findSupplierByNameFailure_throwsCommandException() {
+        Name nameToFind = ALICESUPPLIER.getName();
+        assertThrows(CommandException.class,
+                EditMessages.MESSAGE_INVALID_EDIT_SUPPLIER, () -> modelManager.findSupplierByName(nameToFind));
+    }
+
 }
