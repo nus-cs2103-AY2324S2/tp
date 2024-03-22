2ab5eef1394f2da652b5152781b63fb53f4a2b0d
ReflectiveObsidian <e0958151@u.nus.edu>
2024-03-22 11:48:14 +0800

    Remove redundant prefix checker

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -60,13 +60,4 @@ public class AddCommandParser implements Parser<AddCommand> {
     private static String[] separateAttributeNamesAndValues(String parts) {
         return parts.trim().split(" ", 2);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
