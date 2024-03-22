9f0d8f859e1e9fa6133e679b78dd8e1d53e08c90
dhlee03 <dhclearsky@gmail.com>
2024-03-23 01:33:06 +0800

    Add testcases for sponsor and staff

---
+++ src/test/java/seedu/address/model/person/SponsorTest.java
@@ -0,0 +1,24 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.testutil.PersonBuilder;
+
+public class SponsorTest {
+    @Test
+    public void isSamePerson() {
+        // same object -> returns true
+        Sponsor sponsor = (Sponsor) new PersonBuilder().withCategory("SPONSOR").build();
+        assert (sponsor.isSamePerson(sponsor));
+
+
+        assert (!sponsor.isSamePerson(null));
+
+
+        Staff staff = (Staff) new PersonBuilder().withCategory("STAFF").build();
+        assert (!sponsor.isSamePerson(staff));
+
+        Sponsor differentSponsor = (Sponsor) new PersonBuilder().withCategory("SPONSOR").withName("john").build();
+        assert (!sponsor.isSamePerson(differentSponsor));
+    }
+
+}
---
+++ src/test/java/seedu/address/model/person/StaffTest.java
@@ -0,0 +1,32 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.testutil.PersonBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class StaffTest {
+    @Test
+    public void isSamePerson() {
+        Staff staff = (Staff) new PersonBuilder().withCategory("STAFF").withName("John").build();
+        Staff staffCopy = (Staff) new PersonBuilder().withCategory("STAFF").withName("John").build();
+        Staff other = (Staff) new PersonBuilder().withCategory("STAFF").withName("Peter").build();
+        assertFalse(staff.isSamePerson(other));
+        assert (staff.isSamePerson(staffCopy));
+
+        Sponsor sponsor = (Sponsor) new PersonBuilder().withCategory("SPONSOR").withName("Jake").build();
+        assertFalse(staff.isSamePerson(sponsor));
+
+    }
+
+    @Test
+    public void getGroupNumber() {
+        Staff test = (Staff) new PersonBuilder().withCategory("STAFF").withName("John").build();
+        test.setGroupNumber(1);
+        assertNotEquals(5, test.getGroupNumber());
+        assertEquals(1, test.getGroupNumber());
+    }
+}
+
