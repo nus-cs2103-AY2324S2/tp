b2db29aa121cecbb221945671145fee6e9d07b76
ReflectiveObsidian <e0958151@u.nus.edu>
2024-03-22 12:37:45 +0800

    Fix test for argument multimap

--- src/test/java/seedu/address/logic/parser/ArgumentMultimapTest.java
+++ src/test/java/seedu/address/logic/parser/ArgumentMultimapTest.java
@@ -9,11 +9,13 @@ import seedu.address.logic.parser.Prefix;
 
 class ArgumentMultimapTest {
     @Test
-    public void verifyArgumentMultimap() {
+    public void verifyNoDuplicatePrefixes() {
         ArgumentMultimap argumentMultimap = new ArgumentMultimap();
-        Prefix prefix = new Prefix("name");
-        argumentMultimap.put(prefix, "Alice");
-        assertEquals("Alice", argumentMultimap.getValue(prefix).get());
+        Prefix prefix = new Prefix("p");
+        argumentMultimap.put(prefix, "value1");
+        argumentMultimap.put(prefix, "value2");
+        assertEquals("Multiple values specified for the following single-valued field(s): "
+                + "p", Messages.getErrorMessageForDuplicatePrefixes(prefix));
     }
 
 
