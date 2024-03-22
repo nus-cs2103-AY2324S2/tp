8b3095b3158f21268d011eae447c3a87e35768c1
nichee <E0957568@u.nus.edu>
2024-03-23 02:44:49 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/EditAttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditAttendanceCommandTest.java
@@ -1,12 +1,8 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ClassBook;
 import seedu.address.model.Model;
@@ -38,12 +34,11 @@ class EditAttendanceCommandTest {
         editAttendanceCommand = new EditAttendanceCommand(Index.fromZeroBased(0), descriptor);
     }
 
-//    @Test
-//    public void execute_validIndexAndAttendance_success() throws CommandException {
-//        CommandResult commandResult = editAttendanceCommand.execute(model);
-//        assertEquals(String.format(EditAttendanceCommand.MESSAGE_EDIT_PERSON_SUCCESS, personToEdit),
-//                commandResult.getFeedbackToUser());
-//        // Further assertions can be made here to ensure that the attendance record was edited correctly
-//    }
-
+    //    @Test
+    //    public void execute_validIndexAndAttendance_success() throws CommandException {
+    //        CommandResult commandResult = editAttendanceCommand.execute(model);
+    //        assertEquals(String.format(EditAttendanceCommand.MESSAGE_EDIT_PERSON_SUCCESS, personToEdit),
+    //                commandResult.getFeedbackToUser());
+    //        // Further assertions can be made here to ensure that the attendance record was edited correctly
+    //    }
 }
