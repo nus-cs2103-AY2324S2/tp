0126b2068040783e008c205767ee3e271d101b07
modembcc <modemhappy@gmail.com>
2024-03-22 15:05:44 +0800

    Show tasks based on their status

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -1,17 +1,18 @@
 package seedu.address.ui;
 
+import java.io.IOException;
+
 // import java.util.logging.Logger;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 // import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
+import seedu.address.model.project.Task;
+import seedu.address.ui.TaskListPanel.TaskListCell;
 
 /**
  * Panel containing the list of persons.
@@ -24,6 +25,10 @@ public class TaskListPanel extends UiPart<Region> {
 
     @FXML
     private Label showingProjectName;
+    @FXML
+    private ListView<Task> undoneTaskListView;
+    @FXML
+    private ListView<Task> doneTaskListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
@@ -32,6 +37,29 @@ public class TaskListPanel extends UiPart<Region> {
         super(FXML);
         this.currentProject = currentProject;
         showingProjectName.setText(currentProject.getName().fullName);
+        // Manually set the items of the ListView
+
+        undoneTaskListView.getItems().clear();
+        undoneTaskListView.getItems().addAll(currentProject.getUndoneTasks());
+
+        undoneTaskListView.setCellFactory(param -> new TaskListCell());
+
+        doneTaskListView.getItems().clear();
+        doneTaskListView.getItems().addAll(currentProject.getDoneTasks());
+
+        doneTaskListView.setCellFactory(param -> new TaskListCell());
     }
+
+    class TaskListCell extends ListCell<Task> {
+    @Override
+    protected void updateItem(Task task, boolean empty) {
+        super.updateItem(task, empty);
+        if (empty || task == null) {
+            setGraphic(null);
+        } else {
+            setGraphic(new TaskCard(task).getRoot());
+        }
+    }
+}
 
 }
