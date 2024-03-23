e8af5ec0085a037f2d6872781c5cd691106a73b9
Shauryan Agrawal <Shauryan100@gmail.com>
2024-03-23 23:10:39 +0800

    Add meetings property to person in addressbook

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -10,14 +10,131 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Policy;
-import seedu.address.model.person.Relationship;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
+
+/**
+ * Jackson-friendly version of {@link Person}.
+ */
+//class JsonAdaptedPerson {
+//
+//    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
+//
+//    private final String name;
+//    private final String phone;
+//    private final String email;
+//    private final String address;
+//    private final String policy;
+//    private final String relationship;
+//    private final List<JsonAdaptedTag> tags = new ArrayList<>();
+//
+//    private final List<JsonAdaptedMeeting> meetings = new ArrayList<>();
+//
+//    /**
+//     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+//     */
+//    @JsonCreator
+//    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
+//            @JsonProperty("email") String email, @JsonProperty("address") String address,
+//            @JsonProperty("relationship") String relationship, @JsonProperty("policy") String policy,
+//            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+//
+//        this.name = name;
+//        this.phone = phone;
+//        this.email = email;
+//        this.address = address;
+//        this.policy = policy;
+//        this.relationship = relationship;
+//
+//        if (tags != null) {
+//            this.tags.addAll(tags);
+//        }
+//
+//
+//    }
+//
+//    /**
+//     * Converts a given {@code Person} into this class for Jackson use.
+//     */
+//    public JsonAdaptedPerson(Person source) {
+//        name = source.getName().fullName;
+//        phone = source.getPhone().value;
+//        email = source.getEmail().value;
+//        address = source.getAddress().value;
+//        policy = source.getPolicy().value;
+//        relationship = source.getRelationship().value;
+//        tags.addAll(source.getTags().stream()
+//                .map(JsonAdaptedTag::new)
+//                .collect(Collectors.toList()));
+//    }
+//
+//    /**
+//     * Converts this Jackson-friendly adapted person object into the model's {@code Person} object.
+//     *
+//     * @throws IllegalValueException if there were any data constraints violated in the adapted person.
+//     */
+//    public Person toModelType() throws IllegalValueException {
+//        final List<Tag> personTags = new ArrayList<>();
+//        for (JsonAdaptedTag tag : tags) {
+//            personTags.add(tag.toModelType());
+//        }
+//
+//        if (name == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+//        }
+//        if (!Name.isValidName(name)) {
+//            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+//        }
+//        final Name modelName = new Name(name);
+//
+//        if (phone == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
+//        }
+//        if (!Phone.isValidPhone(phone)) {
+//            throw new IllegalValueException(Phone.MESSAGE_CONSTRAINTS);
+//        }
+//        final Phone modelPhone = new Phone(phone);
+//
+//        if (email == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+//        }
+//        if (!Email.isValidEmail(email)) {
+//            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+//        }
+//        final Email modelEmail = new Email(email);
+//
+//        if (address == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
+//        }
+//        if (!Address.isValidAddress(address)) {
+//            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
+//        }
+//        final Address modelAddress = new Address(address);
+//
+//        if (relationship == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+//                    Relationship.class.getSimpleName()));
+//        }
+//        if (!Relationship.isValidRelationship(relationship)) {
+//            throw new IllegalValueException(Relationship.MESSAGE_CONSTRAINTS);
+//        }
+//        final Relationship modelRelationship = new Relationship(relationship);
+//
+//        if (policy == null) {
+//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Policy.class.getSimpleName()));
+//        }
+//        final Policy modelPolicy = new Policy(policy);
+//
+//        final Set<Tag> modelTags = new HashSet<>(personTags);
+//
+//        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelRelationship, modelPolicy, modelTags);
+//
+//    }
+//
+//}
+
+
+
 
 /**
  * Jackson-friendly version of {@link Person}.
@@ -32,26 +149,32 @@ class JsonAdaptedPerson {
     private final String address;
     private final String policy;
     private final String relationship;
-    private final List<JsonAdaptedTag> tags = new ArrayList<>();
+    private final List<JsonAdaptedTag> adaptedTags = new ArrayList<>();
+    private final List<JsonAdaptedMeeting> adaptedMeetings = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-            @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("relationship") String relationship, @JsonProperty("policy") String policy,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
-
+    public JsonAdaptedPerson(@JsonProperty("name") String name,
+                             @JsonProperty("phone") String phone,
+                             @JsonProperty("email") String email,
+                             @JsonProperty("address") String address,
+                             @JsonProperty("relationship") String relationship,
+                             @JsonProperty("policy") String policy,
+                             @JsonProperty("tags") List<JsonAdaptedTag> adaptedTags,
+                             @JsonProperty("meetings") List<JsonAdaptedMeeting> adaptedMeetings) {
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.policy = policy;
         this.relationship = relationship;
-
-        if (tags != null) {
-            this.tags.addAll(tags);
+        if (adaptedTags != null) {
+            this.adaptedTags.addAll(adaptedTags);
+        }
+        if (adaptedMeetings != null) {
+            this.adaptedMeetings.addAll(adaptedMeetings);
         }
     }
 
@@ -65,8 +188,11 @@ class JsonAdaptedPerson {
         address = source.getAddress().value;
         policy = source.getPolicy().value;
         relationship = source.getRelationship().value;
-        tags.addAll(source.getTags().stream()
+        adaptedTags.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
+                .collect(Collectors.toList()));
+        adaptedMeetings.addAll(source.getMeetings().stream()
+                .map(JsonAdaptedMeeting::new)
                 .collect(Collectors.toList()));
     }
 
@@ -77,8 +203,13 @@ class JsonAdaptedPerson {
      */
     public Person toModelType() throws IllegalValueException {
         final List<Tag> personTags = new ArrayList<>();
-        for (JsonAdaptedTag tag : tags) {
+        for (JsonAdaptedTag tag : adaptedTags) {
             personTags.add(tag.toModelType());
+        }
+
+        final List<Meeting> personMeetings = new ArrayList<>();
+        for (JsonAdaptedMeeting meeting : adaptedMeetings) {
+            personMeetings.add(meeting.toModelType());
         }
 
         if (name == null) {
@@ -128,9 +259,19 @@ class JsonAdaptedPerson {
         final Policy modelPolicy = new Policy(policy);
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
+        final Person person = new Person(modelName, modelPhone, modelEmail, modelAddress, modelRelationship, modelPolicy, modelTags);
 
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelRelationship, modelPolicy, modelTags);
+        // Add meetings to the person
+        for (Meeting meeting : personMeetings) {
+            person.addMeeting(meeting);
+        }
 
+        return person;
     }
 
 }
+
+
+
+
+
