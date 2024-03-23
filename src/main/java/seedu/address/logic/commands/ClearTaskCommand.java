ed6584a0721d2466ca8b47df13faf5ae8d120e2f
Chong Chin Herng <chongahzong@gmail.com>
2024-03-24 00:07:10 +0800

    Change ClearTaskCommand to throw an exception

--- src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +10,17 @@ public class ClearTaskCommand extends Command {
 
     public static final String COMMAND_WORD = "cleartask";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Unassigns all tasks assigned to the person identified "
+            + "by the index number used in the last person listing.\n"
+            + "Parameters: PERSON_INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "ClearTask command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from cleartask");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
