5b218b6a14b90b735f4bf036d4d0a06c2a9ff50d
Chong Chin Herng <chongahzong@gmail.com>
2024-03-24 00:21:43 +0800

    Update ClearTaskCommand

--- src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearTaskCommand.java
@@ -1,10 +1,16 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.HashSet;
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Unassigns all tasks assigned to an existing person in the address book.
@@ -19,7 +25,7 @@ public class ClearTaskCommand extends Command {
             + "Parameters: PERSON_INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d";
+    public static final String MESSAGE_SUCCESS = "Tasks for %1$s has been cleared.";
 
     private final Index index;
 
@@ -32,10 +38,27 @@ public class ClearTaskCommand extends Command {
         this.index = index;
     }
 
+    private Person getPersonToBeUnassigned(Model model) throws CommandException {
+        List<Person> lastShownPersonList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownPersonList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        return lastShownPersonList.get(index.getZeroBased());
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(
-                String.format(MESSAGE_ARGUMENTS, index.getOneBased()));
+        Person personToBeUnassigned = getPersonToBeUnassigned(model);
+
+        Person unassignedPerson = new Person(personToBeUnassigned.getName(), personToBeUnassigned.getPhone(),
+                personToBeUnassigned.getEmail(), personToBeUnassigned.getAddress(), new HashSet<>());
+
+        model.setPerson(personToBeUnassigned, unassignedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, unassignedPerson.getName()));
     }
 
     @Override
