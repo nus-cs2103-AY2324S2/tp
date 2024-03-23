a432afac5dd9c0a9e2e16fbdfb645a59b7b0e488
LAPTOP-T43DNL0C\User1 <alexgenrong@gmail.com>
2024-03-23 13:24:56 +0800

    Fix classes failing gradle tests

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -92,11 +92,12 @@ public class PersonBuilder {
         nric = personToCopy.getNric();
         name = personToCopy.getName();
         phone = personToCopy.getPhone();
+        address = personToCopy.getAddress();
         dateOfBirth = personToCopy.getDateOfBirth();
         sex = personToCopy.getSex();
+        status = personToCopy.getStatus();
 
         /*
-        address = personToCopy.getAddress();
         allergies = personToCopy.getAllergies();
         bloodType = personToCopy.getBloodType();
         country = personToCopy.getCountry();
@@ -235,6 +236,7 @@ public class PersonBuilder {
      */
     public Person build() {
         Person p = new Person(nric, name, phone, address, dateOfBirth, sex, status);
+        /*
         p.setEmail(email);
         p.setCountry(country);
         p.setAllergies(allergies);
@@ -243,6 +245,7 @@ public class PersonBuilder {
         p.setDateOfAdmission(dateOfAdmission);
         p.setDiagnosis(diagnosis);
         p.setSymptom(symptom);
+         */
         return p;
     }
 }
