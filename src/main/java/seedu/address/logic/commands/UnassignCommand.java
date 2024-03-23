8716e63c31c36709a13e1939aa16b08bf6a56dc9
Chong Chin Herng <chongahzong@gmail.com>
2024-03-23 15:30:58 +0800

    Change UnassignCommand to throw an exception

--- src/main/java/seedu/address/logic/commands/UnassignCommand.java
+++ src/main/java/seedu/address/logic/commands/UnassignCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +10,22 @@ public class UnassignCommand extends Command {
 
     public static final String COMMAND_WORD = "unassign";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Unassigns the task identified "
+            + "by the index number used in the last task listing "
+            + "to the person identified "
+            + "by the index number used in the last person listing. "
+            + "Does nothing if the task not assigned to the person.\n"
+            + "Parameters: TASK_INDEX (must be a positive integer) "
+            + "to/ [PERSON_INDEX (must be a positive integer)]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "to/ 2";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Unassign command not implemented yet";
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from unassign");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
