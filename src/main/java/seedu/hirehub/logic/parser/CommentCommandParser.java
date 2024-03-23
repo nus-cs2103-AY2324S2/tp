9667d65039cbfffb1e0105f50426d6011d1e808f
logical-1985516 <ngyanjie0418@gmail.com>
2024-03-24 02:12:56 +0800

    Fix error message and standardize code

--- src/main/java/seedu/hirehub/logic/parser/CommentCommandParser.java
+++ src/main/java/seedu/hirehub/logic/parser/CommentCommandParser.java
@@ -2,11 +2,9 @@ package seedu.hirehub.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.hirehub.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.hirehub.logic.parser.CliSyntax.PREFIX_COMMENT;
 
 import seedu.hirehub.commons.core.index.Index;
 import seedu.hirehub.logic.commands.CommentCommand;
-import seedu.hirehub.logic.commands.StatusCommand;
 import seedu.hirehub.logic.parser.exceptions.ParseException;
 import seedu.hirehub.model.person.Comment;
 
@@ -20,7 +18,7 @@ public class CommentCommandParser implements Parser<CommentCommand> {
         String[] parsedIndexComment = args.trim().split(" ", 2);
 
         if (parsedIndexComment.length < 2 || parsedIndexComment[1].trim().equals("")) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatusCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CommentCommand.MESSAGE_USAGE));
         }
 
         Index index;
