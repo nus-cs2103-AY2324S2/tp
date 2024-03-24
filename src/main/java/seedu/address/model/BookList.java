df188e5d6860835ff5613f9bdd03f50d8af85047
jinhanfromNUS <e1083004@u.nus.edu>
2024-03-25 01:42:30 +0800

    Add methods to BookList

--- src/main/java/seedu/address/model/BookList.java
+++ src/main/java/seedu/address/model/BookList.java
@@ -4,10 +4,55 @@ import java.util.ArrayList;
 
 import seedu.address.model.book.Book;
 
+/**
+ * The BookList class represents a collection of books.
+ * It manages operations such as adding, retrieving, and deleting books from the list.
+ */
 public class BookList {
     private ArrayList<Book> bookList;
 
+    /**
+     * Construct an empty BookList.
+     */
     public BookList() {
         this.bookList = new ArrayList<>();
     }
+
+    /**
+     * Construct a BookList with the specified list of books.
+     *
+     * @param bookList The list of books to initialize the BookList with.
+     */
+    public BookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    /**
+     * Add a book to the BookList.
+     *
+     * @param book The book to be added.
+     */
+    public void addBook(Book book) {
+        this.bookList.add(book);
+    }
+
+    /**
+     * Retrieve the book at the specified index from the BookList.
+     *
+     * @param index The index of the book to retrieve.
+     * @return The book at the specified index.
+     */
+    public Book getBook(int index) {
+        return bookList.get(index);
+    }
+
+    /**
+     * Delete the book at the specified index from the BookList.
+     *
+     * @param index The index of the book to delete.
+     */
+    public void deleteBook(int index) {
+        this.bookList.remove(index);
+    }
+
 }
