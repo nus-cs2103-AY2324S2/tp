9b96f42c289cd8e637cfd41888dae34916b23b52
wallacexuhanxiao <wallacexuhanxiao@gmail.com>
2024-03-24 23:50:45 +0800

    Fix style error in previous commit

--- src/test/java/seedu/address/logic/commands/ChangeDataSourceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ChangeDataSourceCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_CHANGED_DATA_SOURCE;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.nio.file.Path;
