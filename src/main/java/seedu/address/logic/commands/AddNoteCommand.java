06822786d3d15ae0e723176649875697db0745e5
jayne1010 <e0970489@u.nus.edu>
2024-03-24 20:34:47 +0800

    Resolve merge conflict

--- src/main/java/seedu/address/logic/commands/AddNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/AddNoteCommand.java
@@ -22,18 +22,15 @@ public class AddNoteCommand extends Command {
     public static final String COMMAND_WORD = "addnote";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the note of the person whose profile matches "
-            + "the specified IC (must be a valid identity card number). \n"
-            + "Existing remark will be appended by default. \n"
-            + "Parameters: "
-            + PREFIX_IC + "IC "
+            + "the specified IC (case-insensitive). "
+            + "Existing remark will be appended by default. "
+            + "To replace the original note, add -replace at the end of your command. "
+            + "E.g. addnote i/S0123456Q n/Diabetes -replace\n"
+            + "Parameters: IC (National Registration Identity Card) "
             + PREFIX_NOTE + "NOTE \n"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_IC + "S0123456Q "
-            + PREFIX_NOTE + "Diabetes \n"
-            + "To replace the original note, add -replace at the end of your command. \n"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_IC + "S0123456Q "
-            + PREFIX_NOTE + "Diabetes -replace";
+            + "Example: " + COMMAND_WORD
+            + " S0123456Q "
+            + PREFIX_NOTE + "Diabetes";
 
     public static final String MESSAGE_MODIFY_NOTE_SUCCESS = "Note for %1$s (ic: %2$s) modified successfully!";
     private final IdentityCardNumberMatchesPredicate icPredicate;
