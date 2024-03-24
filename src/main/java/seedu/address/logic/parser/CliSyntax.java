7316edd4c350bf6b2401322700877aefacbf6770
bgopi23 <baskargopinath01@gmail.com>
2024-03-24 16:08:05 +0800

    Improve formatting

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,8 +12,13 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_NOTE = new Prefix("note/");
-    public static final Prefix PREFIX_CLEAR_CONFIRM = new Prefix("/confirm");
+    public static final Prefix PREFIX_CLEAR_CONFIRM = new Prefix("/confirm");       
     public static final Prefix[] ALL_PREFIXES = {
-        PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_NOTE
+        PREFIX_NAME,
+        PREFIX_PHONE,
+        PREFIX_EMAIL,
+        PREFIX_ADDRESS,
+        PREFIX_TAG,
+        PREFIX_NOTE
     };
 }
