59eeb4ba4b2b69e9f6ff0f06f070bc1ef982d429
kaiyi27 <e1070556@u.nus.edu>
2024-03-24 13:08:32 +0800

    Add policy into FindCommand

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_RELATIONSHIP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
@@ -11,6 +12,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.CombinedPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.PolicyContainsKeywordsPredicate;
 import seedu.address.model.person.RelationshipContainsKeywordsPredicate;
 import seedu.address.model.person.TagContainsKeywordsPredicate;
 
@@ -28,7 +30,7 @@ public class FindCommandParser implements Parser<FindCommand> {
 
     @Override
     public FindCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_RELATIONSHIP, PREFIX_TAG);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_RELATIONSHIP, PREFIX_TAG, PREFIX_POLICY);
 
 
         if (args.trim().isEmpty()) {
@@ -39,12 +41,14 @@ public class FindCommandParser implements Parser<FindCommand> {
         List<String> nameKeywords = argMultimap.getAllValues(PREFIX_NAME);
         List<String> relationshipKeywords = argMultimap.getAllValues(PREFIX_RELATIONSHIP);
         List<String> tagKeywords = argMultimap.getAllValues(PREFIX_TAG);
+        List<String> policyKeywords = argMultimap.getAllValues(PREFIX_POLICY);
 
         CombinedPredicate combinedPredicate =
                 new CombinedPredicate(
                         new NameContainsKeywordsPredicate(nameKeywords),
                         new RelationshipContainsKeywordsPredicate(relationshipKeywords),
-                        new TagContainsKeywordsPredicate(tagKeywords));
+                        new TagContainsKeywordsPredicate(tagKeywords),
+                        new PolicyContainsKeywordsPredicate(policyKeywords));
 
         return new FindCommand(combinedPredicate);
     }
