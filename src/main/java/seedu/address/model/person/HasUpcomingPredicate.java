81ea8188e027316be363e035f45c45bbf1e5bf4c
yunchao <tanyunchao@gmail.com>
2024-03-25 06:29:06 +0800

    Add HasUpcomingPredicate class to check if a Person has upcoming appointments

---
+++ src/main/java/seedu/address/model/person/HasUpcomingPredicate.java
@@ -0,0 +1,20 @@
+package seedu.address.model.person;
+
+import java.util.function.Predicate;
+
+/**
+ * A predicate that checks if a {@code Person} has {@code Upcoming} appointments.
+ */
+public class HasUpcomingPredicate implements Predicate<Person>{
+
+    /**
+     * Tests if the given person has upcoming events.
+     *
+     * @param person The person to be tested.
+     * @return true if the person has upcoming events, false otherwise.
+     */
+    @Override
+    public boolean test(Person person) {
+        return person.getUpcoming().hasUpcoming();
+    }    
+}
