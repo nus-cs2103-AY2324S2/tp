5293a54eeb37d610e6b0ae758d0941a45e551273
kaiyi27 <e1070556@u.nus.edu>
2024-03-24 13:31:58 +0800

    Update policy to FindCommand and other class

--- src/main/java/seedu/address/model/person/PolicyContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/PolicyContainsKeywordsPredicate.java
@@ -7,7 +7,7 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Relationship} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code Policy} matches any of the keywords given.
  */
 public class PolicyContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
