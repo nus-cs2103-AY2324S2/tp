6b17268144cd2cca4f2bb0eb3a1a5308860e6b92
bgopi23 <baskargopinath01@gmail.com>
2024-03-24 16:10:38 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,7 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_NOTE = new Prefix("note/");
-    public static final Prefix PREFIX_CLEAR_CONFIRM = new Prefix("/confirm");       
+    public static final Prefix PREFIX_CLEAR_CONFIRM = new Prefix("/confirm");
     public static final Prefix[] ALL_PREFIXES = {
         PREFIX_NAME,
         PREFIX_PHONE,
