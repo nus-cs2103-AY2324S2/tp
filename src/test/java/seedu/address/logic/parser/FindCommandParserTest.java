f7ecd8482e2ffccd5c0b486e4d3794445c695e43
Benson <bensonang36@gmail.com>
2024-03-25 16:58:53 +0800

    Add TestCases to increase coverage

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.parser;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import javafx.collections.transformation.FilteredList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -9,9 +13,12 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Availability;
 import seedu.address.model.person.AvailableAtDatePredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.TypicalPersons;
 
 public class FindCommandParserTest {
 
@@ -60,5 +67,69 @@ public class FindCommandParserTest {
             e.printStackTrace();
         }
     }
+    @Test
+    public void parse_validNameAndAvailabilityArgs() {
+        Person Alice = TypicalPersons.ALICE; //Alice has availabilities 14/02/2024
+        Person Benson = TypicalPersons.BENSON;  //Benson has availabilities 20/03/2024, 03/05/2024
+        try {
+            // single space between keywords
+            FindCommand findCommand = parser.parse(" n/Alice a/20/03/2024");
+            Predicate<Person> pred = findCommand.getPredicate();
+            assertEquals(pred.test(Alice), true);
+            assertEquals(pred.test(Benson), true);
 
+            // multiple whitespaces between keywords
+            FindCommand findCommand2 = parser.parse(" n/  \n Alice \n \t a/20/03/2024  \t");
+            Predicate<Person> pred2 = findCommand2.getPredicate();
+            assertEquals(pred2.test(Alice), true);
+            assertEquals(pred2.test(Benson), true);
+        } catch (ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+
+    }
+
+    @Test
+    public void parse_validMultipleNameArgs() {
+        Person Alice = TypicalPersons.ALICE; //Alice has availabilities 14/02/2024
+        Person Benson = TypicalPersons.BENSON;  //Benson has availabilities 20/03/2024, 03/05/2024
+        try {
+            // single space between keywords
+            FindCommand findCommand = parser.parse(" n/Alice n/Benson");
+            Predicate<Person> pred = findCommand.getPredicate();
+            assertEquals(pred.test(Alice), true);
+            assertEquals(pred.test(Benson), true);
+
+            // multiple whitespaces between keywords
+            FindCommand findCommand2 = parser.parse(" n/  \n Alice \n \t n/Benson  \t");
+            Predicate<Person> pred2 = findCommand2.getPredicate();
+            assertEquals(pred2.test(Alice), true);
+            assertEquals(pred2.test(Benson), true);
+        } catch (ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+
+    }
+
+    @Test
+    public void parse_validMultipleAvailabilitiesArgs() {
+        Person Alice = TypicalPersons.ALICE; //Alice has availabilities 14/02/2024
+        Person Benson = TypicalPersons.BENSON;  //Benson has availabilities 20/03/2024, 03/05/2024
+        try {
+            // single space between keywords
+            FindCommand findCommand = parser.parse(" a/14/02/2024 a/20/03/2024");
+            Predicate<Person> pred = findCommand.getPredicate();
+            assertEquals(pred.test(Alice), true);
+            assertEquals(pred.test(Benson), true);
+
+            // multiple whitespaces between keywords
+            FindCommand findCommand2 = parser.parse(" a/  \n 14/02/2024 \n \t a/20/03/2024  \t");
+            Predicate<Person> pred2 = findCommand2.getPredicate();
+            assertEquals(pred2.test(Alice), true);
+            assertEquals(pred2.test(Benson), true);
+        } catch (ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+
+    }
 }
