e7efcab4c6209559e3c59e366b00b12654271a9e
Miffi <miffi@decadent.rest>
2024-03-25 16:23:34 +0800

    Satisfying the checkstyle CI

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -2,11 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
-
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FilterCommand object
@@ -16,6 +13,7 @@ public class FilterCommandParser implements Parser<FilterCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FilterCommand
      * and returns a FilterCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FilterCommand parse(String args) throws ParseException {
--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -2,14 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FilterCommand;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 public class FilterCommandParserTest {
 
