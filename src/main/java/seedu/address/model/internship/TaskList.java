cda7961c21f301c6d4c53d00260109981674c6e2
LWS49 <e0970244@u.nus.edu>
2024-03-26 03:57:40 +0800

    Add TaskList

---
+++ src/main/java/seedu/address/model/internship/TaskList.java
@@ -0,0 +1,87 @@
+package seedu.address.model.internship;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.InternshipAddTaskCommand;
+
+/**
+ * Represents a list of tasks in the internship book.
+ */
+public class TaskList {
+    private final ArrayList<Task> taskList;
+
+    /**
+     * Constructs a TaskList object.
+     */
+    public TaskList() {
+        this.taskList = new ArrayList<>();
+    }
+
+    /**
+     * Constructs a TaskList object from an existing list of tasks.
+     */
+    public TaskList(ArrayList<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    /**
+     * Constructs a TaskList object from a string representation of a task list.
+     */
+    public TaskList(String taskListString) {
+        if (taskListString.equals("[]") || taskListString.isEmpty()) {
+            this.taskList = new ArrayList<>();
+        } else {
+            ArrayList<Task> taskList = new ArrayList<>();
+            Pattern pattern = Pattern.compile("\\{\"task\": \"(.*?)\", \"deadline\": \"(.*?)\"\\}");
+            Matcher matcher = pattern.matcher(taskListString);
+
+            while (matcher.find()) {
+                String task = matcher.group(1);
+                String deadline = matcher.group(2);
+                taskList.add(new Task(task, deadline));
+            }
+
+            this.taskList = taskList;
+        }
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public Task getTask(int index) {
+        // Index is 1-based
+        return taskList.get(index - 1);
+    }
+
+    public int getTaskListSize() {
+        return taskList.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        int count = 1;
+        for (Task task : taskList) {
+            builder.append(count++).append(". (").append(task.toString()).append(")\n");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof InternshipAddTaskCommand)) {
+            return false;
+        }
+
+        TaskList otherTaskList = (TaskList) other;
+        return taskList.equals(otherTaskList.taskList);
+    }
+}
