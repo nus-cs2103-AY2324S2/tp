64bf3078d0a65e6a9d9c4b8548000122c75b848c
smolegz <zhuohui.koo@gmail.com>
2024-03-25 18:31:15 +0800

    Test

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -71,7 +71,6 @@ public class CommandBox extends UiPart<Region> {
         String keyName = event.getCode().getName();
         assert !keyName.isEmpty();
 
-        //Perform nothing if there is no history.
         if (inputHistory.inputList.isEmpty()) {
             return ;
         }
