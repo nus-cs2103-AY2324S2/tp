365a4d4634f539025c5c40edb33c8942b17c3c0c
AidenLYT <ytstirlingite@hotmail.com>
2024-03-26 02:49:14 +0800

    Add the use of logger in BkTreeCommandMatcher.java to enable logging system
    * Add logging at BkTreeCommandMatcher to track assertion
    * Add logging at findMatch to track distance calculated for the current item

--- src/main/java/seedu/address/logic/parser/fuzzy/BkTreeCommandMatcher.java
+++ src/main/java/seedu/address/logic/parser/fuzzy/BkTreeCommandMatcher.java
@@ -1,12 +1,17 @@
 package seedu.address.logic.parser.fuzzy;
 
 import java.util.List;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 
 /**
  * BkTree is a data structure used for fuzzy input
  * @param <T> type of elements stored in BkTree
  */
 public class BkTreeCommandMatcher<T> implements CommandMatcher<T> {
+    private static final Logger logger = LogsCenter.getLogger(BkTreeCommandMatcher.class);
+
     private BkTreeNode<T> root;
     private DistanceFunction<T> distanceFunction;
 
@@ -17,12 +22,18 @@ public class BkTreeCommandMatcher<T> implements CommandMatcher<T> {
      * @param items the list of items to be stored in BkTree
      */
     public BkTreeCommandMatcher(List<T> items) {
-        assert items != null && !items.isEmpty() : "Items list must not be empty";
+        logger.info("Check if items list is null or not");
+        if (items == null || items.isEmpty()) {
+            logger.severe("Item list is null or empty.");
+            throw new AssertionError("Items list must not be empty!");
+        }
+        logger.info("Items list check passed. Proceed to setting up required functions.");
         root = new BkTreeNode<>(items.get(0));
         distanceFunction = new LevenshteinDistance<>();
         for (int i = 1; i < items.size(); i++) {
             insert(root, items.get(i));
         }
+        logger.info("Set up completed.");
     }
 
     private void insert(BkTreeNode<T> node, T item) {
@@ -61,6 +72,8 @@ public class BkTreeCommandMatcher<T> implements CommandMatcher<T> {
         }
 
         this.closestDistance = distanceFunction.calculateDistance(item, closestMatch);
+        logger.fine("Current Item:" + item + "Its Closest Match: "
+                + closestMatch + "Its closest distance: " + closestDistance);
         return closestMatch;
     }
 
