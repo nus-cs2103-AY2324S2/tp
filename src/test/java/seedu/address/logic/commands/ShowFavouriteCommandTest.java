c37c44d80abf3a7a60b5cf28b7449ea1cb6d54fd
Sun Ruiheng <sunruiheng2001@gmail.com>
2024-03-26 01:29:52 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/ShowFavouriteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ShowFavouriteCommandTest.java
@@ -5,16 +5,12 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import javafx.collections.transformation.FilteredList;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
 import seedu.address.testutil.PersonBuilder;
 
 /**
