abe8b8e1a2bb299816d2cd1ff6675b76e0f579d2
appleraincoat <james20111951@gmail.com>
2024-03-25 21:38:26 +0800

    Fix checkstyle issues

--- src/test/java/seedu/realodex/model/remark/RemarkTest.java
+++ src/test/java/seedu/realodex/model/remark/RemarkTest.java
@@ -1,7 +1,8 @@
 package seedu.realodex.model.remark;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.realodex.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
