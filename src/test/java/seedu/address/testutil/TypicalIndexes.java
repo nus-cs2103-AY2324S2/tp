d68ee63048840304c7db16bced2ec4e83d1a518d
Tejas Garrepally <code@gtejas.com>
2024-03-25 17:09:42 +0800

    Add list of indices

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -2,6 +2,8 @@ package seedu.address.testutil;
 
 import seedu.address.commons.core.index.Index;
 
+import java.util.List;
+
 /**
  * A utility class containing a list of {@code Index} objects to be used in tests.
  */
@@ -9,4 +11,5 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
+    public static final List<Index> INDEX_LIST = List.of(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON, INDEX_THIRD_PERSON);
 }
