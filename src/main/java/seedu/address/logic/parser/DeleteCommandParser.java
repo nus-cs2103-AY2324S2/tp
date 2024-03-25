c18716ad4d1eac29ea5c6b39636546c71bf290ca
Ragnapop <jinhunl@gmail.com>
2024-03-25 20:28:33 +0800

    Update DeleteCommandParser.java

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,12 +2,11 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
-
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NUSID;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.GroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NusId;
+
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -37,7 +36,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
 
             }
 
-            else{
+            else {
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
             }
 
