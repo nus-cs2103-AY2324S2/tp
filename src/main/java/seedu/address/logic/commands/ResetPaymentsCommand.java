dcb78d78598cf377e3a52a6f923c1bfbd00f71cc
Kin Tat <110956850+KinTatHo@users.noreply.github.com>
2024-03-25 11:05:37 +0800

    Checkstyle error fixes

--- src/main/java/seedu/address/logic/commands/ResetPaymentsCommand.java
+++ src/main/java/seedu/address/logic/commands/ResetPaymentsCommand.java
@@ -7,8 +7,8 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Id;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Payment;
+import seedu.address.model.person.Person;
 
 /**
  * Resets the payments for a person to $0.
