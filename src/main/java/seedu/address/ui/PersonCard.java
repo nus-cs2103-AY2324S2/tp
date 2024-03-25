6395a623b69ca012befaf8cd1978369639c162ac
yiwern5 <nicoletyw505@gmail.com>
2024-03-25 16:42:18 +0800

    Update person card ui
    modified efficiency layout and colour

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
 import seedu.address.model.person.Person;
 
 /**
@@ -54,7 +55,13 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        efficiency.setText(person.getEfficiency().value);
+        efficiency.setText(person.getEfficiency().value + "%");
+        double efficiencyValue = Double.parseDouble(person.getEfficiency().value);
+        if (efficiencyValue < 80 && efficiencyValue > 20) {
+            efficiency.setStyle("-fx-text-fill: yellow;");
+        } else if (efficiencyValue <= 20) {
+            efficiency.setStyle("-fx-text-fill: red;");
+        }
         Label departmentLabel = new Label(person.getDepartment().department);
         departmentLabel.setStyle("-fx-background-color: #3e7b91;");
         tags.getChildren().add(departmentLabel);
