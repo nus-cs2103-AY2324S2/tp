8d0a45a7ccf16a9b4185f47d8b5d3974b69c2755
yleeyilin <yiiiliiinn@gmail.com>
2024-03-25 20:05:45 +0800

    Add addressbook tests

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -34,10 +34,13 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.NoteCommand;
+import seedu.address.logic.commands.PinCommand;
 import seedu.address.logic.commands.SearchCommand;
+import seedu.address.logic.commands.UnpinCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.KeywordPredicate;
 import seedu.address.model.person.Maintainer;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Staff;
@@ -187,6 +190,17 @@ public class AddressBookParserTest {
         assertTrue(command instanceof NoteCommand);
     }
 
+    @Test
+    public void parseCommand_pin() throws Exception {
+        PinCommand command = (PinCommand) parser.parseCommand("/pin ; name : Bob Choo");
+        assertEquals(new PinCommand(new Name("Bob Choo")), command);
+    }
+
+    @Test
+    public void parseCommand_unpin() throws Exception {
+        UnpinCommand command = (UnpinCommand) parser.parseCommand("/unpin ; name : Bob Choo");
+        assertEquals(new UnpinCommand(new Name("Bob Choo")), command);
+    }
 
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() {
