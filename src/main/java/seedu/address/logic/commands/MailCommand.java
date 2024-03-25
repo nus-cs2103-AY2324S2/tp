2607da28ed9a799c6ad77e333de72abddfe88f0d
Derekljh <E0960128@u.nus.edu>
2024-03-25 17:10:55 +0800

    Add Mail function

---
+++ src/main/java/seedu/address/logic/commands/MailCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+
+import java.awt.*;
+import java.net.URI;
+
+/**
+ * Lists all email of contacts associated with the tag.
+ */
+public class MailCommand extends Command {
+
+    public static final String COMMAND_WORD = "mail";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists the email of contacts containing the inputted tag\n"
+            + "Parameters: TAG\n"
+            + "Example: " + COMMAND_WORD + "friends";
+
+    public static final String MESSAGE_EMAIL_CONTACT_SUCCESS = "Here is the list of emails: %1$s";
+
+    private final TagContainsKeywordsPredicate predicate;
+
+    public MailCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        requireNonNull(history);
+
+        model.updateFilteredPersonList(predicate);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < model.getFilteredPersonList().size(); i++) {
+            builder.append(model.getFilteredPersonList().get(i).getEmail()).append(" ");
+        }
+        return new CommandResult(
+                String.format(MESSAGE_EMAIL_CONTACT_SUCCESS, builder));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof MailCommand)) {
+            return false;
+        }
+
+        MailCommand otherMailCommand = (MailCommand) other;
+        return predicate.equals(otherMailCommand.predicate);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/MailCommandParser.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.MailCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class MailCommandParser implements Parser<MailCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public MailCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MailCommand.MESSAGE_USAGE));
+        }
+
+        String[] tagKeywords = trimmedArgs.split("\\s+");
+
+        return new MailCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
+    }
+
+}
