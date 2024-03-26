fe64240ee199517bf0afc6896a626d9e0a2b0a8f
meowwtama <122257205+meowwtama@users.noreply.github.com>
2024-03-26 20:34:37 +0800

    Edit return command to take in Book
    Added methods to return immutable views of book list in Person class.
    
    Now return command accepts the book to be returned as a parameter so
    that we can keep track of the books returned.

--- src/main/java/seedu/address/logic/parser/ReturnCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ReturnCommandParser.java
@@ -2,11 +2,15 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_BOOKLIST;
+
+import java.util.NoSuchElementException;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.ReturnCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.book.Book;
 
 /**
  * Parses input arguments and creates a new ReturnCommand object
@@ -21,15 +25,17 @@ public class ReturnCommandParser implements Parser<ReturnCommand> {
      */
     public ReturnCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_BOOKLIST);
 
         Index index;
+        String bookTitle;
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        } catch (IllegalValueException ive) {
+            bookTitle = argMultimap.getValue(PREFIX_BOOKLIST).get();
+        } catch (IllegalValueException | NoSuchElementException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReturnCommand.MESSAGE_USAGE), ive);
         }
 
-        return new ReturnCommand(index);
+        return new ReturnCommand(index, new Book(bookTitle));
     }
 }
