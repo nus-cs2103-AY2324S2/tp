a271131cdfe32e284568ea1e9104b044a644b834
Emberlynn-Loo <112895879+Emberlynn-Loo@users.noreply.github.com>
2024-03-26 23:52:05 +0800

    Update RelationshipUtilTest.java

--- src/test/java/seedu/address/model/person/relationship/RelationshipUtilTest.java
+++ src/test/java/seedu/address/model/person/relationship/RelationshipUtilTest.java
@@ -223,15 +223,15 @@ public class RelationshipUtilTest {
         // Create relationships involving person1 and person2
         Relationship relationship1 = new Relationship(uuid1, uuid2, "family");
         Relationship relationship2 = new Relationship(uuid2, uuid1, "friend");
-    
+
         // Create RelationshipUtil instance and add the relationships
         RelationshipUtil relationshipUtil = new RelationshipUtil();
         relationshipUtil.addRelationship(relationship1);
         relationshipUtil.addRelationship(relationship2);
-    
+
         // Delete all relationships involving person2
         relationshipUtil.deleteRelationshipsOfPerson(uuid2);
-    
+
         // Check if the relationships involving person2 have been deleted
         assertFalse(relationshipUtil.hasRelationship(relationship1));
         assertFalse(relationshipUtil.hasRelationship(relationship2));
