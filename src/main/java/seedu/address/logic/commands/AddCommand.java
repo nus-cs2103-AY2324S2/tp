30aa8253c687de3a7aa3891095a4999594a5c802
h4ow3i <e0968947@u.nus.edu>
2024-03-26 15:12:32 +0800

    Apply requested changes from PR review
    This commit implements the changes requested during the PR review.
    
    Specifically, updates have been made to several classes to address
    issues raised in the review process. These changes ensure better
    code quality and alignment with project requirements.

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -29,7 +29,7 @@ public class AddCommand extends Command {
             + PREFIX_LASTNAME + "LAST NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_SEX + "SEX "
-            + PREFIX_PAYRATE + "PAY RATE"
+            + PREFIX_PAYRATE + "PAY RATE "
             + PREFIX_ADDRESS + "ADDRESS "
             + PREFIX_BANKDETAILS + "BANK DETAILS \n"
             //  + PREFIX_TAG + "TAG...\n"
@@ -38,7 +38,7 @@ public class AddCommand extends Command {
             + PREFIX_LASTNAME + "Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_SEX + "m "
-            + PREFIX_PAYRATE + "14"
+            + PREFIX_PAYRATE + "14 "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_BANKDETAILS + "0495858505";
     //  + PREFIX_TAG + "owesMoney";
