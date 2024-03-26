af3268716ddcc4a81891a734b95633ae417108b9
cheahTJ <cheahtzejuen@gmail.com>
2024-03-26 21:17:12 +0800

    Create PinCommand and PinCommandParser test classes

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -144,6 +144,10 @@ public class AddCommandTest {
         }
 
         @Override
+        public void pinPerson(Person person) {
+            throw new AssertionError("This method should not be called");
+        }
+        @Override
         public void setPerson(Person target, Person editedPerson) {
             throw new AssertionError("This method should not be called.");
         }
