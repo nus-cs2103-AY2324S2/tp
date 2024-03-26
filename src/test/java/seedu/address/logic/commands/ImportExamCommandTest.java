5471736592f6921a0d0be062d5ec1147e1ed2500
Zer0Legion <lengkhai@gmail.com>
2024-03-26 19:17:52 +0800

    Style: fix checkstyle violations
    Now the code is much neater and follows a standard convention.
    It looks like one single person has written the entire codebase.
    
    Lets improve coding standard.

--- src/test/java/seedu/address/logic/commands/ImportExamCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportExamCommandTest.java
@@ -1,12 +1,21 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.ImportExamCommand.addToErrorReport;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddExamCommandParser;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -14,25 +23,11 @@ import seedu.address.model.exam.Exam;
 import seedu.address.model.exam.UniqueExamList;
 import seedu.address.model.person.Score;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ImportExamCommand.addToErrorReport;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 public class ImportExamCommandTest {
 
-    public static final String MIDTERM_NOT_FOUND_ERROR = "\nBelow are the errors that occurred while importing exams:\n" +
-            "Midterm: Exam not found\n";
+    public static final String MIDTERM_NOT_FOUND_ERROR =
+            "\nBelow are the errors that occurred while importing exams:\n"
+            + "Midterm: Exam not found\n";
     public static final String CREATE_MIDTERM_100 = " n/Midterm s/100";
     public static final String VALID_PATH = "src/test/data/ImportExamCommandTest/testimportexam.csv";
     private ImportExamCommand importExamCommand;
@@ -41,7 +36,6 @@ public class ImportExamCommandTest {
     @BeforeEach
     public void setUp() {
         model = mock(Model.class);
-        StringBuilder errorReport = new StringBuilder();
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         UniqueExamList examList = new UniqueExamList();
         examList.add(new Exam("Midterm", new Score(100)));
@@ -66,8 +60,13 @@ public class ImportExamCommandTest {
 
     @Test
     public void testAddToErrorReport() {
+        Path invalidFilePath = Paths.get("invalid/path/to/file.csv");
+        importExamCommand = new ImportExamCommand(invalidFilePath);
+
         addToErrorReport("Email", "Invalid email format");
-        assertEquals("\nBelow are the errors that occurred while importing exams:\nEmail: Invalid email format\n", ImportExamCommand.generateErrorReport());
+        assertEquals(
+                "\nBelow are the errors that occurred while importing exams:\nEmail: Invalid email format\n",
+                ImportExamCommand.generateErrorReport());
     }
 
     @Test
@@ -83,11 +82,12 @@ public class ImportExamCommandTest {
         addExamCommand.execute(model);
         ImportExamCommand importExamCommand = new ImportExamCommand(filePath);
         importExamCommand.execute(model);
-        assertCommandSuccess(importExamCommand, model, String.format(ImportExamCommand.MESSAGE_SUCCESS, filePath), model);
+        assertCommandSuccess(importExamCommand, model, String.format(
+                ImportExamCommand.MESSAGE_SUCCESS, filePath), model);
     }
 
     @Test
-    public void test_ExamNotExist_successWithErrorReport() throws CommandException {
+    public void testExamNotExist_successWithErrorReport() throws CommandException {
         Path filePath = Paths.get(VALID_PATH);
         ImportExamCommand importExamCommand = new ImportExamCommand(filePath);
         String expectedMessage = String.format(ImportExamCommand.MESSAGE_SUCCESS, filePath);
--- src/test/java/seedu/address/logic/parser/ImportExamCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ImportExamCommandParserTest.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.ImportExamCommand;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ImportExamCommand;
 
 public class ImportExamCommandParserTest {
     private ImportExamCommandParser parser = new ImportExamCommandParser();
@@ -12,12 +13,14 @@ public class ImportExamCommandParserTest {
     @Test
     public void parse_noArgsPassed_failure() {
         assertParseFailure(
-                parser, "importExam ie/", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportExamCommand.MESSAGE_USAGE));
+                parser, "importExam ie/",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportExamCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_notCsvFile_failure() {
         assertParseFailure(
-                parser, "importExam ie/file.json", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportExamCommand.MESSAGE_USAGE));
+                parser, "importExam ie/file.json",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportExamCommand.MESSAGE_USAGE));
     }
 }
