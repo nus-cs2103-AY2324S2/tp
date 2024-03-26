04179620b68249281abad73381f3227035f29a6e
Theodore Koo <e0958222@u.nus.edu>
2024-03-26 21:40:41 +0800

    no message

--- src/main/java/seedu/address/model/client/TagContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/client/TagContainsKeywordsPredicate.java
@@ -1,7 +1,7 @@
 package seedu.address.model.client;
 
-import java.util.function.Predicate;
 import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
