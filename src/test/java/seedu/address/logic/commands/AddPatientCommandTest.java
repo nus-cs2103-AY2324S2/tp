4095c4df1054b1a4e759b7d1d4a3a005b9bcc028
alfaloo <zhiylu528@gmail.com>
2024-03-26 22:48:46 +0800

    Implement editappt

--- src/test/java/seedu/address/logic/commands/AddPatientCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPatientCommandTest.java
@@ -152,6 +152,11 @@ public class AddPatientCommandTest {
         }
 
         @Override
+        public void setAppointment(Appointment target, Appointment editedAppointment) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
