0482579af499e510fd61a9f1ec1a94f6df634d7f
NatLeong <>
2024-03-26 13:48:28 +0800

    Add tests for ReadCommand and ReadCommandParser

--- src/test/java/seedu/address/logic/commands/ReadCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ReadCommandTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.NON_EXISTENT_NRIC;
-import static seedu.address.testutil.TypicalPersons.BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.ReadCommand.MESSAGE_READ_PERSON_SUCCESS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -16,28 +21,32 @@ import seedu.address.model.person.Nric;
 
 public class ReadCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
-    /*@Test
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    @Test
     void execute_validNric_success() {
-
+        String expectedMessage = String.format(MESSAGE_READ_PERSON_SUCCESS, Messages.formatRead(ALICE));
+        ReadCommand command = new ReadCommand(ALICE.getNric());
+        expectedModel.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
     }
 
     @Test
-    void execute_invalidNric_failure() {
-
-    }*/
+    void execute_nonExistentNric_failure() {
+        Nric nonexistentNric = new Nric("S1234567A");
+        assertCommandFailure(new ReadCommand(nonexistentNric), model, Messages.MESSAGE_PERSON_NOT_FOUND);
+    }
 
     @Test
     void equals() {
-        ReadCommand readFirstCommand = new ReadCommand(BOB.getNric());
+        ReadCommand readFirstCommand = new ReadCommand(ALICE.getNric());
         ReadCommand readSecondCommand = new ReadCommand(new Nric(NON_EXISTENT_NRIC));
 
         // same object -> returns true
         assertTrue(readFirstCommand.equals(readFirstCommand));
 
         // same values -> returns true
-        ReadCommand readFirstCommandCopy = new ReadCommand(BOB.getNric());
+        ReadCommand readFirstCommandCopy = new ReadCommand(ALICE.getNric());
         assertTrue(readFirstCommand.equals(readFirstCommandCopy));
 
         // different types -> returns false
@@ -52,8 +61,8 @@ public class ReadCommandTest {
 
     @Test
     void testToString() {
-        ReadCommand readCommand = new ReadCommand(BOB.getNric());
-        String expected = ReadCommand.class.getCanonicalName() + "{nric=" + BOB.getNric() + "}";
+        ReadCommand readCommand = new ReadCommand(ALICE.getNric());
+        String expected = ReadCommand.class.getCanonicalName() + "{nric=" + ALICE.getNric() + "}";
         assertEquals(expected, readCommand.toString());
     }
 
