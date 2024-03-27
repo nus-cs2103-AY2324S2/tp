d05f06bb240811d32d979727cfc5ab0413224473
bgopi23 <baskargopinath01@gmail.com>
2024-03-27 14:52:08 +0800

    Update add command

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -69,12 +69,4 @@ public class AddCommandParser implements Parser<AddCommand> {
             return new AddCommand(person);
         }
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
 }
