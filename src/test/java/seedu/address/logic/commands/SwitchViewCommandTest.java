466aa81bc3823ff9c9f5e75bfa152316f9d1bfa3
rik <vslimrik@gmail.com>
2024-03-27 15:02:24 +0800

    Update tests

--- src/test/java/seedu/address/logic/commands/SwitchViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SwitchViewCommandTest.java
@@ -1,20 +1,20 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+import static seedu.address.logic.commands.SwitchViewCommand.MESSAGE_SUCCESS;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
-public class ExitCommandTest {
+public class SwitchViewCommandTest {
     private Model model = new ModelManager();
     private Model expectedModel = new ModelManager();
 
     @Test
     public void execute_exit_success() {
-        CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true, false);
-        assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_SUCCESS, false, false, true);
+        assertCommandSuccess(new SwitchViewCommand(), model, expectedCommandResult, expectedModel);
     }
 }
