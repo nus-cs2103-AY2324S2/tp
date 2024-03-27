e3c487864a58984ceb9fd50785c8643b1cf20474
Eunice Koh <eunicekoh46@gmail.com>
2024-03-27 20:03:49 +0800

    resolve conflict

--- src/main/java/seedu/address/logic/commands/ReminderCommand.java
+++ src/main/java/seedu/address/logic/commands/ReminderCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
@@ -11,8 +9,6 @@ import seedu.address.model.person.Person;
  */
 public class ReminderCommand extends Command {
     public static final String COMMAND_WORD = "reminder";
-    public static final String MESSAGE_USAGE = "Reminder should be followed by the number of days.\n"
-            + "Example: reminder 4";
     public static final String MESSAGE_SUCCESS = "Listed applications that are due or have interviews in %1$d days.";
 
     private int numberOfDays;
@@ -26,14 +22,6 @@ public class ReminderCommand extends Command {
         // Define a predicate to filter persons with interviews within N days
         Predicate<Person> isWithinNDaysPredicate = person ->
                 person.getInterviewDate() != null && person.getInterviewDate().isWithinNDays(numberOfDays);
-
-        // Get all persons from the model
-        List<Person> allPersons = model.getAddressBook().getPersonList();
-
-        // Filter the persons using the predicate
-        List<Person> reminderList = allPersons.stream()
-                .filter(isWithinNDaysPredicate)
-                .collect(Collectors.toList());
 
         // Update the filtered list in the model using a predicate
         model.updateFilteredPersonList(isWithinNDaysPredicate);
