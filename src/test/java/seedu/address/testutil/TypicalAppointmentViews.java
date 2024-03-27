4224287e5cbc25f0fa67d301e8076e93c6cfe676
unknown <xuelinglow09@gmail.com>
2024-03-28 00:18:12 +0800

    Make changes to AppointmentView

--- src/test/java/seedu/address/testutil/TypicalAppointmentViews.java
+++ src/test/java/seedu/address/testutil/TypicalAppointmentViews.java
@@ -7,7 +7,15 @@ import static seedu.address.testutil.TypicalAppointments.ALICE_APPT_1;
 import static seedu.address.testutil.TypicalAppointments.AMY_APPT;
 import static seedu.address.testutil.TypicalAppointments.BENSON_APPT;
 import static seedu.address.testutil.TypicalAppointments.BOB_APPT;
+import static seedu.address.testutil.TypicalAppointments.getTypicalAppointments;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.appointment.Appointment;
 import seedu.address.model.appointment.AppointmentView;
 
 /**
@@ -35,4 +43,23 @@ public class TypicalAppointmentViews {
 
     private TypicalAppointmentViews() {
     } // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical
+     * persons and appointments
+     */
+    public static AddressBook getTypicalAddressBookWithAppointmentViews() {
+        AddressBook ab = getTypicalAddressBook();
+
+        for (Appointment appointment : getTypicalAppointments()) {
+            //appointmentView is updated every time
+            ab.addAppointment(appointment);
+        }
+        return ab;
+    }
+
+    public static List<AppointmentView> getTypicalAppointmentViews() {
+        return new ArrayList<>(Arrays.asList(ALICE_APPT_VIEW, ALICE_APPT_VIEW_1, BENSON_APPT_VIEW));
+    }
+
 }
