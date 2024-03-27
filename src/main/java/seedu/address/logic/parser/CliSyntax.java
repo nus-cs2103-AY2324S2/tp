c6819afd9f4e5d4e10ff679379d18f8c76bd8c2b
HENRY\YS <yusoonng3@gmail.com>
2024-03-27 17:12:54 +0800

    Add syntax for adding note

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -9,6 +9,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_CLASS_GROUP = new Prefix("c/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_NOTE = new Prefix("note/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_TELEGRAM = new Prefix("t/");
     public static final Prefix PREFIX_GITHUB = new Prefix("g/");
