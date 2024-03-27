31b6b0acb40625c5c4533e1f20380f011e43ccf0
ziiqii <ziiiqiii@gmail.com>
2024-03-27 20:39:29 +0800

    Add tests for Reservation and its fields

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -36,6 +36,12 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_DATE_AMY_RSV = "2024-01-01";
+    public static final String VALID_DATE_BOB_RSV = "2024-12-31";
+    public static final String VALID_TIME_AMY_RSV = "0000";
+    public static final String VALID_TIME_BOB_RSV = "2359";
+    public static final String VALID_PAX_AMY_RSV = "1";
+    public static final String VALID_PAX_BOB_RSV = "8";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
