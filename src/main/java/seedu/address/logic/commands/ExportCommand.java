9c7f4570f795a6b6b38b36795270542adbc5ec07
ChrisHo1341 <christopher.hwh@gmail.com>
2024-03-27 11:03:34 +0800

    Update ExportCommand.java
    Fix MESSAGE_USAGE and adds exception for when user saves to HRConnect and ArchivedApplicants

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -22,15 +23,27 @@ public class ExportCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports all contacts currently shown.\n"
         + "Usually used after 'filter' or 'find' command to export a subset of contacts. \n"
-        + "Example: " + COMMAND_WORD;
+        + "Parameters: FILE_NAME (cannot be HRConnect or ArchivedApplicants)"
+        + "Example: " + COMMAND_WORD + " NEW_APPLICANTS";
 
     public static final String MESSAGE_SUCCESS = "Contacts have been exported!";
 
     public static final String FILE_OPS_ERROR_FORMAT = "Could not save data due to the following error: %s";
 
+    private static final String HRCONNECT = "HRConnect";
+
+    private static final String ARCHIVED_APPLICANTS = "ArchivedApplicants";
+
     private Path savePath;
 
+    private String fileName;
+
+    /**
+     * 
+     * @param fileName name of file where contacts are to be saved
+     */
     public ExportCommand(String fileName) {
+        this.fileName = fileName.trim();
         savePath = Paths.get("data", fileName + ".json");
     }
 
@@ -42,6 +55,10 @@ public class ExportCommand extends Command {
         JsonAddressBookStorage storage = new JsonAddressBookStorage(savePath);
         AddressBook book = new AddressBook();
         book.setPersons(lastShownList);
+
+        if (fileName.equals(HRCONNECT) || fileName.equals(ARCHIVED_APPLICANTS)) {
+            throw new CommandException(Messages.MESSAGE_INVALID_FILE_NAME);
+        }
 
         try {
             storage.saveAddressBook(book);
