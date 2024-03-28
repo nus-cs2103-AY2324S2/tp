2a9a30b32437a596f83818a258eff3b826c30237
larainezo <larainelwx@gmail.com>
2024-03-28 23:57:37 +0800

    Fix code style

--- src/test/java/seedu/address/logic/commands/DeleteTimeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTimeCommandTest.java
@@ -3,7 +3,12 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_DELETE_TIME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_DELETE_TIME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_FREE_TIME_TAG_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -17,7 +22,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.testutil.AddPersonFreeTimeDescriptorBuilder;
 import seedu.address.testutil.DeletePersonFreeTimeDescriptorBuilder;
 
 /**
