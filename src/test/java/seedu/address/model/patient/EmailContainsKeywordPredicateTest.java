74b42c4bc5bd2a3844972b8dd69eb20d1ed66d6b
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2024-03-28 14:39:23 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/patient/EmailContainsKeywordPredicateTest.java
+++ src/test/java/seedu/address/model/patient/EmailContainsKeywordPredicateTest.java
@@ -1,20 +1,18 @@
 package seedu.address.model.patient;
 
-
 import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.patient.Patient;
 
 /**
  * Tests that a {@code Person}'s {@code Email} matches a part of the keywords given.
  */
-public class EmailContainsKeywordPredicate implements Predicate<Patient> {
+public class EmailContainsKeywordPredicateTest implements Predicate<Patient> {
     private final List<String> keywords;
 
-    public EmailContainsKeywordPredicate(List<String> keywords) {
+    public EmailContainsKeywordPredicateTest(List<String> keywords) {
         this.keywords = keywords;
     }
 
@@ -31,11 +29,12 @@ public class EmailContainsKeywordPredicate implements Predicate<Patient> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof EmailContainsKeywordPredicate)) {
+        if (!(other instanceof EmailContainsKeywordPredicateTest)) {
             return false;
         }
 
-        EmailContainsKeywordPredicate otherNameContainsKeywordsPredicate = (EmailContainsKeywordPredicate) other;
+        EmailContainsKeywordPredicateTest otherNameContainsKeywordsPredicate =
+                (EmailContainsKeywordPredicateTest) other;
         return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
     }
 
