bf1d3b8543531198dc4db869e4503d156b75c52f
Apzure <block4556@gmail.com>
2024-03-28 11:57:02 +0800

    Fix Capitalization

--- src/main/java/seedu/address/ui/PersonListOfSelectedEventPanel.java
+++ src/main/java/seedu/address/ui/PersonListOfSelectedEventPanel.java
@@ -18,6 +18,7 @@ import seedu.address.model.person.Person;
  */
 public class PersonListOfSelectedEventPanel extends UiPart<Region> {
     private static final String FXML = "PersonListOfSelectedEventPanel.fxml";
+
     private final Logger logger = LogsCenter.getLogger(PersonListOfSelectedEventPanel.class);
 
     @FXML
