9508deca4fb9e588cabce51a2a38fc8792309a1b
yap-zong-xin <yapzongxin@hotmail.com>
2024-03-28 18:25:20 +0800

    update edit checkstyle

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -3,18 +3,17 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS_EDIT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL_EDIT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE_EDIT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME_EDIT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE_EDIT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_EDIT;
-
-import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_NAME;
-import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_PHONE;
-import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL_EDIT;
+import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE_EDIT;
 import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_GRADE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME_EDIT;
+import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE_EDIT;
+import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG_EDIT;
+import static seedu.address.logic.parser.CliSyntax.OPTION_PRINT_TAG;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -49,22 +48,34 @@ public class EditCommandParser implements Parser<EditCommand> {
         // Check if any option to print is requested
         if (argMultimap.getValue(OPTION_PRINT_NAME).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), true, false, false, false, false, false);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    true, false, false,
+                    false, false, false);
         } else if (argMultimap.getValue(OPTION_PRINT_PHONE).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), false, true, false, false, false, false);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    false, true, false,
+                    false, false, false);
         } else if (argMultimap.getValue(OPTION_PRINT_EMAIL).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), false, false, true, false, false, false);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    false, false, true,
+                    false, false, false);
         } else if (argMultimap.getValue(OPTION_PRINT_ADDRESS).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), false, false, false, true, false, false);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    false, false,
+                    false, true, false, false);
         } else if (argMultimap.getValue(OPTION_PRINT_TAG).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), false, false, false, false, true, false);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    false, false, false,
+                    false, true, false);
         } else if (argMultimap.getValue(OPTION_PRINT_GRADE).isPresent()) {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            return new EditCommand(index, new EditCommand.EditPersonDescriptor(), false, false, false, false, false, true);
+            return new EditCommand(index, new EditCommand.EditPersonDescriptor(),
+                    false, false, false,
+                    false, false, true);
         }
 
         Index index;
