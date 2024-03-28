aad685865bcaf490da846b201a153749e7f50673
larainezo <larainelwx@gmail.com>
2024-03-28 23:50:48 +0800

    Add test cases

--- src/test/java/seedu/address/logic/commands/AddTimeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTimeCommandTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.commands.CommandTestUtil.DESC_ADD_TIME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_ADD_TIME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_FREE_TIME_TAG_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
@@ -20,6 +21,7 @@ import seedu.address.logic.commands.AddTimeCommand.EditPersonDescriptor;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.AddPersonFreeTimeDescriptorBuilder;
 
 /**
@@ -70,6 +72,31 @@ public class AddTimeCommandTest {
                 new AddPersonFreeTimeDescriptorBuilder().build());
 
         assertCommandFailure(addTimeCommand, model, Messages.MESSAGE_NO_FREETIME_SPECIFIED);
+    }
+
+    /**
+     * Edit filtered list where valid free time is specified
+     */
+    @Test
+    public void execute_newFreeTime_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_FIRST_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        AddTimeCommand addTimeCommand = new AddTimeCommand(outOfBoundIndex,
+                new AddPersonFreeTimeDescriptorBuilder().withFreeTimeTags(VALID_FREE_TIME_TAG_BOB).build());
+
+
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        String expectedMessage = String.format(AddTimeCommand.MESSAGE_ADD_FREETIME_SUCCESS,
+                Messages.format(editedPerson));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandSuccess(addTimeCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
--- src/test/java/seedu/address/logic/commands/DeleteTimeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTimeCommandTest.java
@@ -3,11 +3,7 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_DELETE_TIME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_DELETE_TIME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_FREE_TIME_TAG_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -20,6 +16,8 @@ import seedu.address.logic.commands.DeleteTimeCommand.EditPersonDescriptor;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.AddPersonFreeTimeDescriptorBuilder;
 import seedu.address.testutil.DeletePersonFreeTimeDescriptorBuilder;
 
 /**
@@ -70,6 +68,30 @@ public class DeleteTimeCommandTest {
                 new DeletePersonFreeTimeDescriptorBuilder().build());
 
         assertCommandFailure(deleteTimeCommand, model, Messages.MESSAGE_NO_FREETIME_SPECIFIED);
+    }
+
+    /**
+     * Edit filtered list where valid free time is specified
+     */
+    @Test
+    public void execute_newFreeTime_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_FIRST_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        DeleteTimeCommand deleteTimeCommand = new DeleteTimeCommand(outOfBoundIndex,
+                new DeletePersonFreeTimeDescriptorBuilder().withFreeTimeTags(VALID_FREE_TIME_TAG_BOB).build());
+
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        String expectedMessage = String.format(DeleteTimeCommand.MESSAGE_DELETE_FREETIME_SUCCESS,
+                Messages.format(editedPerson));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandSuccess(deleteTimeCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
