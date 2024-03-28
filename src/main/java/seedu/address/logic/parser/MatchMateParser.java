dbbf4d326447423b9fbbe7db152832ceeb7dd394
Ryan <ryanreno2912@gmail.com>
2024-03-28 14:32:33 +0800

    Resolve checkstyle issue

--- src/main/java/seedu/address/logic/parser/MatchMateParser.java
+++ src/main/java/seedu/address/logic/parser/MatchMateParser.java
@@ -129,7 +129,8 @@ public class MatchMateParser {
 
         case UnmarkImportantCommand.COMMAND_WORD:
             return new UnmarkImportantCommandParser().parse(arguments);
-            
+
+
         case RequireSkillCommand.COMMAND_WORD:
             return new RequireSkillCommandParser().parse(arguments);
 
