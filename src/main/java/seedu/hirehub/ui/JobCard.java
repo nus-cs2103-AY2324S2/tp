f8c1c7ed4332106645343837e6a7da7a04fcb593
0liverkong <oliverkong03@gmail.com>
2024-03-29 01:12:02 +0800

    Fix typo

--- src/main/java/seedu/hirehub/ui/JobCard.java
+++ src/main/java/seedu/hirehub/ui/JobCard.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.hirehub.model.job.Job;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Job}.
  */
 public class JobCard extends UiPart<Region> {
 
